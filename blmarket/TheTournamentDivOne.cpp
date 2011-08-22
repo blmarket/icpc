f6de2dea12efe2953242a393e6974d50e5684658
Jeong Heon <blmarket@gmail.com>
2011-08-22 22:40:50 +0900

    SRM 453 Med 연습 시작.
    대충 생각한 idea는... 일단 모든 가능한 draw 조합을 뽑아놔야겠다는
    
    생각 정도가 들었다. feasible한 게임이 되려면, 전체 draw 횟수의 합이
    
    짝수가 되어야 할 것이고, 가장 큰 단일팀의 draw 횟수가 전체 draw수의
    
    절반을 넘으면 안되는 정도...인데 이런 조건들을 만족하는 feasible한
    
    draw값들의 set을 찾는건... 사실 거기까진 생각 안하고 짜기 시작했다.

---
+++ blmarket/TheTournamentDivOne.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+vector<int> points;
+vector<vector<int> > V;
+int w,d;
+int sum;
+
+class TheTournamentDivOne 
+{
+public:
+    int find(vector <int> points_, int w_, int d_) 
+    {		
+        points = points_;
+        w = w_;
+        d = d_;
+
+        sum = 0; 
+        for(int i=0;i<size(points);i++) sum += points[i];
+
+        if(w == 1) return (sum+1)/2;
+        V.clear();
+        V.resize(size(points));
+
+        for(int i=0;i<size(points);i++)
+        {
+            int tmp = points[i];
+            for(int j=0;;j++, tmp -= d)
+            {
+                if((tmp % w) == 0)
+                {
+                    V[i].pb(j);
+                    cout << j << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {10, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; int Arg2 = 1; int Arg3 = 6; verify_case(0, Arg3, find(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arr0[] = {1, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; int Arg2 = 1; int Arg3 = -1; verify_case(1, Arg3, find(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arr0[] = {1, 4, 0, 2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 1; int Arg3 = 3; verify_case(2, Arg3, find(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arr0[] = {8, 3, 8, 5, 9, 2, 7, 11}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 2; int Arg3 = 15; verify_case(3, Arg3, find(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    TheTournamentDivOne ___test; 
+    ___test.run_test(0); 
+} 
+// END CUT HERE
