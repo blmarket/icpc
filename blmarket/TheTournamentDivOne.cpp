2152e3ebbc6d7b62c353394e88c44c6a1e830ddf
Jeong Heon <blmarket@gmail.com>
2011-08-22 22:58:37 +0900

    디버깅 코드 추가. 패망의 징조가 보인다.

--- blmarket/TheTournamentDivOne.cpp
+++ blmarket/TheTournamentDivOne.cpp
@@ -77,6 +77,8 @@ public:
                 memcpy(cur, next, sizeof(cur));
             }
 
+            cout << cur[0] << " " << cur[1] << endl;
+
             if(cur[0] < i * 2) continue;
             cout << cur[0] + (sum - d * cur[0]) / w << endl;
         }
