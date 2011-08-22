69906d4cc8b234ead20ae9eb15668e02243d1e73
Jeong Heon <blmarket@gmail.com>
2011-08-22 23:02:23 +0900

    --Empty Message--

--- blmarket/TheTournamentDivOne.cpp
+++ blmarket/TheTournamentDivOne.cpp
@@ -71,6 +71,7 @@ public:
                 int idx = lower_bound(V[j].begin(), V[j].end(), -i) - V[j].begin();
                 if(idx == V[j].size()) 
                 {
+                    cout << j << " " << idx << endl;
                     cur[0] = -1;
                     break;
                 }
