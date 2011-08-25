0abe177bd0fffb0ce0e21a03f828a7fb4415c93a
Jeong Heon <blmarket@gmail.com>
2011-08-25 16:09:01 +0900

    디버깅 메시지를 좀 더 자세히 찍어보게 함.

--- blmarket/TheSoccerDivOne.cpp
+++ blmarket/TheSoccerDivOne.cpp
@@ -43,6 +43,7 @@ public:
         {
             int &r = dp[i][a][b];
             r = 1000;
+            int f1,f2;
 
             for(int j=0;j<=4;j++)
             {
@@ -55,17 +56,21 @@ public:
                     for(int m=b-k;m<=b+k;m+=2)
                     {
                         if(m < 0 || m >= 15)  continue;
-                        int &prev = dp[i-1][a+l-j][m];
+                        int tmp = dp[i-1][a+l-j][m] + (newscore > myp ? 1 : 0);
 
-                        setmin(r, prev + (newscore > myp ? 1 : 0));
+                        if(tmp < r)
+                        {
+                            r = tmp;
+                            f1 = a+l-j;
+                            f2 = m;
+                        }
                     }
-                    //int &prev = dp[i-1][a+j-l];
                 }
             }
 
             if(r < 1000)
             {
-                cerr << i << " " << a << " " << b << " = " << r << endl;
+                cerr << i << " " << a << " " << b << " = " << r << " from : " << f1 << " " << f2 << endl;
             }
         }
 
@@ -81,7 +86,7 @@ public:
 	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
 	void test_case_0() { int Arr0[] = {5, 17}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(0, Arg1, find(Arg0)); }
 	void test_case_1() { int Arr0[] = {5, 18, 21, 19}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(1, Arg1, find(Arg0)); }
-	void test_case_2() { int Arr0[] = {4, 1, 5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(2, Arg1, find(Arg0)); }
+	void test_case_2() { int Arr0[] = {11, 23, 18}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(2, Arg1, find(Arg0)); }
 	void test_case_3() { int Arr0[] = {4, 16, 16, 16, 16,16,16,16,16,16,16,16}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; verify_case(3, Arg1, find(Arg0)); }
 
 
