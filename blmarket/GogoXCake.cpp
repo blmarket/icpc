01c7a6c7e3a8a4ffd751eb9a88c7d021e972b869
Jeong, Heon <blmarket@gmail.com>
2012-01-22 12:03:45 +0900

    --Empty Message--

---
+++ blmarket/GogoXCake.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+class GogoXCake 
+{
+public:
+	string solve(vector <string> cake, vector <string> cutter) 
+	{
+            int firstpos = -1;
+            for(int i=0;i<size(cutter[0]);i++)
+            {
+                if(cutter[0][i] == '.') 
+                {
+                    firstpos = i;
+                    break;
+                }
+            }
+            for(int i=0;i<size(cake);i++)
+            {
+                for(int j=0;j<size(cake[i]);j++)
+                {
+                    if(cake[i][j] == '.')
+                    {
+                        int x = i;
+                        int y = j - firstpos;
+                        for(int a=0;a<size(cutter);a++)
+                        {
+                            for(int b=0;b<size(cutter[0]);b++)
+                            {
+                                if(cutter[a][b] == '.')
+                                {
+                                    if(cake[x+a][y+b] != '.') return "NO";
+                                    cake[x+a][y+b] = 'X';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            for(int i=0;i<size(cake);i++) for(int j=0;j<size(cake[i]);j++) if(cake[i][j] == '.') return "NO";
+            return "YES";
+	}
+	
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); if ((Case == -1) || (Case == 6)) test_case_6(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const string &Expected, const string &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"X.X"
+,"..."
+,"..."
+,"X.X"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {".X"
+,".."
+,"X."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "YES"; verify_case(0, Arg2, solve(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {"..XX"
+,"...X"
+,"X..."
+,"XX.."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {".."
+,".."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "NO"; verify_case(1, Arg2, solve(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"...X..."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"..."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "YES"; verify_case(2, Arg2, solve(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {".X."
+,"X.X"
+,".X."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "YES"; verify_case(3, Arg2, solve(Arg0, Arg1)); }
+	void test_case_4() { string Arr0[] = {"XXXXXXX"
+,"X.....X"
+,"X.....X"
+,"X.....X"
+,"XXXXXXX"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {".X."
+,"XXX"
+,".X."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "NO"; verify_case(4, Arg2, solve(Arg0, Arg1)); }
+	void test_case_5() { string Arr0[] = {".."
+,"X."
+,".X"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {".."
+,".X"
+,"X."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "NO"; verify_case(5, Arg2, solve(Arg0, Arg1)); }
+	void test_case_6() { string Arr0[] = {"X.."
+,".XX"
+,".XX"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {".XX"
+,".XX"
+,"X.."}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "NO"; verify_case(6, Arg2, solve(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+
+
+// BEGIN CUT HERE 
+int main() {
+	GogoXCake ___test;
+	___test.run_test(-1);
+}
+// END CUT HERE
