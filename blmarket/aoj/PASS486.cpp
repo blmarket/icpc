ad59137f50f2f6731fe588f1c765bee1d3d604c3
Jeong, Heon <blmarket@gmail.com>
2012-04-25 21:22:55 +0900

    --Empty Message--

---
+++ blmarket/aoj/PASS486.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <list>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+vector<int> primes;
+
+void process(int dataId)
+{
+}
+
+void precalc(void)
+{
+    primes.pb(2);
+    for(int i=3;i<=10000000;i+=2)
+    {
+        bool fail = false;
+        for(int j=0;primes[j]*primes[j]<i;j++)
+        {
+            if((i % primes[j]) == 0) { fail=true; break; }
+        }
+        if(!fail) primes.pb(i);
+    }
+    cout << size(primes) << endl;
+}
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    for(int i=1;i<=N;i++)
+    {
+        process(i);
+    }
+}
