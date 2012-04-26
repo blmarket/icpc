09453e8a32a14965cc73db794c380392d093f234
Jeong, Heon <blmarket@gmail.com>
2012-04-26 20:58:48 +0900

    --Empty Message--

--- blmarket/aoj/WITHDRAWL.cpp
+++ blmarket/aoj/WITHDRAWL.cpp
@@ -29,7 +29,7 @@ bool comp(const PII &a, const PII &b)
 {
     if(a.first * b.second == a.second * b.first)
     {
-        return a.second < b.second;
+        return a.second > b.second;
     }
     return a.first * b.second < a.second * b.first;
 }
@@ -46,17 +46,24 @@ void process(int dataId)
         V.pb(mp(t1,t2));
     }
     sort(V.begin(), V.end(), comp);
-    for(int i=0;i<n;i++)
-    {
-        cout << V[i].first << "," << V[i].second << " ";
-    }
-    cout << endl;
 
-    int s1=0, s2=0;
-    for(int i=0;i<k;i++)
+    int s1=V[0].first, s2=V[0].second;
+    for(int i=1;i<k;i++)
     {
-        s1 += V[i].first;
-        s2 += V[i].second;
+        PII t = mp(s1 + V[i].first, s2 + V[i].second);
+
+        for(int j=i+1;j<n;j++)
+        {
+            PII tt = mp(s1 + V[j].first, s2 + V[j].second);
+            if(comp(t,tt))
+            {
+                swap(V[i], V[j]);
+                t=tt;
+            }
+        }
+
+        s1 = t.first;
+        s2 = t.second;
     }
     double ret = (double)s1 / s2;
     printf("%.12lf\n",ret);
