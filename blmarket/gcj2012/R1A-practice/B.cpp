99ab50609067412f6de92842ca71cdd7ec44dbb6
Jeong, Heon <blmarket@gmail.com>
2012-04-30 11:26:25 +0900

    --Empty Message--

---
+++ blmarket/gcj2012/R1A-practice/B.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+#define MULTI 2
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int n;
+int a[1005], b[1005];
+int mark[1005];
+
+void solve(int dataId)
+{
+    int score = 0;
+    int ng = 0;
+    while(score < n*2)
+    {
+        int candi = -1;
+        for(int i=0;i<n;i++)
+        {
+            if(b[i] <= score)
+            {
+                ng++;
+                score += 2 - mark[i];
+                mark[i] = 2;
+                break;
+            }
+            if(a[i] > score) continue;
+            if(candi == -1 || b[candi] < b[i])
+                candi = i;
+        }
+        if(candi == -1)
+        {
+            printf("Too Bad\n");
+            return;
+        }
+        ng++; score++;
+        mark[candi] = 1;
+    }
+    printf("%d\n", ng);
+}
+
+void process(int dataId)
+{
+    cin >> n;
+    memset(mark, 0, sizeof(mark));
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i] >> b[i];
+    }
+}
+
+class ForkSolver {
+public:
+    ForkSolver() : nchilds(0) { }
+
+    vector<pair<pid_t,int> > outfds;
+    void _solve(int dataId);
+    int nchilds;
+} solver;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    solver.outfds.resize(N+1);
+    for(int i=1;i<=N;i++)
+    {
+        process(i);
+        solver._solve(i);
+    }
+    for(int i=1;i<=N;i++)
+    {
+        int status;
+        char buffer[8192];
+        waitpid(solver.outfds[i].first, &status, 0);
+        ssize_t sz;
+        while(sz = read(solver.outfds[i].second, buffer, sizeof(buffer)))
+        {
+            ssize_t iter = 0, outsz;
+
+            while(iter != sz)
+            {
+                outsz = write(1, buffer + iter, sz - iter);
+                if(outsz == -1)
+                {
+                    perror("write");
+                    exit(-1);
+                }
+                iter += outsz;
+            }
+        }
+    }
+}
+
+void ForkSolver::_solve(int dataId)
+{
+    int pipefd[2];
+
+    while(nchilds >= MULTI)
+    {
+        int status = 0;
+        int ret = waitpid(-1, &status, 0);
+        if(ret == -1)
+            perror("waitpid");
+        else
+            nchilds--;
+    }
+
+    pipe(pipefd);
+    pid_t pid = fork();
+    if(pid == -1)
+    {
+        perror("fork");
+        exit(-1);
+    }
+
+    if(pid) // I'm parent!
+    {
+        close(pipefd[1]); // close write end, i'll only read
+        outfds[dataId] = mp(pid, pipefd[0]);
+    }
+    else
+    {
+        if(close(pipefd[0]) == -1) perror("close");
+        if(dup2(pipefd[1], 1) == -1) perror("dup2");
+        if(close(pipefd[1]) == -1) perror("close");
+        solve(dataId);
+        fprintf(stderr, "%d\n", dataId);
+        exit(0); // force exit
+    }
+}
+
+
