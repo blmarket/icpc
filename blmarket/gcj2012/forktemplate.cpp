b369195e1e8f126023c4ee6706404d17618c11eb
Jeong, Heon <blmarket@gmail.com>
2012-04-30 10:36:44 +0900

    init codespace for 2012 R1A

---
+++ blmarket/gcj2012/forktemplate.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+#define MULTI 3
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+void solve(int dataId)
+{
+    // do solve data here
+    cout << dataId << endl;
+    printf("0\n",dataId);
+}
+
+void process(int dataId)
+{
+    // do read data here
+}
+
+class ForkSolver {
+public:
+    ForkSolver() : nchilds(0) { }
+
+    vector<pair<pid_t,int> > outfds;
+    void _solve(int dataId);
+    int nchilds;
+} solver;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    solver.outfds.resize(N+1);
+    for(int i=1;i<=N;i++)
+    {
+        process(i);
+        solver._solve(i);
+    }
+    for(int i=1;i<=N;i++)
+    {
+        int status;
+        char buffer[8192];
+        waitpid(solver.outfds[i].first, &status, 0);
+        ssize_t sz;
+        while(sz = read(solver.outfds[i].second, buffer, sizeof(buffer)))
+        {
+            ssize_t iter = 0, outsz;
+
+            while(iter != sz)
+            {
+                outsz = write(0, buffer + iter, sz - iter);
+                if(outsz == -1)
+                {
+                    perror("write");
+                    exit(-1);
+                }
+                iter += outsz;
+            }
+        }
+    }
+}
+
+void ForkSolver::_solve(int dataId)
+{
+    int pipefd[2];
+
+    while(nchilds >= MULTI)
+    {
+        int status = 0;
+        int ret = waitpid(-1, &status, 0);
+        if(ret == -1)
+            perror("waitpid");
+        else
+            nchilds--;
+    }
+
+    pipe(pipefd);
+    pid_t pid = fork();
+    if(pid == -1)
+    {
+        perror("fork");
+        exit(-1);
+    }
+
+    if(pid) // I'm parent!
+    {
+        close(pipefd[1]); // close write end, i'll only read
+        outfds[dataId] = mp(pid, pipefd[0]);
+    }
+    else
+    {
+        close(pipefd[0]);
+        dup2(pipefd[1], 0);
+        close(pipefd[1]);
+        solve(dataId);
+        exit(0); // force exit
+    }
+}
+
+
