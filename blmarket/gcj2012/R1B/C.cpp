47e2e8503d020fd8a4a5fba85d6567cc009bda65
Jeong, Heon <blmarket@gmail.com>
2012-05-06 02:44:23 +0900

    --Empty Message--

--- blmarket/gcj2012/R1B/C.cpp
+++ blmarket/gcj2012/R1B/C.cpp
@@ -30,43 +30,42 @@ template<typename T> int size(const T &a) { return a.size(); }
 int n;
 vector<long long> data;
 
-template<typename T> bool perm(vector<long long> &ret, const T &begin, const T &end)
+bool perm(vector<pair<long long, long long> > &ret, int s, int e)
 {
-    int sz = end - begin;
-    cout << end-begin << endl;
-    if(end - begin < 4)
+    int sz = e - s;
+    if(sz < 4)
     {
-        int sz = end - begin;
         ret.clear();
         for(int i=1;i<(1<<sz);i++)
         {
             long long sum = 0;
             for(int j=0;j<sz;j++) if((1<<j) & i)
-                sum += *(begin + j);
-            ret.pb(sum);
+                sum += data[s + j];
+            ret.pb(mp(sum, i << s));
         }
         return false;
     }
 
-    vector<long long> p1,p2;
-    T mid = begin + sz/2;
-    if(perm(p1, begin, mid)) return true;
-    if(perm(p2, mid, end)) return true;
+    vector<pair<long long, long long> > p1,p2;
+    if(perm(p1, s, (s+e)/2)) return true;
+    if(perm(p2, (s+e)/2, e)) return true;
 
     ret.clear();
+
     for(int i=0;i<size(p2);i++) ret.pb(p2[i]);
     for(int i=0;i<size(p1);i++)
     {
         ret.pb(p1[i]);
         for(int j=0;j<size(p2);j++)
         {
-            ret.pb(p1[i] + p2[j]);
+            ret.pb(mp(p1[i].first + p2[j].first, p1[i].second | p2[j].second));
         }
     }
+
     sort(ret.begin(), ret.end());
     for(int i=0;i+1<size(ret);i++)
     {
-        if(ret[i] == ret[i+1])
+        if(ret[i].first == ret[i+1].first)
         {
             cout << "write here" << endl;
             return true;
@@ -89,8 +88,8 @@ void process(int dataId)
 
     // i think 48 bits are enough...
     // 48, 24, 12, 6, 3
-    vector<long long> p1;
-    perm(p1, &data[0], &data[48]);
+    vector<pair<long long, long long> > p1;
+    perm(p1, 0, 48);
 }
 
 int main(void)
