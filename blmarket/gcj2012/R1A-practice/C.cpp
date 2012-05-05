4f9c9e5b217d936421c1fefc0ca6f668a77e65e0
Jeong, Heon <blmarket@gmail.com>
2012-05-05 14:07:52 +0900

    --Empty Message--

--- blmarket/gcj2012/R1A-practice/C.cpp
+++ blmarket/gcj2012/R1A-practice/C.cpp
@@ -117,6 +117,10 @@ int go(int a, long long state)
         {
             if(getbit(mask, cur.pb) && getbit(state, cur.pb) != j) continue;
             if(i == j) continue;
+            if(i) setbit(state, cur.pa); else zerobit(state, cur.pa);
+            if(j) setbit(state, cur.pb); else zerobit(state, cur.pb);
+            int tmp = go(a+1, state);
+            if(ret < tmp) ret = tmp;
         }
     }
 
