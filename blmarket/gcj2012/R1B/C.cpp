72d49a2c1215eb841fa3ea4eb4e5e1d5cdcefac7
Jeong, Heon <blmarket@gmail.com>
2012-05-06 09:47:11 +0900

    --Empty Message--

--- blmarket/gcj2012/R1B/C.cpp
+++ blmarket/gcj2012/R1B/C.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
+#include <bitset>
 #include <sstream>
 #include <numeric>
 #include <iterator>
@@ -31,85 +32,9 @@ template<typename T> int size(const T &a) { return a.size(); }
 int n;
 vector<long long> data;
 
-void out(UI mask)
+template<typename T> bool comp(const T &a, const T &b)
 {
-    for(int i=0;i<32;i++)
-    {
-        if(mask & (1<<i))
-        {
-            cout << data[i] << " ";
-        }
-    }
-}
-
-bool perm(vector<pair<long long, pair<UI,UI> > > &ret, int s, int e)
-{
-    int sz = e - s;
-    if(sz == 1)
-    {
-        ret.clear();
-        ret.pb(mp(data[s], mp(1<<s,0)));
-        return false;
-    }
-
-    vector<pair<long long, pair<UI,UI> > > p1,p2;
-    if(perm(p1, s, (s+e)/2)) return true;
-    if(perm(p2, (s+e)/2, e)) return true;
-
-    sort(p1.begin(), p1.end());
-    sort(p2.begin(), p2.end());
-
-    int j = 0;
-    for(int i=0;i<size(p1);i++)
-    {
-        while(j < size(p2) && p2[j].first < p1[i].first) j++;
-        if(j == size(p2)) break;
-        if(p2[j].first == p1[i].first)
-        {
-            out(p1[i].second.first);
-            out(p2[j].second.second);
-            printf("\n");
-            out(p1[i].second.second);
-            out(p2[j].second.first);
-            printf("\n");
-            return true;
-        }
-    }
-
-    if(sz == 32)
-    {
-        cerr << "we fail" << endl;
-        return false;
-    }
-
-    ret.clear();
-    for(int i=0;i<size(p2);i++)
-        ret.pb(p2[i]);
-    for(int i=0;i<size(p1);i++)
-    {
-        ret.pb(p1[i]);
-        LL k1 = p1[i].second.first;
-        LL k2 = p1[i].second.second;
-        for(int j=0;j<size(p2);j++)
-        {
-            LL k3 = p2[j].second.first;
-            LL k4 = p2[j].second.second;
-
-            ret.pb(mp(p1[i].first + p2[j].first, mp(k1|k3, k2|k4)));
-            if(p1[i].first > p2[j].first)
-            {
-                ret.pb(mp(p1[i].first - p2[j].first, mp(k1|k4, k2|k3)));
-            }
-            else
-            {
-                ret.pb(mp(p2[j].first - p1[i].first, mp(k2|k3, k1|k4)));
-            }
-        }
-    }
-
-    cerr << sz << " " << ret.size() << endl;
-
-    return false;
+    return a.first < b.first;
 }
 
 void process(int dataId)
@@ -120,13 +45,30 @@ void process(int dataId)
     {
         cin >> data[i];
     }
+    sort(data.begin(), data.end());
+
+    typedef vector<pair<long long, pair<bitset<505>, bitset<505> > > > vec_t;
+    
+    vec_t arr, arr2;
 
     printf("\n");
-    vector<pair<long long, pair<UI,UI> > > p1;
+    long long numRange = 1000000000000LL;
+    for(int i=0;i<size(data);i++)
+    {
+        bitset<505> ab, bb;
+        ab.set(i);
+        arr.pb(mp(data[i], mp(ab,bb)));
+        ab.set(i,0);
+    }
+
     while(true)
     {
-        if(perm(p1, 0, 32)) return;
-        random_shuffle(data.begin(), data.end());
+        arr2.clear();
+
+
+        arr.swap(arr2);
+        sort(arr.begin(), arr.end(), comp<vec_t::value_type>);
+        numRange /= 2;
     }
 }
 
