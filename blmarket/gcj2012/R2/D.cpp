539b8441120451b093dd5b8d62296e2695140425
Jeong, Heon <blmarket@gmail.com>
2012-05-27 00:47:52 +0900

    --Empty Message--

--- blmarket/gcj2012/R2/D.cpp
+++ blmarket/gcj2012/R2/D.cpp
@@ -91,8 +91,8 @@ void go(int x,int y)
                     }
                     if(it != reach.end())
                         it->second = 0;
-                    cerr << "hello?" << endl;
                 }
+                cerr << "hello?" << endl;
                 done = true;
                 break;
             }
