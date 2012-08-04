859d35d244c1175f633a16dbf92ad3251e4dc2b5
Jeong, Heon <blmarket@gmail.com>
2012-08-05 01:19:48 +0900

    --Empty Message--

---
+++ blmarket/ColorfulChocolates.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int maxS;
+
+int go(vector<int> a) 
+{
+    if(size(a) == 0) return -1;
+    int ret = 0;
+
+    for(int i=0;i<size(a);i++) {
+        for(int j=i;j<size(a);j++) {
+            int len = (j-i)+1;
+            if(len < ret) continue;
+            int mid = (i+j)/2;
+            int tmp = 0, tmp2 = a[mid] + 1;
+            for(int k=mid+1;k<=j;k++) {
+                tmp += a[k] - tmp2;
+                tmp2++;
+            }
+            tmp2 = a[mid] - 1;
+            for(int k=mid-1;k>=i;k--) {
+                tmp += tmp2 - a[k];
+                tmp2--;
+            }
+
+            if(tmp > maxS) continue;
+            ret = len;
+        }
+    }
+    return ret;
+}
+
+class ColorfulChocolates 
+{
+public:
+    int maximumSpread(string chocolates, int maxSwaps) 
+    {		
+        maxS = maxSwaps;
+        int ret = -1;
+        for(int i=0;i<26;i++) {
+            vector<int> tmp;
+            tmp.clear();
+            for(int j=0;j<size(chocolates);j++) {
+                if(chocolates[j] == 'A' + i) {
+                    tmp.pb(j);
+                }
+            }
+            int tmp2 = go(tmp);
+            if(tmp2 == -1) continue;
+            if(ret < tmp2) ret = tmp2;
+        }
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arg0 = "ABCDCBC"; int Arg1 = 1; int Arg2 = 2; verify_case(0, Arg2, maximumSpread(Arg0, Arg1)); }
+	void test_case_1() { string Arg0 = "ABCDCBC"; int Arg1 = 2; int Arg2 = 3; verify_case(1, Arg2, maximumSpread(Arg0, Arg1)); }
+	void test_case_2() { string Arg0 = "ABBABABBA"; int Arg1 = 3; int Arg2 = 4; verify_case(2, Arg2, maximumSpread(Arg0, Arg1)); }
+	void test_case_3() { string Arg0 = "ABBABABBA"; int Arg1 = 4; int Arg2 = 5; verify_case(3, Arg2, maximumSpread(Arg0, Arg1)); }
+	void test_case_4() { string Arg0 = "QASOKZNHWNFODOQNHGQKGLIHTPJUVGKLHFZTGPDCEKSJYIWFOO"; int Arg1 = 77; int Arg2 = 5; verify_case(4, Arg2, maximumSpread(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    ColorfulChocolates ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
