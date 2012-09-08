6cfa8d76b70852bdd27364e87f06dac9bae12991
Jeong, Heon <blmarket@gmail.com>
2012-09-08 23:05:39 +0900

    --Empty Message--

--- blmarket/TheBrickTowerHardDivOne.cpp
+++ blmarket/TheBrickTowerHardDivOne.cpp
@@ -58,7 +58,8 @@ int * exstate(int *arr) {
     for(int i=0;i<4;i++) {
         if(m.count(arr[i]) == 0) { 
             m[arr[i]] = m.size();
-            cout << arr[i] << " = " << m[arr[i]] << endl;
+            if(arr[0] == 1 && arr[1] == 2 && arr[2] == 2 && arr[3] == 1)
+                cout << arr[i] << " = " << m[arr[i]] << endl;
         }
         tstate[i] = m[arr[i]];
     }
