7dd8b118cd89f3c863398e93d8e63ac761cfae42
Jeong, Heon <blmarket@gmail.com>
2012-09-08 23:16:43 +0900

    --Empty Message--

--- blmarket/TheBrickTowerHardDivOne.cpp
+++ blmarket/TheBrickTowerHardDivOne.cpp
@@ -87,9 +87,12 @@ void gen(int *arr, int pos) {
         int X[] = {0,1,1,0,1,0,0,1};
         bool flag = !memcmp(arr, X, sizeof(X));
 
+        int ee2 = *max_element(arr, arr+4) + 1;
+
+        int p = countPerm(ee) / countPerm(ee2);
+
         int s1 = getstate(arr);
         int s2 = getstate(exstate(arr + 4));
-        int p = countPerm(ee);
         int nSame = countsame(arr + 4);
 
         if(flag) cout << ee << " " << p << endl;
