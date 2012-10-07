de7b831a0ba1cf0337fcdae149f94f7d14605dbd
Jeong, Heon <blmarket@gmail.com>
2012-10-07 16:53:51 +0900

    --Empty Message--

--- blmarket/STable.cpp
+++ blmarket/STable.cpp
@@ -86,6 +86,14 @@ public:
 								}
 						}
 				}
+
+				for(int i=1;i<=size(s);i++) {
+						for(int j=1;j<=size(t);j++) {
+								cout << move[i][j] << " ";
+						}
+						cout << endl;
+				}
+
 				for(int i=0;i<=size(s);i++) cnt[i][0] = 1;
 				for(int i=0;i<=size(t);i++) cnt[0][i] = 1;
 				for(int i=1;i<=size(s);i++) {
