efe5c454dd6c6b7c46188348144b49001428087b
Jeong, Heon <blmarket@gmail.com>
2012-11-16 23:44:33 +0900

    --Empty Message--

--- blmarket/nanacup/square.cpp
+++ blmarket/nanacup/square.cpp
@@ -76,7 +76,7 @@ void bigint::shr() {
 				V[i] /= 2;
 		}
 		V[0] /= 2;
-		if(V.back() == 0) V.pop_back();
+		while(V.back() == 0) V.pop_back();
 }
 
 bigint& bigint::operator+=(const bigint &rhs) {
