899fa4b7e73b959bccaf746803f099ae24e98b85
Jeong, Heon <blmarket@gmail.com>
2012-11-18 10:11:09 +0900

    --Empty Message--

---
+++ blmarket/nanacup/R1/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int n,m;
+vector<string> data;
+vector<string> words;
+
+void input() 
+{
+    scanf(" %d %d", &n, &m);
+    for(int i=0;i<n;i++) {
+        char buffer[1024];
+        scanf(" %s", buffer);
+        data.pb(buffer);
+    }
+    int tmp;
+    scanf("%d", &tmp);
+    for(int i=0;i<tmp;i++) {
+        char buffer[512];
+        scanf(" %s", buffer);
+        words.pb(buffer);
+    }
+}
+
+int main(void)
+{
+    input();
+    cout << size(words) << endl;
+}
