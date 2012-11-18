dcca187d3190f3050ca5f94e77cdbdd4dbcb6835
Jeong, Heon <blmarket@gmail.com>
2012-11-18 10:18:20 +0900

    --Empty Message--

--- blmarket/nanacup/R1/main.cpp
+++ blmarket/nanacup/R1/main.cpp
@@ -24,10 +24,13 @@ typedef pair<int,int> PII;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 bool bySize(const string &lhs, const string &rhs) { return lhs.size() < rhs.size(); }
+const int dx[] = { -1, 0, 0, 1 }, dy[] = { 0, -1, 1, 0 };
 
 int n,m;
 vector<string> data;
 vector<string> words;
+
+bool check(int x, int y) { return (x>=0 && y>=0 && x<n && y<m); }
 
 void input() 
 {
@@ -46,10 +49,35 @@ void input()
     }
 }
 
+bool go(const string &in, int x, int y, int pos) {
+    if(!check(x,y)) return false;
+    if(data[x][y] != in[pos]) return false;
+    
+    for(int i=0;i<4;i++) {
+        if(go(in, x + dx[i], y + dy[i], pos + 1)) return true;
+    }
+    return false;
+}
+
+bool findexact(const string &in) {
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(go(in, i, j, 0)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main(void)
 {
     input();
     sort(words.rbegin(), words.rend(), bySize);
-    for(int i=0;i<size(words);i++) cout << words[i].size() << " ";
-    cout << endl;
+    for(int i=0;i<size(words);i++) {
+        if(findexact(words[i])) {
+            cout << words[i] << endl;
+        }
+    }
+    return 0;
 }
