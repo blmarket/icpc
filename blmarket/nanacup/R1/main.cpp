841f8a5de54700ebb6ca1bc0b612285af3422d3c
Jeong, Heon <blmarket@gmail.com>
2012-11-19 10:36:50 +0900

    Increased timelimit

--- blmarket/nanacup/R1/main.cpp
+++ blmarket/nanacup/R1/main.cpp
@@ -34,7 +34,7 @@ double initGetTime()
     return tv.tv_sec + tv.tv_usec * 0.000001;
 }
 
-double time_limit = 4.9;
+double time_limit = 4.94;
 double time_start = initGetTime();
 
 double getTime()
