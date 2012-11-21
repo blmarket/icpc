c510da0457e4bc62e7ea0bd7e7740fe381f14114
Jeong, Heon <blmarket@gmail.com>
2012-11-21 21:59:46 +0900

    Completed visualizer

--- blmarket/nanacup/R1/visualizer/app.js
+++ blmarket/nanacup/R1/visualizer/app.js
@@ -12,6 +12,17 @@ var dataid = process.argv.pop();
 var infilename = dataid + ".in";
 var resfilename = dataid + ".out.mid";
 
+var board = readinfile(infilename);
+readoutfile(board, resfilename);
+
+console.log(board);
+
+app.get('/', function(req, res) {
+  res.render('index.jade', { board: board });
+});
+
+app.listen(3000);
+
 function readinfile(infilename) {
   var data = fs.readFileSync(infilename, 'utf-8');
 
@@ -25,21 +36,30 @@ function readinfile(infilename) {
   for(var i=1;i<=n;i++) {
     var tmp = [];
     for(var j=0;j<m;j++) {
-      tmp.push(lines[i][j]);
+      tmp.push({ value: lines[i][j] });
     }
     board.push(tmp);
   }
   return board;
 }
 
-var board = readinfile(infilename);
+function readoutfile(board, outfilename) {
+  var data = fs.readFileSync(outfilename, 'utf-8');
 
-app.get('/', function(req, res) {
-  res.render('index.jade', { board: board });
-});
+  var lines = data.split('\n');
+  var n = Number(lines[0]);
 
-function print_table(board) {
+  for(var i=1;i<=n;i++) {
+    var line = lines[i].split(' ');
+    line.pop();
+    for(var j=1;j<line.length;j+=2) {
+      var x = Number(line[j+1]);
+      var y = Number(line[j]);
+      board[x][y].style = line[0] + ' something';
+      board[x][y].group = line[0];
+      if(board[x][y].value != line[0].charAt(j/2)) {
+        board[x][y].style += ' wrong';
+      }
+    }
+  }
 }
-print_table(board);
-
-app.listen(3000);
