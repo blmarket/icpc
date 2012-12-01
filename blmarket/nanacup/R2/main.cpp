a8fea2ddc4843ba2acebf81a167e1ac2472db642
Jeong, Heon <blmarket@gmail.com>
2012-12-01 23:11:21 +0900

    --Empty Message--

--- blmarket/nanacup/R2/main.cpp
+++ blmarket/nanacup/R2/main.cpp
@@ -46,7 +46,7 @@ void go(int a, int b) {
 
 void expansion(int a, int b) {
     memset(visit, 0, sizeof(visit));
-    function<void(int, int)> func = [&](int a, int b) {
+    function<void(int, int)> func = [visit, func](int a, int b) {
         if(visit[a][b]) return;
         func(a,b);
     };
