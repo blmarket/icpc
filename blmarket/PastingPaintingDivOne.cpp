ee01b2d773d697754f0c4ff0890750438002ad0e
Jeong, Heon <blmarket@gmail.com>
2012-12-03 11:00:31 +0900

    --Empty Message--

---
+++ blmarket/PastingPaintingDivOne.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+char tempboard[1000][1000];
+vector<string> board;
+int T;
+
+void paste(int a) {
+    int n = size(board);
+    int m = size(board[0]);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(board[i][j] != '.') tempboard[i+a][j+a] = board[i][j];
+        }
+    }
+}
+
+void cnt(LL &R, LL &G, LL &B) {
+    R=G=B=0;
+    for(int i=0;i<200;i++) {
+        for(int j=0;j<200;j++) {
+            if(tempboard[i][j] == 'R') R++;
+            else if(tempboard[i][j] == 'G') G++;
+            else if(tempboard[i][j] == 'B') B++;
+        }
+    }
+}
+
+class PastingPaintingDivOne 
+{
+public:
+    vector<long long> cntColors(vector <string> clipboard, int T_) 
+    {
+        memset(tempboard, 0, sizeof(tempboard));
+        board = clipboard;
+        T = T_;
+
+        LL R,G,B,pr,pg,pb;
+        LL diffR,diffG,diffB;
+        for(int i=0;i<100;i++) {
+            paste(i);
+            cnt(R,G,B);
+            if(i+1 == T) {
+                vector<LL> ret;
+                ret.pb(R); ret.pb(G); ret.pb(B);
+                return ret;
+            }
+            diffR = R-pr;diffG=G-pg; diffB=B-pb;
+            pr=R;pg=G;pb=B;
+        }
+
+        int elasp = T - 100;
+        R += diffR * elasp;
+        G += diffG * elasp;
+        B += diffB * elasp;
+
+        vector<LL> ret;
+        ret.pb(R); ret.pb(G); ret.pb(B);
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const vector<long long> &Expected, const vector<long long> &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: " << print_array(Expected) << endl; cerr << "\tReceived: " << print_array(Received) << endl; } }
+	void test_case_0() { string Arr0[] = {
+"..G",
+"R..",
+"BG."
+}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; long Arr2[] = {3, 4, 3 }; vector<long long> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); verify_case(0, Arg2, cntColors(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {
+"R...",
+"....",
+"....",
+"...R"
+}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; long Arr2[] = {4, 0, 0 }; vector<long long> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); verify_case(1, Arg2, cntColors(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"RGB"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 100000; long Arr2[] = {100000, 100000, 100000 }; vector<long long> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); verify_case(2, Arg2, cntColors(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {"."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1000000000; long Arr2[] = {0, 0, 0 }; vector<long long> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); verify_case(3, Arg2, cntColors(Arg0, Arg1)); }
+	void test_case_4() { string Arr0[] = {
+"RB.",
+".G."
+}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 100; long Arr2[] = {100, 1, 100 }; vector<long long> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); verify_case(4, Arg2, cntColors(Arg0, Arg1)); }
+	void test_case_5() { string Arr0[] = {
+"..........G..........",
+".........G.G.........",
+"........G...G........",
+".......G.....G.......",
+"......G..B.B..G......",
+".....G...B.B...G.....",
+"....G...........G....",
+"...G...R.....R...G...",
+"..G.....RRRRRR....G..",
+".G..........RR.....G.",
+"GGGGGGGGGGGGGGGGGGGGG"
+}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1000000000; long Arr2[] = {2000000018, 17000000048, 2000000005 }; vector<long long> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); verify_case(5, Arg2, cntColors(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    PastingPaintingDivOne ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
