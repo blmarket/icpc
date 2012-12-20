ed1bf4cbd9501ad158bfa3678c7ae7539dc904bc
Jeong, Heon <blmarket@gmail.com>
2012-12-21 01:29:13 +0900

    --Empty Message--

--- blmarket/TheDivisionGame.cpp
+++ blmarket/TheDivisionGame.cpp
@@ -27,8 +27,9 @@ class TheDivisionGame
 public:
     long long countWinningIntervals(int L, int R) 
     {
+        primes.clear();
         primes.pb(2);
-        for(int i=3;i<1010000;i+=2) {
+        for(int i=3;i<10100000;i+=2) {
             bool fail = false;
             for(int j=0;j<size(primes);j++) {
                 if(primes[j] * primes[j] > i) break;
