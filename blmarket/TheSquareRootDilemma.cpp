0b95355d10d06e806f9f70415d10bdef3ab3d29f
Jeong, Heon <blmarket@gmail.com>
2013-01-21 21:23:44 +0900

    --Empty Message--

--- blmarket/TheSquareRootDilemma.cpp
+++ blmarket/TheSquareRootDilemma.cpp
@@ -52,7 +52,7 @@ public:
             }
         }
 
-        if(false) {
+        if(true) {
             int ret2 = 0;
             for(int i=1;i<=N;i++) {
                 for(int j=1;j<=M;j++) {
