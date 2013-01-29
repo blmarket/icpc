11f6c23e0196d1d4ba79f60455a4a9c2e33c7048
Jeong, Heon <blmarket@gmail.com>
2013-01-30 01:21:48 +0900

    --Empty Message--

---
+++ blmarket/BallsSeparating.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <functional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int sum(vector<int> &v) {
+    int ret = 0;
+    for(int i=0;i<size(v);i++) {
+        ret += v[i];
+    }
+    return ret;
+}
+
+int n;
+vector<int> red, green, blue;
+int reds, greens, blues;
+
+int chk(int a, int b, int c) {
+    if(reds == 0) a = -1;
+    if(greens == 0) b = -2;
+    if(blues == 0) c = -3;
+
+    int ret = 0;
+    for(int i=0;i<n;i++) {
+        if(i == a) {
+            ret += green[i] + blue[i];
+            continue;
+        }
+        if(i == b) {
+            ret += red[i] + blue[i];
+            continue;
+        }
+        if(i == c) {
+            ret += red[i] + green[i];
+            continue;
+        }
+
+        int mini = max(max(red[i], green[i]), blue[i]);
+        ret += red[i] + green[i] + blue[i] - mini;
+    }
+
+    return ret;
+}
+
+class BallsSeparating 
+{
+public:
+    int minOperations(vector <int> red_, vector <int> green_, vector <int> blue_) 
+    {		
+        red = red_; green = green_; blue = blue_;
+        n = size(red);
+        if(sum(red) == 0) {
+            if(sum(blue) == 0) return 0;
+            swap(red, blue);
+        } else {
+            if(sum(green) == 0) {
+                if(sum(blue) == 0) return 0;
+                swap(green, blue);
+            }
+        }
+
+        reds = sum(red);
+        greens = sum(green);
+        blues = sum(blue);
+
+        int ret = -1;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                for(int k=0;k<n;k++) {
+                    int tmp = chk(i,j,k);
+                    if(tmp == -1) continue;
+                    if(ret == -1 || ret > tmp) ret = tmp;
+                }
+            }
+        }
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1, 1, 1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {1, 1, 1}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arg3 = 6; verify_case(0, Arg3, minOperations(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arr0[] = {5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {8}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arg3 = -1; verify_case(1, Arg3, minOperations(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arr0[] = {4, 6, 5, 7}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {7, 4, 6, 3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {6, 5, 3, 8}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arg3 = 37; verify_case(2, Arg3, minOperations(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arr0[] = {7, 12, 9, 9, 7}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {7, 10, 8, 8, 9}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {8, 9, 5, 6, 13}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arg3 = 77; verify_case(3, Arg3, minOperations(Arg0, Arg1, Arg2)); }
+	void test_case_4() { int Arr0[] = {842398, 491273, 958925, 849859, 771363, 67803, 184892, 391907, 256150, 75799}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {268944, 342402, 894352, 228640, 903885, 908656, 414271, 292588, 852057, 889141}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {662939, 340220, 600081, 390298, 376707, 372199, 435097, 40266, 145590, 505103}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arg3 = 7230607; verify_case(4, Arg3, minOperations(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    BallsSeparating ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
