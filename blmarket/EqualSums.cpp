99db921607cc86a593798eade6cb6245129d9210
Jeong, Heon <blmarket@gmail.com>
2013-01-30 01:41:18 +0900

    --Empty Message--

---
+++ blmarket/EqualSums.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int n;
+vector<string> board;
+
+class EqualSums 
+{
+public:
+    int count(vector <string> board_) 
+    {
+        board = board_;
+        n = size(board);
+        for(int i=0;i<n;i++) for(int j=0;j<n;j++) if(board[i][j] != '-') board[i][j] -= '0';
+
+        while(true) {
+            bool found = false;
+            for(int i=0;i<n;i++) for(int j=i+1;j<n;j++) {
+                for(int k=0;k<n;k++) for(int l=k+1;l<n;l++) {
+                    int nm = 0;
+                    if(board[i][k] == '-') nm++;
+                    if(board[i][l] == '-') nm++;
+                    if(board[j][k] == '-') nm++;
+                    if(board[j][l] == '-') nm++;
+
+                    if(nm == 0) {
+                        if(board[i][k] + board[j][l] != board[i][l] + board[j][k]) return 0;
+                    }
+                    if(nm == 1) {
+                        int s1 = board[i][k] + board[j][l];
+                        int s2 = board[i][l] + board[j][k];
+                        if(board[i][k] == '-') board[i][k] = s2 - board[j][l];
+                        if(board[j][l] == '-') board[j][l] = s2 - board[i][k];
+                        if(board[i][l] == '-') board[i][l] = s1 - board[j][k];
+                        if(board[j][k] == '-') board[j][k] = s1 - board[i][l];
+                        found = true;
+                    }
+                }
+            }
+            if(!found) break;
+        }
+        return 1;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); if ((Case == -1) || (Case == 6)) test_case_6(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"1-",
+ "-2"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(0, Arg1, count(Arg0)); }
+	void test_case_1() { string Arr0[] = {"123",
+ "4--",
+ "--9"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(1, Arg1, count(Arg0)); }
+	void test_case_2() { string Arr0[] = {"9--",
+ "-9-",
+ "--9"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 271; verify_case(2, Arg1, count(Arg0)); }
+	void test_case_3() { string Arr0[] = {"11",
+ "12"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(3, Arg1, count(Arg0)); }
+	void test_case_4() { string Arr0[] = {"-4--",
+ "-0-2",
+ "--1-",
+ "4---"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 10; verify_case(4, Arg1, count(Arg0)); }
+	void test_case_5() { string Arr0[] = {"--2",
+ "02-",
+ "-10"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(5, Arg1, count(Arg0)); }
+	void test_case_6() { string Arr0[] = {"----------1---------------0-----7-----------------",
+ "-----4--------------------------------------------",
+ "--------5-------------5-3---5---------------6-----",
+ "-------2----------1-------------------------------",
+ "-----4--------------------------------------------",
+ "-----3--------------------------------------------",
+ "-6----------5-------------------------------------",
+ "-------------------------------7---5----------6---",
+ "--------6-------------6-4---6---------------7-----",
+ "-------------4----------------5-------------------",
+ "3------------------------------------------6------",
+ "3------------------------------------------6------",
+ "-------------4----------------5-------------------",
+ "---------------2-------------------------3--------",
+ "--2------------------------------------------2----",
+ "---8---------------1-------------------3----------",
+ "---------------3----------------------------------",
+ "----7----------------5---0-----------------------5",
+ "----------------5-------------------------3-----1-",
+ "----------1---------------0-----7-----------------",
+ "-------------5----------------6-------------------",
+ "-7----------6-------------------------------------",
+ "---8---------------1-------------------3----------",
+ "-----------------------3--------------------------",
+ "----8----------------6---1-----------------------6",
+ "------------------------------------------4-----2-",
+ "-----------5---------------5----------------------",
+ "-----------------------------6--------------------",
+ "----8----------------6---1-----------------------6",
+ "----------------5-------------------------3-----1-",
+ "-------------------------------6---4--2-------5---",
+ "-6----------5-------------------------------------",
+ "--------5-------------5-3---5---------------6-----",
+ "-------------5----------------6-------------------",
+ "-----3--------------------------------------------",
+ "---------------2-------------------------3--------",
+ "---------4---------------------------6------------",
+ "-------------------------------6---4--2-------5---",
+ "------2-------------1------------22---------------",
+ "--------5-------------5-3---5---------------6-----",
+ "-----------5--3------------5----------------------",
+ "--2------------------------------------------2----",
+ "---------5---------------------------7------------",
+ "-------------4----------------5-------------------",
+ "-----------------5------------------4---6------2--",
+ "-------------------------------6---4--2-------5---",
+ "-----------------------2--------------------------",
+ "----------------6-------------------------4-----2-", 
+ "-------------------------------6---4--2-------5---",
+ "--------5-------------5-3---5---------------6-----"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 45094393; verify_case(6, Arg1, count(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    EqualSums ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
