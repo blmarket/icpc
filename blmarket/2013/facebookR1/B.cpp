dca0c6793f2883bcba3a493e8aaf843e015f6b50
Jeong, Heon <blmarket@gmail.com>
2013-02-03 11:38:37 +0900

    --Empty Message--

--- blmarket/2013/facebookR1/B.cpp
+++ blmarket/2013/facebookR1/B.cpp
@@ -27,13 +27,44 @@ template<typename T> int size(const T &a) { return a.size(); }
 int m,l;
 string k1, k2;
 string block1[105], block2[105];
+bool links[105][105];
+
+bool matches(const string &a, const string &b) {
+    for(int i=0;i<l;i++) {
+        if(a[i] == '?' || b[i] == '?') continue;
+        if(a[i] != b[i]) return false;
+    }
+    return true;
+}
 
 void assign(const string &a, string target[]) {
     for(int i=0;i<m;i++) {
         target[i] = a.substr(i*l, l);
-        cout << target[i] << " ";
     }
-    cout << endl;
+}
+
+bool visit[105];
+int back[105];
+
+bool check_flow(int a) {
+    if(visit[a]) return false;
+    for(int i=0;i<m;i++) if(links[a][i]) {
+        if(back[i] == -1 || check_flow(back[i])) {
+            back[i] = a;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool try_flow() {
+    memset(back, -1, sizeof(back));
+    for(int i=0;i<m;i++) {
+        memset(visit, 0, sizeof(visit));
+        if(check_flow(i) == false) return false;
+    }
+    return true;
 }
 
 void process(void) {
@@ -46,6 +77,17 @@ void process(void) {
     k2 = buff;
     assign(k1, block1);
     assign(k2, block2);
+
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<m;j++) {
+            links[i][j] = matches(block1[i], block2[j]);
+        }
+    }
+
+    if(try_flow() == false) {
+        printf("IMPOSSIBLE\n");
+        return;
+    }
 }
 
 int main(void)
