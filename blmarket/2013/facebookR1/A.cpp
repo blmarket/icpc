99582bf35be952053db432625b2b92a35eb31a25
Jeong, Heon <blmarket@gmail.com>
2013-02-03 11:22:03 +0900

    --Empty Message--

--- blmarket/2013/facebookR1/A.cpp
+++ blmarket/2013/facebookR1/A.cpp
@@ -69,7 +69,7 @@ void process(void) {
         ret += tmp;
         cout << (cc1 * cc2) % mod << " ";
 
-        cc1 *= (n-k+1-i);
+        cc1 *= (n-k-i);
         cc1 *= inverse[n-1-i];
         cc1 %= mod;
     }
