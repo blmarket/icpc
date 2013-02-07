b245d411394d6d8c417adf8caf9dd217d1f7db3e
Jeong, Heon <heon.jeong@sk.com>
2013-02-07 12:59:15 +0900

    --Empty Message--

--- blmarket/TheJediTest.cpp
+++ blmarket/TheJediTest.cpp
@@ -56,7 +56,7 @@ int go(int a, int b) {
             value = elasp;
         }
     }
-    if(a == 1 && b == 309) {
+    if(a == 2 && b == 525) {
         cout << ret << " : " << value << endl;
     }
     return memo[key] = ret;
