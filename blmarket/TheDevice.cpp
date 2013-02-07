c18e8557c9642e4d90f5677b38c818fc4970d457
Jeong, Heon <heon.jeong@sk.com>
2013-02-07 11:17:33 +0900

    --Empty Message--

---
+++ blmarket/TheDevice.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int flag[55];
+
+class TheDevice 
+{
+public:
+    int minimumAdditional(vector <string> plates) 
+    {		
+        memset(flag, 0, sizeof(flag));
+        int n = size(plates);
+        int m = size(plates[0]);
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                string &a = plates[i];
+                string &b = plates[j];
+                for(int k=0;k<size(a);k++) {
+                    int t1 = a[k] - '0';
+                    int t2 = b[k] - '0';
+                    int tmp = 1 << (t1 * 2 + t2);
+                    flag[k] |= tmp;
+                }
+            }
+        }
+        for(int i=0;i<m;i++) {
+            cout << flag[i] << " ";
+        }
+        cout << endl;
+        return 0;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"010",
+ "011",
+ "101"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(0, Arg1, minimumAdditional(Arg0)); }
+	void test_case_1() { string Arr0[] = {"0",
+ "1",
+ "0",
+ "1"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(1, Arg1, minimumAdditional(Arg0)); }
+	void test_case_2() { string Arr0[] = {"01010101",
+ "10101010"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(2, Arg1, minimumAdditional(Arg0)); }
+	void test_case_3() { string Arr0[] = {"10010101011",
+ "00010101001",
+ "00100010111",
+ "00101010101",
+ "01010111101"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(3, Arg1, minimumAdditional(Arg0)); }
+	void test_case_4() { string Arr0[] = {"1101001011010",
+ "0010000010101",
+ "1010101011110",
+ "1101010100111",
+ "1011111110111"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(4, Arg1, minimumAdditional(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    TheDevice ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
