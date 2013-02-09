bd37d4c304be9c2868b58d1c5e9ed4e80bee60c8
Jeong, Heon <blmarket@gmail.com>
2013-02-10 06:51:19 +0900

    --Empty Message--

---
+++ blmarket/2013/facebookR2/C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int N;
+PII rela[1005];
+
+void process() 
+{
+    scanf("%d", &N);
+    for(int i=0;i+1<N;i++) {
+        char flag;
+        scanf("%d %c %d", &rela[i].first, &flag, &rela[i].second);
+        if(flag == '>') {
+            swap(rela[i].first, rela[i].second);
+        }
+        cout << rela[i].first << "," << rela[i].second << endl;
+    }
+}
+
+int main(void)
+{
+    int T;
+    scanf("%d",&T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ",i);
+        process();
+    }
+}
