b2261ec8fa9217ae2a9cfb6f249ebb6077f99db7
Jeong, Heon <blmarket@gmail.com>
2013-02-10 06:29:24 +0900

    --Empty Message--

---
+++ blmarket/2013/facebookR2/B.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+long long N,K,P;
+
+void process() 
+{
+    cin >> N >> K >> P;
+    if(K >= N) {
+        cout << 1 << endl;
+        return;
+    }
+
+    LL tmp = N % K;
+    LL round = (N + K - 1) / K;
+    // in here, they always vote so they will not vote for tmp + K turn.
+    LL votes = 0;
+    LL winround = round;
+
+    cerr << tmp << ":" << votes << " ";
+
+    for(LL i=tmp+K;i!=N;i+=K) {
+        round--;
+        LL tmp2 = tmp + K;
+        votes += K;
+
+        if(votes * 100 >= tmp2 * P) { // will win
+            winround = round;
+            votes = 0;
+        } 
+    }
+    cout << winround << endl;
+}
+
+int main(void)
+{
+    int T;
+    scanf("%d",&T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ",i);
+        process();
+    }
+}
