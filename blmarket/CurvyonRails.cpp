214818b2ed87c31c98f66a5819ad68b973ec9642
Jeong, Heon <heon.jeong@sk.com>
2013-02-15 14:55:59 +0900

    --Empty Message--

--- blmarket/CurvyonRails.cpp
+++ blmarket/CurvyonRails.cpp
@@ -71,7 +71,7 @@ public:
         cur[0] = 0;
         for(int i=0;i<size(field);i++) {
             move(field[i]);
-            cout << "----------" << endl;
+            cout << i << " : " << size(cur) << endl;
         }
         if(cur.count(0) == 0) return -1;
         return cur[0];
