84471102ceba773ecba874bc5d2b220944a8fa94
Jeong, Heon <blmarket@gmail.com>
2013-02-17 07:44:53 +0900

    --Empty Message--

--- blmarket/2013/facebookR3/C.cpp
+++ blmarket/2013/facebookR3/C.cpp
@@ -113,10 +113,7 @@ LL go(int cut) {
 void process(void) {
     input();
 
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) cerr << R[i][j] << " ";
-        cerr << endl;
-    }
+    cout << "input done" << endl;
 
     sort(values.begin(), values.end());
     values.erase(unique(values.begin(), values.end()), values.end());
