8c44b603d6a5da8fbe297ea74c5e203ffeec60af
Jeong, Heon <blmarket@gmail.com>
2013-02-20 01:45:22 +0900

    --Empty Message--

--- blmarket/rgen.cpp
+++ blmarket/rgen.cpp
@@ -16,7 +16,7 @@ int main(void) {
 
     for(int i=0;i<50;i++) mapp[i][i] = 'N';
 
-    for(int i=0;i<80;i++) {
+    for(int i=0;i<40;i++) {
         int x = rand() % 50;
         int y = rand() % 50;
         mapp[x][y] = mapp[y][x] = 'N';
