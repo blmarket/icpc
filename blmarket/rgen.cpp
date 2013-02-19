0683f2d376414fb0b7f611a9bb5b0bb9cbac13f4
Jeong, Heon <blmarket@gmail.com>
2013-02-20 01:34:07 +0900

    --Empty Message--

--- blmarket/rgen.cpp
+++ blmarket/rgen.cpp
@@ -1,11 +1,27 @@
 #include <cstdlib>
+#include <vector>
+#include <string>
 #include <iostream>
 
 using namespace std;
+
+vector<string> mapp(50, string(50, 'Y'));
 
 int main(void) {
     cout << "{";
     for(int i=0;i<50;i++) {
         cout << (rand() % 90000) + 10000 << ",";
     }
+    cout << endl;
+
+    for(int i=0;i<10;i++) {
+        int x = rand() % 50;
+        int y = rand() % 50;
+        mapp[x][y] = mapp[y][x] = 'N';
+    }
+
+    for(int i=0;i<50;i++) {
+        cout << mapp[i] << ",";
+    }
+    cout << endl;
 }
