5c5d76616228b8f94e85f77631837dbb055ba934
Jeong, Heon <heon.jeong@sk.com>
2013-02-27 09:56:39 +0900

    --Empty Message--

---
+++ blmarket/DirectionBoard.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+const int dx[] = {-1, 0,0, 1};
+const int dy[] = {0,-1,1,0};
+
+int N,M;
+int intake[20][20];
+
+int R(int x) { return (x + N) % N; }
+int C(int y) { return (y + M) % M; }
+
+class DirectionBoard 
+{
+public:
+    int getMinimum(vector <string> board) 
+    {		
+        N = size(board); M = size(board[0]);
+        memset(intake, 0, sizeof(intake));
+
+        for(int i=0;i<N;i++) for(int j=0;j<M;j++) {
+            if(board[i][j] == 'U') board[i][j] = 0;
+            else if(board[i][j] == 'L') board[i][j] = 1;
+            else if(board[i][j] == 'R') board[i][j] = 2;
+            else board[i][j] = 3;
+
+            int d = board[i][j];
+            int nx = R(i + dx[d]), ny = C(j + dy[d]);
+            intake[nx][ny]++;
+        }
+
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<M;j++) cout << intake[i][j] << " ";
+            cout << endl;
+        }
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"RRRD",
+ "URDD",
+ "UULD",
+ "ULLL"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(0, Arg1, getMinimum(Arg0)); }
+	void test_case_1() { string Arr0[] = {"RRRD",
+ "URLL",
+ "LRRR"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; verify_case(1, Arg1, getMinimum(Arg0)); }
+	void test_case_2() { string Arr0[] = {"RRD",
+ "URD",
+ "ULL"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; verify_case(2, Arg1, getMinimum(Arg0)); }
+	void test_case_3() { string Arr0[] = {"ULRLRD",
+ "UDDLRR"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(3, Arg1, getMinimum(Arg0)); }
+	void test_case_4() { string Arr0[] = {"UDLRLDLD",
+ "DLDLLDLR",
+ "LLLLLDLD",
+ "UUURRRDD"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 9; verify_case(4, Arg1, getMinimum(Arg0)); }
+	void test_case_5() { string Arr0[] = {"UDUDUUDUDUDUDUR",
+ "LLLLDUUDRDLUDRU",
+ "DLLDLDURDURUDDL",
+ "UDUDUUDUDUDUDUR",
+ "LLLLDUUDRDLUDRU",
+ "DLLDLDURDURUDDL",
+ "UDUDUUDUDUDUDUR",
+ "LLLLDUUUDDLUDRU",
+ "DLLDLDURDURUDDL",
+ "UDUDUUDUDUDUDUR",
+ "LLLLDUUDRDLUDRU",
+ "DLLDLDURDURUDDL",
+ "UDUDUUDUDUDUDUR",
+ "LLLLDUUDRDLUDRU",
+ "RRRDLDURDURUDDR"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 73; verify_case(5, Arg1, getMinimum(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    DirectionBoard ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
