e430b53c522710a3ef129db428190cf1ffd1069b
Jeong, Heon <blmarket@gmail.com>
2013-03-06 22:33:40 +0900

    --Empty Message--

--- blmarket/NewArenaPassword.cpp
+++ blmarket/NewArenaPassword.cpp
@@ -54,7 +54,7 @@ public:
 	void test_case_1() { string Arg0 = "puyopuyo"; int Arg1 = 4; int Arg2 = 0; verify_case(1, Arg2, minChange(Arg0, Arg1)); }
 	void test_case_2() { string Arg0 = "loool"; int Arg1 = 3; int Arg2 = 1; verify_case(2, Arg2, minChange(Arg0, Arg1)); }
 	void test_case_3() { string Arg0 = "arena"; int Arg1 = 5; int Arg2 = 0; verify_case(3, Arg2, minChange(Arg0, Arg1)); }
-	void test_case_4() { string Arg0 = "amavckdkz"; int Arg1 = 7; int Arg2 = 5; verify_case(4, Arg2, minChange(Arg0, Arg1)); }
+	void test_case_4() { string Arg0 = "bacbacbabacsbabacbabasbabacbabacbacbababcabaababcb"; int Arg1 = 49; int Arg2 = 5; verify_case(4, Arg2, minChange(Arg0, Arg1)); }
 
 // END CUT HERE
 
