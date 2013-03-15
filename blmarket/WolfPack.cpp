d93dcd23078397342af920ccc0c1e2ca2b3e5281
Jeong, Heon <blmarket@gmail.com>
2013-03-15 10:48:07 +0900

    --Empty Message--

---
+++ blmarket/WolfPack.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+const int dx[] = {-1,0,0,1};
+const int dy[] = {0,-1,1,0};
+
+vector<int> x,y;
+int n,m;
+
+int pari(int a, int b) {
+    int ret = (a+b) % 2;
+    ret = (ret+2)%2;
+    return ret;
+}
+
+void move(int a, int b) {
+    while(true) {
+        bool dir[4] = {true, true, true, true};
+        bool havesome = false;
+        for(int i=0;i<n;i++) {
+            int d1 = x[i] - a;
+            int d2 = y[i] - b;
+            if(abs(d1) + abs(d2) <= m) continue;
+
+            havesome = true;
+            if(d1 > 0) dir[0] = false;
+            if(d1 < 0) dir[3] = false;
+            if(d2 > 0) dir[1] = false;
+            if(d2 < 0) dir[2] = false;
+        }
+
+        if(!havesome) {
+            cout << a << " " << b << endl;
+            return;
+        }
+
+        for(int i=0;i<4;i++) {
+            if(dir[i]) {
+                a += dx[i];
+                b += dy[i];
+                break;
+            }
+        }
+    }
+}
+
+class WolfPack 
+{
+public:
+    int calc(vector <int> x_, vector <int> y_, int m_) 
+    {
+        x=x_; y=y_; m=m_; n = size(x);
+
+        int parity = (x[0] + y[0]) % 2;
+        parity = (parity + 2) % 2;
+        for(int i=1;i<size(x);i++) {
+            if(pari(x[i], y[i]) != parity) return 0;
+        }
+
+        if((m % 2) == parity) move(0, 0);
+        else move(0, 1);
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {3,5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {0,0}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1; int Arg3 = 1; verify_case(0, Arg3, calc(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arr0[] = {0,0}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {0,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1; int Arg3 = 0; verify_case(1, Arg3, calc(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arr0[] = {0,2,4}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {0,0,0}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; int Arg3 = 4; verify_case(2, Arg3, calc(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arr0[] = {7,8}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {8,7}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1; int Arg3 = 2; verify_case(3, Arg3, calc(Arg0, Arg1, Arg2)); }
+	void test_case_4() { int Arr0[] = {-2,-2,-2,0,0,0,2,2,2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {-2,0,2,-2,0,2,-2,0,2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1000; int Arg3 = 387540818; verify_case(4, Arg3, calc(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    WolfPack ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
