e22547ea685c9e5afc08c7fc11215c9b990f6545
Jeong, Heon <heon.jeong@sk.com>
2013-03-29 16:43:13 +0900

    --Empty Message--

--- blmarket/TheArray.cpp
+++ blmarket/TheArray.cpp
@@ -31,7 +31,7 @@ public:
         long long e = 3000000000LL;
         while(s < e) {
             cout << s << " " << e << endl;
-            long long m = (s+e+1) / 2;
+            long long m = (s+e) / 2;
             bool check = false;
 
             long long d1 = (abs(m - first) + d - 1) / d;
