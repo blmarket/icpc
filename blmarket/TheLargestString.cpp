c2a64da9918c16d4f958dc5a09c7b22660b81fdc
Jeong, Heon <blmarket@gmail.com>
2013-04-01 01:29:03 +0900

    --Empty Message--

--- blmarket/TheLargestString.cpp
+++ blmarket/TheLargestString.cpp
@@ -61,7 +61,7 @@ public:
             maxc = findmax(s);
 
             string ret = s1 + t1;
-            if(maxc <= t[0]) return ret; // TODO: FIX
+            if(maxc < t[0]) return ret; // TODO: FIX
 
             for(int i=0;i<size(s);i++) {
                 if(s[i] == maxc) {
