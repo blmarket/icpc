b03cc3767568d2dbde418b6bdcd493f0760facda
Jeong, Heon <blmarket@gmail.com>
2013-04-07 01:24:06 +0900

    --Empty Message--

--- blmarket/TheNumberGameDivOne.cpp
+++ blmarket/TheNumberGameDivOne.cpp
@@ -44,8 +44,7 @@ public:
         }
 
         bool flag[10000] = {0};
-        flag[1] = false;
-        for(int i=2;i<100;i++) {
+        for(int i=1;i<100;i++) {
             for(int j=2;j<i;j++) if((i % j) == 0) {
                 if(flag[i/j] == false) {
                     flag[i] = true;
