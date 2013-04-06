06bcf2c995b3ad4ea15c63df028ebbbf9592604e
Jeong, Heon <blmarket@gmail.com>
2013-04-07 01:20:53 +0900

    --Empty Message--

--- blmarket/TheNumberGameDivOne.cpp
+++ blmarket/TheNumberGameDivOne.cpp
@@ -44,9 +44,9 @@ public:
         }
         cout << size(primes) << endl;
 
-        bool flag[100] = {0};
+        bool flag[10000] = {0};
         flag[1] = false;
-        for(int i=2;i<100;i++) {
+        for(int i=2;i<10000;i++) {
             for(int j=2;j<i;j++) if((i % j) == 0) {
                 if(flag[i/j] == false) {
                     flag[i] = true;
