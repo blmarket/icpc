e1ae46e84506c4ac75d4912114cb43aa254e4ab9
Jeong, Heon <blmarket@gmail.com>
2013-04-07 01:27:56 +0900

    --Empty Message--

--- blmarket/TheNumberGameDivOne.cpp
+++ blmarket/TheNumberGameDivOne.cpp
@@ -52,8 +52,8 @@ public:
                     break;
                 }
             }
+            cout << flag[i] << " ";
         }
-        cout << endl << flag[128] << endl;
 
         return "";
     }
