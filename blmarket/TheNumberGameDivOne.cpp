cbc9b8c71e0d98f5ae4a99fc3d3cba5c4ec33c5d
Jeong, Heon <blmarket@gmail.com>
2013-04-07 01:17:21 +0900

    --Empty Message--

--- blmarket/TheNumberGameDivOne.cpp
+++ blmarket/TheNumberGameDivOne.cpp
@@ -27,6 +27,7 @@ class TheNumberGameDivOne
 public:
     string find(long long n) 
     {		
+        primes.clear();
         for(int i=2;i<=10000;i+=2) {
             bool fail = false;
             for(int j=0;j<size(primes);j++) {
