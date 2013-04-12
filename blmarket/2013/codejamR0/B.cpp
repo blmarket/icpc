0957638bdb1a56beca18bb7cc15f397d7c2372b5
Jeong, Heon <blmarket@gmail.com>
2013-04-13 08:24:24 +0900

    --Empty Message--

--- blmarket/2013/codejamR0/B.cpp
+++ blmarket/2013/codejamR0/B.cpp
@@ -41,7 +41,7 @@ bool chkgood(const string &a) {
 }
 
 void precalc() {
-    chkgood("11");
+    chkgood("12");
     /*
     for(long long i=1;i<=10000000;i++) {
         if(chkpalin(toString(i)) == false) continue;
