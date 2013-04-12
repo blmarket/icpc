3602c04cda64c5b1a542ede3bfe007614123ea50
Jeong, Heon <blmarket@gmail.com>
2013-04-13 08:09:13 +0900

    --Empty Message--

--- blmarket/2013/codejamR0/A.cpp
+++ blmarket/2013/codejamR0/A.cpp
@@ -35,14 +35,16 @@ char acc(char a, char b) {
 char process(void) {
     for(int i=0;i<4;i++) {
         scanf(" %s", board[i]);
+        cout << board[i] << endl;
     }
+
+
     bool hasdot = false;
     for(int i=0;i<4;i++) for(int j=0;j<4;j++) if(board[i][j] == '.') hasdot = true;
     for(int i=0;i<4;i++) {
         char t1 = board[i][0];
         char t2 = board[0][i];
         for(int j=1;j<4;j++) {
-            cout << t1 << " " << " ";
             t1 = acc(t1, board[i][j]);
             t2 = acc(t2, board[j][i]);
         }
