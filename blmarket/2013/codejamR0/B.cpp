0bf09b94ef74756f7d44be348bc04cc32d4d01eb
Jeong, Heon <blmarket@gmail.com>
2013-04-13 08:54:07 +0900

    --Empty Message--

--- blmarket/2013/codejamR0/B.cpp
+++ blmarket/2013/codejamR0/B.cpp
@@ -45,7 +45,6 @@ bool comp(const string &a, const string &b) {
 int ret;
 string A,B;
 vector<string> bigV;
-bool putflag = true;
 void candidate(string candi) {
     string tmp(size(candi)*2-1, '0');
     for(int i=0;i<size(candi);i++) candi[i] -= '0';
@@ -54,12 +53,7 @@ void candidate(string candi) {
             tmp[i+j] += candi[i] * candi[j];
         }
     }
-    if(putflag) bigV.pb(tmp);
-
-    if(comp(tmp, A)) return;
-    if(comp(B, tmp)) return;
-    cout << tmp << " ";
-    ret++;
+    bigV.pb(tmp);
 }
 
 bool chkgood(const string &a) {
@@ -84,22 +78,8 @@ bool chkgood(const string &a) {
     return ret;
 }
 
-void precalc() {
-    for(int i=1;i<=100000;i++) {
-        chkgood(toString(i));
-    }
-    cout << endl << endl;
-
-    for(long long i=1;i<=100000;i++) {
-        if(chkpalin(toString(i)) == false) continue;
-        if(chkpalin(toString(i*i))) {
-            cout << i << " : " << i*i << endl;
-        }
-    }
-}
-
 void go(string &tmp) {
-    if(size(tmp) > 52) return;
+    if(size(tmp) > 12) return;
     for(int i=0;i<2;i++) {
         tmp += (char)(i + '0');
         if(chkgood(tmp)) {
@@ -112,19 +92,31 @@ void go(string &tmp) {
 void process(void) {
     char a[200], b[200];
     scanf(" %s %s", a, b);
-    A = a; B = b; ret = 0;
+    A = a; B = b; 
+    
+    int ret = 0;
+    for(int i=0;i<size(bigV);i++) {
+        const string &tmp = bigV[i];
+        if(comp(tmp, A)) continue;
+        if(comp(B, tmp)) continue;
+        ret++;
+    }
 
+    printf("%d\n", ret);
+}
+
+void precalc() {
     chkgood("1"); chkgood("2"); chkgood("3");
     string tmp = "1";
     go(tmp); 
     tmp = "2";
     go(tmp);
-
-    printf("%d\n", ret);
+    sort(bigV.begin(), bigV.end(), comp);
 }
 
 int main(void)
 {
+    precalc();
     int T;
     scanf("%d", &T);
     for(int i=1;i<=T;i++) {
@@ -132,8 +124,4 @@ int main(void)
         process();
         break;
     }
-    cout << bigV.size() << endl;
-    sort(bigV.begin(), bigV.end(), comp);
-    for(int i=0;i<size(bigV);i++)
-        cout << bigV[i] << endl;
 }
