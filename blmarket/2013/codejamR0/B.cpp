35f457f341b89a0c98323f51158bbbb7d78738d3
Jeong, Heon <blmarket@gmail.com>
2013-04-13 08:45:58 +0900

    --Empty Message--

--- blmarket/2013/codejamR0/B.cpp
+++ blmarket/2013/codejamR0/B.cpp
@@ -46,14 +46,12 @@ int ret;
 string A,B;
 void candidate(string candi) {
     string tmp(size(candi)*2-1, '0');
-    cout << candi << " ";
     for(int i=0;i<size(candi);i++) candi[i] -= '0';
     for(int i=0;i<size(candi);i++) {
         for(int j=0;j<size(candi);j++) {
             tmp[i+j] += candi[i] * candi[j];
         }
     }
-    cout << tmp << endl;
 
     if(comp(tmp, A)) return;
     if(comp(B, tmp)) return;
