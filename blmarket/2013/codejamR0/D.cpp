e240e0b70e7026de06d0ee5d1076d9a385c56e18
Jeong, Heon <blmarket@gmail.com>
2013-04-13 09:14:24 +0900

    --Empty Message--

---
+++ blmarket/2013/codejamR0/D.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+map<set<int>, bool> memo;
+
+int types[205];
+vector<int> inside[205];
+int N;
+
+int keys[205];
+bool go(set<int> &cur) {
+    if(memo.count(cur)) return memo[cur];
+
+    for(int i=0;i<N;i++) if(cur.count(i) == 0) {
+        if(keys[types[i]]) {
+            keys[types[i]]--;
+            cur.insert(i);
+            bool tmp = go(cur);
+            if(tmp) {
+                cout << i << " ";
+                return true;
+            }
+            cur.erase(i);
+            keys[types[i]]++;
+        }
+    }
+    return false;
+}
+
+void process(void) {
+    memo.clear();
+    int ik;
+    scanf("%d %d", &ik, &N);
+    memset(keys, 0, sizeof(keys));
+    for(int i=0;i<ik;i++) {
+        int tmp;
+        scanf("%d", &tmp);
+        ++keys[tmp];
+    }
+
+    for(int i=0;i<N;i++) {
+        int tmp;
+        scanf("%d %d", &types[i], &tmp);
+        inside[i].resize(tmp);
+        for(int j=0;j<tmp;j++) {
+            scanf("%d", &inside[i][j]);
+        }
+    }
+
+    set<int> S;
+    go(S);
+}
+
+int main(void)
+{
+    int T;
+    scanf("%d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+}
