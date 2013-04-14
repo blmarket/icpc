95780d8cceb8a803a10539bb43a39dc82d011cff
Jeong, Heon <blmarket@gmail.com>
2013-04-14 10:14:35 +0900

    --Empty Message--

--- blmarket/2013/codejamR0/B.cpp
+++ blmarket/2013/codejamR0/B.cpp
@@ -80,7 +80,6 @@ bool chkgood(const string &a) {
 
 void go(string &tmp) {
     if(size(tmp) > 5) return;
-    cout << tmp << endl;
     for(int i=0;i<3;i++) {
         tmp += (char)(i + '0');
         if(chkgood(tmp)) {
