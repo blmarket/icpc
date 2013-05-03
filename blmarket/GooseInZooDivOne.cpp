bbcc8a7e3e38e6393ec724e269bfb2141554f286
Jeong, Heon <blmarket@gmail.com>
2013-05-03 10:15:08 +0900

    --Empty Message--

---
+++ blmarket/GooseInZooDivOne.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+vector<string> field;
+vector<int> G;
+int dist;
+
+int go(int a,int b) {
+    int ret = 1;
+    field[a][b] = '.';
+    for(int i=a-dist;i<=a+dist;i++) {
+        if(i<0 || i>=size(field)) continue;
+        for(int j=b-dist;j<=b+dist;j++) {
+            if(j<0 || j>=size(field[i])) continue;
+            int dd = abs(i-a) + abs(j-b);
+            if(dd > dist) continue;
+            if(field[i][j] == '.') continue;
+            ret += go(i,j);
+        }
+    }
+    return ret;
+}
+
+class GooseInZooDivOne 
+{
+public:
+    int count(vector <string> field_, int dist_) 
+    {
+        field = field_; dist = dist_;
+        for(int i=0;i<size(field);i++) {
+            for(int j=0;j<size(field[i]);j++) {
+                if(field[i][j] == 'v') {
+                    G.pb(go(i,j));
+                }
+            }
+        }
+        for(int i=0;i<size(G);i++) {
+            cout << G[i] << " ";
+        }
+        cout << endl;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"vvv"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; int Arg2 = 3; verify_case(0, Arg2, count(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {"."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 100; int Arg2 = 0; verify_case(1, Arg2, count(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"vvv"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 0; verify_case(2, Arg2, count(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {"v.v..................v............................"
+,".v......v..................v.....................v"
+,"..v.....v....v.........v...............v......v..."
+,".........vvv...vv.v.........v.v..................v"
+,".....v..........v......v..v...v.......v..........."
+,"...................vv...............v.v..v.v..v..."
+,".v.vv.................v..............v............"
+,"..vv.......v...vv.v............vv.....v.....v....."
+,"....v..........v....v........v.......v.v.v........"
+,".v.......v.............v.v..........vv......v....."
+,"....v.v.......v........v.....v.................v.."
+,"....v..v..v.v..............v.v.v....v..........v.."
+,"..........v...v...................v..............v"
+,"..v........v..........................v....v..v..."
+,"....................v..v.........vv........v......"
+,"..v......v...............................v.v......"
+,"..v.v..............v........v...............vv.vv."
+,"...vv......v...............v.v..............v....."
+,"............................v..v.................v"
+,".v.............v.......v.........................."
+,"......v...v........................v.............."
+,".........v.....v..............vv.................."
+,"................v..v..v.........v....v.......v...."
+,"........v.....v.............v......v.v............"
+,"...........v....................v.v....v.v.v...v.."
+,"...........v......................v...v..........."
+,"..........vv...........v.v.....................v.."
+,".....................v......v............v...v...."
+,".....vv..........................vv.v.....v.v....."
+,".vv.......v...............v.......v..v.....v......"
+,"............v................v..........v....v...."
+,"................vv...v............................"
+,"................v...........v........v...v....v..."
+,"..v...v...v.............v...v........v....v..v...."
+,"......v..v.......v........v..v....vv.............."
+,"...........v..........v........v.v................"
+,"v.v......v................v....................v.."
+,".v........v................................v......"
+,"............................v...v.......v........."
+,"........................vv.v..............v...vv.."
+,".......................vv........v.............v.."
+,"...v.............v.........................v......"
+,"....v......vv...........................v........."
+,"....vv....v................v...vv..............v.."
+,".................................................."
+,"vv........v...v..v.....v..v..................v...."
+,".........v..............v.vv.v.............v......"
+,".......v.....v......v...............v............."
+,"..v..................v................v....v......"
+,".....v.....v.....................v.v......v......."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 898961330; verify_case(3, Arg2, count(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    GooseInZooDivOne ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
