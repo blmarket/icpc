6bcf8fee090bb8aa6db747f2d9179f9c0534e3c8
Jeong, Heon <blmarket@gmail.com>
2013-05-03 22:27:03 +0900

    --Empty Message--

--- blmarket/DeerInZooDivOne.cpp
+++ blmarket/DeerInZooDivOne.cpp
@@ -148,6 +148,13 @@ int go(int pos) {
         }
     }
 
+    for(int i=0;i<N;i++) {
+        for(int j=0;j<N;j++) {
+            cout << match[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return ret;
 }
 
