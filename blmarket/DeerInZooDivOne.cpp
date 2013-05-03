889d26d6a2c2fd6ffb0449496bf2ebb7590ab7dc
Jeong, Heon <blmarket@gmail.com>
2013-05-03 11:21:37 +0900

    --Empty Message--

--- blmarket/DeerInZooDivOne.cpp
+++ blmarket/DeerInZooDivOne.cpp
@@ -84,7 +84,9 @@ void calc(int a, int b) {
     vector<int> t1,t2;
     BipartiteMatching(w, t1, t2);
 
-    cout << a << " " << b << endl;
+    for(int i=0;i<size(t1);i++) cout << t1[i] << " ";
+    cout << endl;
+
 
     match[a][b] = match[b][a] = 1;
 }
