94a50341f83351554d5124c1edb673b45e19da24
Jeong, Heon <blmarket@gmail.com>
2013-05-03 21:24:09 +0900

    --Empty Message--

--- blmarket/DeerInZooDivOne.cpp
+++ blmarket/DeerInZooDivOne.cpp
@@ -156,6 +156,7 @@ public:
         a=a_;b=b_; N = size(a) + 1;
         int ret = 0;
         for(int i=0;i<size(a);i++) {
+            cout << i << " " << ret << endl;
             int tmp = go(i);
             if(ret < tmp) ret = tmp;
         }
