aa3b39bea34ee192863bc061119178d4832f9bf6
Jeong, Heon <blmarket@gmail.com>
2013-05-04 21:59:00 +0900

    --Empty Message--

--- blmarket/2013/codejamR1A/C.cpp
+++ blmarket/2013/codejamR1A/C.cpp
@@ -68,7 +68,7 @@ int main(void)
     int T;
     scanf("%d", &T);
     for(int i=1;i<=T;i++) {
-        printf("Case #%d: ", i);
+        printf("Case #%d:\n", i);
         process();
         cerr << i << endl;
     }
