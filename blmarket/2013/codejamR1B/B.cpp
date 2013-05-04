f26fa7513071f72f92972d45c274837d69313b9d
Jeong, Heon <blmarket@gmail.com>
2013-05-05 01:46:22 +0900

    --Empty Message--

--- blmarket/2013/codejamR1B/B.cpp
+++ blmarket/2013/codejamR1B/B.cpp
@@ -25,10 +25,20 @@ typedef pair<int,int> PII;
 template<typename T> int size(const T &a) { return a.size(); } 
 
 int N,X,Y;
+int combi[505][505];
+
+void precalc() {
+    memset(combi, 0, sizeof(combi));
+    for(int i=1;i<=500;i++) {
+        combi[i][0] = combi[i][i] = 1;
+        for(int j=1;j<i;j++) {
+            combi[i][j] = combi[i-1][j-1] + combi[i-1][j];
+        }
+    }
+}
 
 void process(void) {
     scanf("%d %d %d",&N, &X, &Y);
-    Y = 500;
     int layer = (abs(X) + abs(Y)) / 2;
     if(layer == 0) {
         cout << "1.0" << endl;
@@ -57,11 +67,34 @@ void process(void) {
 
     int given = N - minrange;
 
-    cout << "???" << endl;
+    vector<double> cur, nex;
+    cur.resize(2, 0.5);
+    for(int i=2;i<=given;i++) {
+        nex = vector<double>(i+1);
+
+        for(int j=0;j<i;j++) {
+            int right = i-1-j;
+            if(j > layer*2 || right > layer*2) continue;
+            if(j == layer*2) {
+                nex[j] += cur[j];
+                continue;
+            }
+            if(right == layer*2) {
+                nex[j+1] += cur[j];
+                continue;
+            }
+            nex[j] += cur[j] * 0.5;
+            nex[j+1] += cur[j] * 0.5;
+        }
+        cur.swap(nex);
+    }
+
+    printf("%.12lf\n", cur[Y]);
 }
 
 int main(void)
 {
+    precalc();
     int T;
     scanf("%d", &T);
     for(int i=1;i<=T;i++) {
