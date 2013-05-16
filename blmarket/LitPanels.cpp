1ff4608b160aebad7ac8e79bbb380cab718ef0a9
Jeong, Heon <heon.jeong@sk.com>
2013-05-16 16:09:24 +0900

    --Empty Message--

--- blmarket/LitPanels.cpp
+++ blmarket/LitPanels.cpp
@@ -140,7 +140,7 @@ public:
 	private:
 	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
 	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
-	void test_case_0() { int Arg0 = 18; int Arg1 = 3; int Arg2 = 17; int Arg3 = 2; int Arg4 = 551428729; verify_case(0, Arg4, countPatterns(Arg0, Arg1, Arg2, Arg3)); }
+	void test_case_0() { int Arg0 = 17; int Arg1 = 3; int Arg2 = 16; int Arg3 = 2; int Arg4 = 551428729; verify_case(0, Arg4, countPatterns(Arg0, Arg1, Arg2, Arg3)); }
 	void test_case_1() { int Arg0 = 2; int Arg1 = 3; int Arg2 = 1; int Arg3 = 2; int Arg4 = 40; verify_case(1, Arg4, countPatterns(Arg0, Arg1, Arg2, Arg3)); }
 	void test_case_2() { int Arg0 = 4; int Arg1 = 4; int Arg2 = 3; int Arg3 = 2; int Arg4 = 14096; verify_case(2, Arg4, countPatterns(Arg0, Arg1, Arg2, Arg3)); }
 	void test_case_3() { int Arg0 = 40; int Arg1 = 39; int Arg2 = 5; int Arg3 = 8; int Arg4 = 877713074; verify_case(3, Arg4, countPatterns(Arg0, Arg1, Arg2, Arg3)); }
