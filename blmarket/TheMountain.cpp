a776459d1d2e7a542c675865a78a95a9b72801ab
Jeong, Heon <blmarket@gmail.com>
2013-05-17 02:43:18 +0900

    --Empty Message--

--- blmarket/TheMountain.cpp
+++ blmarket/TheMountain.cpp
@@ -56,7 +56,7 @@ int go(int a, int b) {
 
     if(fill(a,b) == -1) return -1;
 
-    cout << a << "," << b << " = " << endl;
+    cout << a << "," << b << " = " << fill(a,b) << endl;
     for(int i=0;i<N;i++) {
         for(int j=0;j<M;j++) cout << data[i][j] << " ";
         cout << endl;
