c3118be886c4645249954707b11eaeebdf6cf0c1
Jeong, Heon <blmarket@gmail.com>
2013-05-18 11:39:44 +0900

    --Empty Message--

---
+++ blmarket/2013/codejamR1C/B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+set<PII> cur, nex;
+
+void process(void) {
+    cur.insert(mp(0,0));
+    for(int i=1;i<=5;i++) {
+        for(auto it : cur) {
+            cout << it.first << " " << it.second << endl;
+        }
+    }
+}
+
+int main(void)
+{
+    int T;
+    scanf("%d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+        cerr << i << endl;
+    }
+}
