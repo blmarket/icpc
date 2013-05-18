7b1f0df3028d591e83ee3388f2eed4fa9855e613
Jeong, Heon <blmarket@gmail.com>
2013-05-18 12:07:37 +0900

    --Empty Message--

---
+++ blmarket/2013/codejamR1C/C.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+struct atk {
+    int d;
+    int w,e;
+    int s;
+
+    bool operator<(const atk &rhs) const {
+        return d < rhs.d;
+    }
+};
+
+vector<atk> V;
+
+void process(void) {
+    int N;
+    scanf("%d", &N);
+    for(int i=0;i<N;i++) {
+        int d,n,w,e,s,dd,dm,ds;
+        scanf("%d %d %d %d %d %d %d %d", &d, &n, &w, &e, &s, &dd, &dm, &ds);
+        for(int j=0;j<n;j++) {
+            atk tmp;
+            tmp.d = d; tmp.w = w; tmp.e = e; tmp.s = s; 
+
+            V.pb(tmp);
+
+            d += dd; w += dm; e += dm; s += ds;
+        }
+    }
+    sort(V.begin(), V.end());
+}
+
+int main(void)
+{
+    int T;
+    scanf("%d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+        cerr << i << endl;
+    }
+}
