0f4548a3ec297c8d08b063fc32890909ffd108b1
Jeong, Heon <blmarket@gmail.com>
2013-05-19 14:37:07 +0900

    --Empty Message--

---
+++ blmarket/UndoHistory.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+vector<string> undos;
+string cur;
+
+int enter(string line) {
+    int mincost = -1;
+    int md = -1;
+    for(int i=0;i<size(undos);i++) {
+        if(undos[i].size() > line.size()) continue;
+        int sz = size(undos[i]);
+        if(line.substr(0, sz) != undos[i]) continue;
+
+        int tmp = size(line) - sz;
+        if(i+1 < size(undos)) tmp += 2;
+
+        if(mincost == -1 || mincost > tmp) {
+            mincost = tmp;
+            md = i;
+        }
+    }
+
+    cur = undos[md];
+    while(size(cur) != size(line)) {
+        cur += line[size(cur)];
+        undos.pb(cur);
+    }
+
+    return mincost;
+}
+
+class UndoHistory 
+{
+public:
+    int minPresses(vector <string> lines) 
+    {
+        undos.clear();
+        cur = "";
+        undos.pb(cur);
+        int ret = 0;
+        for(int i=0;i<size(lines);i++) {
+            ret += enter(lines[i]);
+        }
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"tomorrow", "topcoder"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 18; verify_case(0, Arg1, minPresses(Arg0)); }
+	void test_case_1() { string Arr0[] = {"a","b"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 6; verify_case(1, Arg1, minPresses(Arg0)); }
+	void test_case_2() { string Arr0[] = {"a", "ab", "abac", "abacus" }; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 10; verify_case(2, Arg1, minPresses(Arg0)); }
+	void test_case_3() { string Arr0[] = {"pyramid", "sphinx", "sphere", "python", "serpent"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 39; verify_case(3, Arg1, minPresses(Arg0)); }
+	void test_case_4() { string Arr0[] = {"ba","a","a","b","ba"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 13; verify_case(4, Arg1, minPresses(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    UndoHistory ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
