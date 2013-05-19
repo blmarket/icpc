fd2683eafc26087b2702d11c9c252707767d866f
Jeong, Heon <blmarket@gmail.com>
2013-05-19 14:58:13 +0900

    --Empty Message--

--- blmarket/TravellingPurchasingMan.cpp
+++ blmarket/TravellingPurchasingMan.cpp
@@ -84,8 +84,7 @@ public:
                 if(ret < tmp2) ret = tmp2;
             }
 
-            cout << (pos < sn) << " " << (time <= s2[pos]) << " " << (mask & (1<<pos)) << endl;
-            if(pos < sn && time <= s2[pos] && (mask & (1<<pos) == 0)) {
+            if(pos < sn && time <= s2[pos] && ((mask & (1<<pos)) == 0)) {
                 int ntime = max(time, s1[pos]) + s3[pos];
                 int nmask = mask | (1<<pos);
                 if(memo[pos][nmask] == -1 || memo[pos][nmask] > ntime) {
