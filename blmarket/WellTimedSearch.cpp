dc4f71580322be20389e08cd0482707bc267778a
Jeong, Heon <blmarket@gmail.com>
2013-05-24 22:07:00 +0900

    --Empty Message--

--- blmarket/WellTimedSearch.cpp
+++ blmarket/WellTimedSearch.cpp
@@ -44,6 +44,7 @@ long double go(int N, int A, int B) {
         for(int i=0;i<A-1;i++) {
             tmp *= 2;
             if(tmp >= N) {
+                cout << "here" << endl;
                 prob = prob * (N-1) / N;
                 N--; A--; B--;
                 onemore = true;
