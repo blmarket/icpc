a025d8333783c3efaec170aabe375ea975355c03
Jeong, Heon <blmarket@gmail.com>
2013-06-02 21:00:26 +0900

    --Empty Message--

--- blmarket/2013/codejamR2/A.cpp
+++ blmarket/2013/codejamR2/A.cpp
@@ -24,57 +24,7 @@ typedef pair<int,int> PII;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 
-int N, M;
-int s[10000005], e[10000005], f[10000005];
-
 void process(void) {
-    scanf("%d %d", &N, &M);
-    for(int i=0;i<M;i++) {
-        scanf("%d %d %d", &s[i], &e[i], &f[i]);
-    }
-
-    long long ret = 0;
-
-    while(true) {
-        bool action = false;
-
-        for(int i=0;i<M;i++) if(f[i]) {
-            for(int j=i+1;j<M;j++) if(f[j]) {
-                if(e[i] < s[j] || e[j] < s[i]) continue;
-                int d1 = e[i] - s[i];
-                int d2 = e[j] - s[j];
-                long long cost = d1 * N - (d1-1) * d1 / 2 + d2 * N - (d2-1) * d2 / 2;
-
-                int nd1 = e[i] - s[j];
-                int nd2 = e[j] - s[i];
-                long long cost2 = nd1 * N - (nd1-1) * nd1 / 2 + nd2 * N - (nd2-1) * nd2 / 2;
-
-                if(cost2 < cost) {
-                    action = true;
-                    int minf = min(f[i], f[j]);
-                    ret += minf * (cost - cost2);
-                    if(f[i] == f[j]) {
-                        swap(e[i], e[j]);
-                    } else {
-                        f[i] -= minf;
-                        f[j] -= minf;
-                        s[M] = s[i]; e[M] = e[j]; f[M] = minf;
-                        s[M+1] = s[j]; e[M+1] = e[i]; f[M+1] = minf;
-                        if(f[i] == 0) {
-                            s[i] = s[M+1]; e[i] = e[M+1]; f[i] = f[M+1];
-                        } else {
-                            s[j] = s[M+1]; e[j] = e[M+1]; f[j] = f[M+1];
-                        }
-                        M++;
-                    }
-                    cout << s[i] << " " << e[i] << " " << f[i] << endl;
-                }
-            }
-        }
-        if(!action) break;
-    }
-    cerr << M << endl;
-    cout << ret << endl;
 }
 
 int main(void)
