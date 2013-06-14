9f286d522cedef6d99f71fe61ecc13e3e7b0260b
Jeong, Heon <blmarket@gmail.com>
2013-06-14 20:35:03 +0900

    --Empty Message--

---
+++ blmarket/ColorfulBuilding.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int N;
+long long cur[40][40][40], nex[40][40][40];
+
+map<pair<char, char>, int> mapper;
+vector<int> cs;
+
+class ColorfulBuilding 
+{
+public:
+    int count(vector <string> color1, vector <string> color2, int L) 
+    {		
+        string c1,c2;
+        for(int i=0;i<size(color1);i++) c1 += color1[i];
+        for(int i=0;i<size(color2);i++) c2 += color2[i];
+
+        N = size(color1);
+        for(int i=0;i<N;i++) {
+            pair<char, char> key = mp(c1[i], c2[i]);
+            int vv = mapper.size() + 1;
+            if(mapper.count(key)) {
+                vv = mapper[key];
+            } else {
+                mapper[key] = vv;
+            }
+            cs.pb(vv);
+            cout << vv << " ";
+        }
+        cout << endl;
+
+        memset(cur, 0, sizeof(cur));
+        return 0;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"aaba"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"aaaa"}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 3; int Arg3 = 6; verify_case(0, Arg3, count(Arg0, Arg1, Arg2)); }
+	void test_case_1() { string Arr0[] = {"aaba"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"aaba"}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; int Arg3 = 0; verify_case(1, Arg3, count(Arg0, Arg1, Arg2)); }
+	void test_case_2() { string Arr0[] = {"ab", "ba", "a", "aab"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"bb", "ba", "a", "aba"}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 5; int Arg3 = 432; verify_case(2, Arg3, count(Arg0, Arg1, Arg2)); }
+	void test_case_3() { string Arr0[] = {"xxxxxxxxxxxxxxxxxxxx",
+ "xxxxxxxxxxxxxxxxxxxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOxxxxxxxxxxxxxxxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxxxxxxxxxxxxxxxOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxxxxxxxxxxxxxxxxxxx",
+ "xxxxxxxxxxxxxxxxxxxx"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"xxxxxxxxxxxxxxxxxxxx",
+ "xxxxxxxxxxxxxxxxxxxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOxxxxxxxxxxxxOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxOOOOOOOOOOOOOOOOxx",
+ "xxxxxxxxxxxxxxxxxxxx",
+ "xxxxxxxxxxxxxxxxxxxx"}; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 58; int Arg3 = 619787617; verify_case(3, Arg3, count(Arg0, Arg1, Arg2)); }
+	void test_case_4() { string Arr0[] = {"SJXcabKTWeUXhwxGixFepQaQlFxrAedNAtVP",
+ "gorBIkcTlOFbLDbFeElYAyAqszQdMbpxSRdE",
+ "SQVHPehlNtesbJDflyGxMqvgzjXisRnqDcQO",
+ "pIAEBwbmpPWCooQAujbGXFGLvXxTmExLARkf",
+ "AFnWyWKVObfxDGCjTxdZaObgwdxlPKtIxaAd",
+ "uznMpJVNjAofbHJjOrZeSHgSagOCUMGbvkVR",
+ "LBRrDbTAyKfVnedKiRfzgRzECpcsziqaTwdo",
+ "JrJHvsEVVGDkNVGqLbpxyLDPloBuNDQKnReI",
+ "SSYpbjKHSCnQhuyYrVauWDHDyhAoGyecrZMv",
+ "UdetQfWEUWHHuAxRSdkJOOJSixKpQXpCFZHO",
+ "KXVsQbuQtIgsULOMsTvPFNUqkBldMTLCipYK",
+ "hoXConjnDWQkZVtyZlwSedvdVrNWqQhGUClQ",
+ "TpsvvyoXsXmQpBAGGVDrXCkodoRHQZmpoQDW",
+ "csiJspzTqeFBRmPgeEtTAzfrfCGlTZqcPuyO",
+ "vsPDVBJVaJmUAtDdcsKoUkPEbDmAwtZKwjjP",
+ "MOfoMhMiKIvGQoeIJXHzyClWRtRuKXMqxUAF",
+ "KyyUCkRBjsYvmPFFEGBqqVhIUdtvIyyLacfu",
+ "BfuwfSajSlcuTzhMufHSQLudvGEGlyHsEmBD",
+ "PLpIXZkdyXveTMfSvqnDGKWOZrTBMUIlZrqF",
+ "dzVMCqrSLbanRJTYpDJNHAOLPSzmvSEPQJYm",
+ "rAjRkrSjouJyFaCSPPLYSzqDmMoADyWAbobJ",
+ "eOCBrJNoyFnGpXpxiExXcoOHnVsaEPXhPfLe",
+ "XMjRksnxWssPEINhdhbiLBSYpCLtwNshFjXS",
+ "HnnDeUAbuswsgsYQuAaXySLkFYUwFXwYTreM",
+ "uqLnwOEGbwZZDgAAWEdLRZxFlogDmlhjhgHM",
+ "NcfaQsgPQfirkYDRFrLQpySmBGfRHumKULZf",
+ "mOpmmgclsxRzXskEywfryqCRyATNoJwnlHiD",
+ "AnoKoKAjrasttjNlHCROnvTJMhEHlVPVoVMo",
+ "yegLyIuRkkENFAjwzDoPLKjgUHHEkfzYDIpo",
+ "EcxRGGfuaBXUFXkSxctJWOLmmVbvoMkWtTvV",
+ "nBMkOBHgaltEVzfyGxseGhmBscfGIbxFbqRn",
+ "GvkEcLtqdOofGtsbWDafVMbNdJxsffKDzSiR",
+ "jhZQUVzTzalrZcebvyqPWtOUUyniBKTWdyLi",
+ "ODJLQPMdjDdTlxrfGsNmBfeGYjzeXApqKDhJ",
+ "vlJkWMzdVQujKdLViQClOrJXMTBkuZEasFgg",
+ "FAsbuzrLVIgaryqXBfuBRAbReleXSSgEKSvt"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arr1[] = {"XAhlUHtfjOpOlQlowWppQcodMGUiqoLobxnD",
+ "IDzGlKrHiVGdBjZxIcoxjwagbWieKvUwZrjg",
+ "SkpkTVeIFeoNQzUUgmYAYskKONNZdgXxSiWd",
+ "rXBGhTmqnvxkmqiutoDzPusDiOUggEFXGCzm",
+ "fzrxVuTbFXunGbeEavjshmrIRlDorTuISCxn",
+ "LsvIaxTkOBjcskiekmrKNNFEXqnfMNXLWqqu",
+ "ekzxGIlbJPVEQPYAbTuMtZKsCiubDXDiBVzU",
+ "hZuOCJxvBOYENGSFcUiKKAKfCdebutVmnyvB",
+ "UveNkKRQmHnedrROQOpHJfrHjwcNLUShlDbn",
+ "ptUkvbaGDryHRkYfHKLkSgVpjWCEcgazyxGK",
+ "nVPYEqTTJsRPjzjfdOiULhnZPFeNcnbXaQlk",
+ "IPXBXRhMQIkxpygsgbcRfMuvIcuzUPPHGOWX",
+ "yWWlNyEyqZSIOXBFAybIuFpVqpvmKRaRFrAE",
+ "EvBJVtHvKhjrFcmtpdBbFTdTVtXXZQKAglKT",
+ "bCVjHzUvyINFkxXageZQMzCyNhcifACdJVDh",
+ "lZITYcDSeIbLweyFtoMAfOQyBNupKlhcNpgo",
+ "BduslNrJdWOUukYFFidEkMFaghfofpxVgvJd",
+ "YrJpDZKqdjEPzdLsOQEdkcrBfNHPemXHokCW",
+ "GjeUKSGjDlgKTyUGNrMQbBLxRUcgrWpkAwOD",
+ "wgxTcswqdJHaDugNIRMvrhBsdDaJAssVbSRW",
+ "qmVmqFEpvgGioMXDSFqEoQcDOAaUoGPEovSO",
+ "KrukPlpfOhawaovCfteTSCIdLMrtImVtiMyQ",
+ "ykwmxHsKMFzFHwcbyyedLvhZPnaNGqJMMCxd",
+ "HUNYCXjNLQIFCLLGpCXHBCHLTxLynBxnHFbx",
+ "uwjzbNbJepVFgMPUXVirxYHzExquBEtPmKju",
+ "xXAxAbJePyUsVHeLytDvAxBGMRtnvCEiZZqe",
+ "xMkQoIVxWPXPgaOYmDjTOXiMImVdzojERNxS",
+ "dwICFwMAmdOIUxyAdXdshasnzwyhfnVWVqZJ",
+ "etypXNVvSTEQvriGBZdSGmDEHhvpSqkFklCS",
+ "YkxpFBCRYUueRcKaJUXVdaMoYMYEooPQVMOr",
+ "DTrexDqclZNKdPuTRFHualJSFziCLPCZjpxo",
+ "TfEijcAsSJPikkmBSbXMqYHAhPTcpcKVSkIX",
+ "xKXHYPYMJxFpSbxltDKYuRiTkOLxpQKnXZPs",
+ "YFYuvuYHfpFJcrLNIdlNfBxRnWdppsdalBkx",
+ "NFTysBvNFjejdnlhRTclbcfGipNCxpFEOriY",
+ "thkgVflJYmbUYbIlafNUMGePQWiZyYzYXvUR"}
+; vector <string> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1200; int Arg3 = 396065426; verify_case(4, Arg3, count(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    ColorfulBuilding ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
