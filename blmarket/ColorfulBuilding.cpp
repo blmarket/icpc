6f385ca97e559503b3c731d2cac8c28a645335ff
Jeong, Heon <blmarket@gmail.com>
2013-06-14 20:49:22 +0900

    --Empty Message--

--- blmarket/ColorfulBuilding.cpp
+++ blmarket/ColorfulBuilding.cpp
@@ -73,6 +73,8 @@ public:
                                 long long &t1 = nex[ncnt][cs[i]][l-ll];
                                 t1 += cur[j][k][l] * mul; t1 %= mod;
                                 mul *= l-ll; mul %= mod;
+
+                                cout << "->" << ncnt << " " << cs[i] << " " << l-ll << t1 << endl;
                             }
 
                             if(i+1 < N) {
