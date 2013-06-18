c0dedca0b061bff879aa81f57b8fdcb127e073d9
Jeong, Heon <blmarket@gmail.com>
2013-06-19 01:00:02 +0900

    --Empty Message--

---
+++ blmarket/RandomPaintingOnABoard.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int sum = 0;
+vector<string> prob;
+map<PII, double> memo;
+
+double calc(int rs, int cs) {
+    PII key = mp(rs, cs);
+    if(memo.count(key)) return memo[key];
+
+    int oth = 0;
+    double tot = 1;
+    for(int i=0;i<size(prob);i++) for(int j=0;j<size(prob[i]);j++) if(prob[i][j] != '0') {
+        if((rs & (1<<i)) && (cs & (1<<j))) {
+            oth += prob[i][j] - '0';
+            continue;
+        }
+        int rrs = rs | (1<<i);
+        int ccs = cs | (1<<j);
+
+        double p1 = (double)(prob[i][j] - '0') / sum;
+        double e1 = calc(rrs, ccs);
+
+        tot += p1 * e1;
+    }
+
+    double p = (double)(sum - oth) / sum;
+    return memo[key] = tot / p;
+}
+
+class RandomPaintingOnABoard 
+{
+public:
+    double expectedSteps(vector <string> prob_) 
+    {
+        prob = prob_;
+        sum = 0;
+        for(int i=0;i<size(prob);i++) for(int j=0;j<size(prob[i]);j++) {
+            sum += prob[i][j] - '0';
+        }
+
+        return calc(0,0);
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const double &Expected, const double &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"10",
+ "01"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); double Arg1 = 3.0; verify_case(0, Arg1, expectedSteps(Arg0)); }
+	void test_case_1() { string Arr0[] = {"11",
+ "11"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); double Arg1 = 3.6666666666666665; verify_case(1, Arg1, expectedSteps(Arg0)); }
+	void test_case_2() { string Arr0[] = {"11",
+ "12"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); double Arg1 = 3.9166666666666665; verify_case(2, Arg1, expectedSteps(Arg0)); }
+	void test_case_3() { string Arr0[] = {"0976",
+ "1701",
+ "7119"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); double Arg1 = 11.214334077031307; verify_case(3, Arg1, expectedSteps(Arg0)); }
+	void test_case_4() { string Arr0[] = {"000000000000001000000",
+ "888999988889890999988",
+ "988889988899980889999",
+ "889898998889980999898",
+ "988889999989880899999",
+ "998888998988990989998",
+ "998988999898990889899"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); double Arg1 = 1028.7662876159634; verify_case(4, Arg1, expectedSteps(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    RandomPaintingOnABoard ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
