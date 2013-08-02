201feacf82b05daa10499f1f0ce970678823a643
Jeong, Heon <heon.jeong@sk.com>
2013-08-02 15:23:54 +0900

    --Empty Message--

--- blmarket/TriangleXor.cpp
+++ blmarket/TriangleXor.cpp
@@ -44,8 +44,9 @@ public:
             long double y1 = (long double)i / (W + i);
             long double y0 = (long double)(i-1) / (W + i-1);
             long double xspace = (long double)W / (W+((i+1)/2));
-            cout << y1 << " " << y0 << " " << nc << " " << xspace << endl;
-            ret += (y1-y0) / 2.0 * nc * W / (W + (i+1)/2);
+            long double sum = (y1-y0) / 2.0 * nc * W / (W + (i+1)/2);
+            cout << y1 << " " << y0 << " " << nc << " " << xspace << " = " << sum << endl;
+            ret += sum;
             if((i%2) == 0) nc -= 2;
         }
 
