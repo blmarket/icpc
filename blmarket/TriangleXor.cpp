6e7ce591ff07666f93ab99bee0c0406af2f0be48
Jeong, Heon <heon.jeong@sk.com>
2013-08-02 15:04:57 +0900

    --Empty Message--

--- blmarket/TriangleXor.cpp
+++ blmarket/TriangleXor.cpp
@@ -40,7 +40,7 @@ public:
             if(nc <= 0) break;
             double y1 = (double)i / (W + i);
             double y0 = (double)(i-1) / (W + i-1);
-            cout << y1 << " " << y0 << endl;
+            cout << y1 << " " << y0 << " " << nc << endl;
             ret += (y1-y0)/2 * nc;
             if((i%2) == 0) nc -= 2;
         }
