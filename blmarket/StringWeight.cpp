6fff26c825f177673a692f9b8f4034036ea8680e
Jeong, Heon <heon.jeong@sk.com>
2013-08-02 09:14:13 +0900

    --Empty Message--

--- blmarket/StringWeight.cpp
+++ blmarket/StringWeight.cpp
@@ -37,7 +37,7 @@ int go(int pos, int used, int now) {
         int ends = max(i, now);
 
         int tmp = go(pos+1, used + i, nnow - i);
-        cout << i << " = " << tmp << endl;
+        cout << i << " = " << tmp << " " << begins << ends << stretch << endl;
         if(tmp >= 0) {
             tmp += begins * (begins + 1) / 2;
             tmp += ends * (ends + 1) / 2;
