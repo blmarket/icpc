28fd6eecca6d5fea24ecf9d73da90a1154881b77
Jeong, Heon <heon.jeong@sk.com>
2013-08-02 09:22:58 +0900

    --Empty Message--

--- blmarket/StringWeight.cpp
+++ blmarket/StringWeight.cpp
@@ -51,7 +51,7 @@ int go(int pos, int used, int now) {
         }
     }
 
-    if(ret != -2) cout << pos << " " << used << " " << now << " = " << ret << endl;
+    // if(ret != -2) cout << pos << " " << used << " " << now << " = " << ret << endl;
     if(L[pos] > 26 && (pos == 0 || pos+1 == size(L))) ret += L[pos] - 26;
     return memo[pos][used][now] = ret;
 }
