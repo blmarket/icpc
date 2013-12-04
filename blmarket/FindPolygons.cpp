1b5413a8edf6932acbf9f4c665c930af6152253b
Jeong, Heon <blmarket@gmail.com>
2013-12-05 01:25:02 +0900

    --Empty Message--

--- blmarket/FindPolygons.cpp
+++ blmarket/FindPolygons.cpp
@@ -26,11 +26,10 @@ public:
     double minimumPolygon(int L) 
     {		
         if(L%2) return -1.0;
-        for(int i=1;i<100;i++) {
+        for(int i=1;i<1000;i++) {
             for(int j=1;j<i;j++) {
                 if(i*i+j*j > 5000) break;
                 int sum = 2*i*(i+j);
-                cout << sum << endl;
                 if(sum == L) {
                     cout << i*i-j*j << " " << 2*i*j << " " << i*i+j*j << endl;
                 }
