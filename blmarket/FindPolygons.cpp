0c4e9061d2ac8842bb031325baaab59efb1c35fb
Jeong, Heon <blmarket@gmail.com>
2013-12-05 02:10:55 +0900

    --Empty Message--

--- blmarket/FindPolygons.cpp
+++ blmarket/FindPolygons.cpp
@@ -57,7 +57,7 @@ public:
                 if(p1 > p2) swap(p1, p2);
                 int idx = lower_bound(V.begin(), V.end(), mp(mp(p1, p2), 0)) - V.begin();
                 if(idx < size(V) && V[idx].first.first == p1 && V[idx].first.second == p2) {
-                    cout << "here " << V[idx].second << " " << dst << endl;
+                    cout << l1 << " " << l2 << " " << V[idx].second << endl;
                     if(V[idx].second == dst) ret = tmp;
                 }
 
@@ -66,7 +66,7 @@ public:
                 if(p1 > p2) swap(p1, p2);
                 idx = lower_bound(V.begin(), V.end(), mp(mp(p1, p2), 0)) - V.begin();
                 if(idx < size(V) && V[idx].first.first == p1 && V[idx].first.second == p2) {
-                    cout << "here2 " << V[idx].second << " " << dst << endl;
+                    cout << l1 << " " << l2 << " " << V[idx].second << endl;
                     if (V[idx].second == dst) ret = tmp;
                 }
             }
