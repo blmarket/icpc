58ff2dd763d7913b8709f4ff9ea23f61e23fa8d8
Jeong, Heon <blmarket@gmail.com>
2013-12-08 10:18:34 +0900

    --Empty Message--

--- blmarket/2014/facebookR1/D.cpp
+++ blmarket/2014/facebookR1/D.cpp
@@ -101,6 +101,7 @@ void solve(int dataId)
         Q.push(src);
         while(!Q.empty()) {
             int pos = Q.front();
+            cout << pos << " " << mincost[pos] << endl;
             Q.pop();
             for(int i=0;i<size(flows[pos]);i++) {
                 int a, b;
