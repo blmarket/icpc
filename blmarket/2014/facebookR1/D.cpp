1adc1f6b7b224880970a08d6a196579fead177d0
Jeong, Heon <blmarket@gmail.com>
2013-12-09 00:35:55 +0900

    --Empty Message--

--- blmarket/2014/facebookR1/D.cpp
+++ blmarket/2014/facebookR1/D.cpp
@@ -48,6 +48,7 @@ int go(int pos, int sv, int cut) {
         bool gogo = true;
         for(int j=0;j<size(primes);j++) {
             if((tmp % primes[j]) == 0 && used[j]) {
+                cout << sv << " fails with " << j << endl;
                 gogo = false;
                 break;
             }
@@ -56,7 +57,6 @@ int go(int pos, int sv, int cut) {
             }
         }
         if(!gogo) {
-            cout << sv << " fails" << endl;
             continue;
         }
         tmp = sv;
