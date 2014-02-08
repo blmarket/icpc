8badc6c920afa7742fb48dfbb9133ec814b84ec1
Jeong, Heon <blmarket@gmail.com>
2014-02-08 11:59:45 +0900

    --Empty Message--

--- blmarket/CombinationLockDiv1.cpp
+++ blmarket/CombinationLockDiv1.cpp
@@ -58,7 +58,7 @@ int go(int a, int b) {
         }
     }
 
-    cout << a << " " << b << " = " << ret << endl;
+    // cout << a << " " << b << " = " << ret << endl;
     return memo[key] = ret;
 }
 
