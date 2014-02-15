ab7bc13d41e4524ee3b386b100b20f26c398cac8
Jeong, Heon <blmarket@gmail.com>
2014-02-16 01:32:58 +0900

    --Empty Message--

--- blmarket/DivideAndShift.cpp
+++ blmarket/DivideAndShift.cpp
@@ -29,8 +29,11 @@ int go(int n, int m) {
 
     int ret = (m-1);
     if(ret < 0) ret += n;
+
     int tmp = (n - m + 1);
     if(ret > tmp) ret = tmp;
+
+    cout << ret << endl;
 
     for(int i=0;i<size(primes);i++) {
         if(n % primes[i] == 0) {
