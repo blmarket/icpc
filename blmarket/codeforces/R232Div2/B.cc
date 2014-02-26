d2c23786d3dfa0f293421f88d98a894a3ae3bec2
Jeong, Heon <blmarket@gmail.com>
2014-02-27 00:44:41 +0900

    --Empty Message--

---
+++ blmarket/codeforces/R232Div2/B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int a,b,c;
+        scanf("%d %d %d", &a, &b, &c);
+        int div = a / b;
+        if(c * div >= a) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
+    }
+    return 0;
+}
