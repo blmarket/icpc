b4185208bdfc2274f5b937bc79d4094fbc41de0d
Jeong, Heon <blmarket@gmail.com>
2014-03-02 00:45:40 +0900

    --Empty Message--

--- blmarket/codeforces/R233Div2/A.cc
+++ blmarket/codeforces/R233Div2/A.cc
@@ -17,20 +17,37 @@ template<typename T> int size(const T &a) { return a.size(); }
 
 typedef pair<int, int> PII;
 
-int main(void) {
-    int n,p,k;
-    scanf("%d %d %d",&n,&p,&k);
-    ostringstream ost;
-    for(int i=p-k;i<=p+k;i++) {
-        if(i < 1 || i >n) continue;
-        if(i == p) {
-            ost << "(" << i << ") ";
-        } else ost << i << " ";
-    }
-    if(p-k > 1) cout << "<< ";
-    cout << ost.str();
-    if(p+k < n) cout << ">>";
-    cout << endl;
+long long arr1[10005];
+long long arr2[10005];
 
+int main(void) {
+    int a,b;
+    scanf("%d %d",&a, &b);
+
+    for(int i=1;i<=a;i++) {
+        int big = a-i+1;
+        arr1[i] = (long long)big * big + (i - 1);
+    }
+
+    for(int i=1;i<=b;i++) {
+        int av = b / i;
+        int ax = b % i;
+
+        arr2[i] = (long long)ax * (av+1) * (av+1) + (long long)(i-ax) * av * av;
+    }
+
+    if(a == 0 || b <= 1) {
+        cout << arr1[a] - arr2[b] << endl;
+        return 0;
+    }
+
+    long long ret = arr1[1] + arr2[1];
+    for(int i=2;i<=b;i++) {
+        int na = i - 1;
+        long long tmp = arr1[na] - arr2[i];
+        if(ret < tmp) ret = tmp;
+    }
+
+    cout << ret << endl;
     return 0;
 }
