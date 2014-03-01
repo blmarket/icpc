3c96678c49d5a68e05c64ce9902c01c466f5ca96
Jeong, Heon <blmarket@gmail.com>
2014-03-01 14:47:31 +0900

    --Empty Message--

---
+++ blmarket/codeforces/R232Div2/D.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+typedef pair<int, int> PII;
+
+int mod = 1000000007;
+
+int n;
+vector<int> primes;
+vector<int> np;
+map<int, int> px;
+
+long long inv[505];
+
+int main(void) {
+    primes.pb(2);
+    for(int i=3;i<=100000;i+=2) {
+        bool pp = true;
+        for(int j=0;j<size(primes);j++) {
+            if(primes[j]*primes[j] > i) break;
+            if(i % primes[j] == 0) { pp = false; break; }
+        }
+        if(pp) primes.pb(i);
+    }
+
+    int t;
+    scanf("%d", &t);
+    for(int i=0;i<t;i++) {
+        int n;
+        scanf("%d", &n);
+        int nn = n;
+        for(;;nn++) {
+            bool isprime = true;
+            for(int j=0;j<size(primes);j++) {
+                if(nn % primes[j] == 0) { isprime = false; break; }
+            }
+            if(isprime) break;
+        }
+        cout << "1/2" << " - 1/" << nn << endl;
+    }
+    
+    return 0;
+}
