f7b202a0af50bd4307142ca503f949adbf917b8b
Jeong, Heon <blmarket@gmail.com>
2014-03-01 10:34:25 +0900

    --Empty Message--

--- blmarket/codeforces/R232Div2/E.cc
+++ blmarket/codeforces/R232Div2/E.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 #include <vector>
 #include <cstdio>
 #include <cstring>
@@ -10,11 +11,16 @@ using namespace std;
 
 typedef pair<int, int> PII;
 
-int tree[30005];
+const int mod = 1000000007;
+
+int depth[300005];
 vector<int> childs[300005];
 
 int pos = 0;
 PII ranges[300005];
+
+int seg1[1024800];
+int seg2[1024800];
 
 PII go(int a) {
     int mypos = pos++;
@@ -24,22 +30,33 @@ PII go(int a) {
     return ranges[a] = mp(mypos, pos);
 }
 
+int addseg(int *arr, int val, int s, int e) {
+    function<void(int, int, int)> func = [&](int pos, int a, int b) {
+        if(a >= s && b <= e) {
+            arr[pos] += val;
+            return;
+        }
+        func(pos*2, a, (a+b)/2);
+        func(pos*2+1, (a+b)/2, b);
+    };
+    func(1, 0, 524288);
+};
+
 int main(void) {
     int n;
     scanf("%d", &n);
+    depth[1] = 0;
     for(int i=2;i<=n;i++) {
-        scanf("%d", &tree[i]);
-        childs[tree[i]].pb(i);
+        int tmp;
+        scanf("%d", &tmp);
+        depth[i] = depth[tmp] + 1;
+        childs[tmp].pb(i);
     }
 
     go(1);
     for(int i=1;i<=n;i++) {
         cout << ranges[i].first << "," << ranges[i].second << endl;
     }
-    return 0;
-
-
-
 
     int m;
     scanf("%d", &m);
@@ -49,13 +66,23 @@ int main(void) {
         if(op == 1) {
             int a,b,c;
             scanf("%d %d %d", &a, &b, &c);
+            addseg(seg1, b + c * depth[a], ranges[a].first, ranges[a].second);
+            addseg(seg2, c, ranges[a].first, ranges[a].second);
         } else {
             int a;
-            long long ret = 0;
             scanf("%d", &a);
+            int pos = ranges[a].first;
+            long long sum = 0;
+
+            do {
+                sum += seg1[pos];
+                sum -= seg2[pos] * depth[a];
+                pos /= 2;
+            } while(pos > 1);
+            sum %= mod;
+            if(sum < mod) sum += mod;
+            cout << sum << endl;
         }
     }
-
-
     return 0;
 }
