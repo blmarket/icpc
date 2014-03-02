de3bb5c2cdeb7b56fa6a033593d60abef5ee0feb
Jeong, Heon <blmarket@gmail.com>
2014-03-02 09:55:38 +0900

    --Empty Message--

---
+++ blmarket/NumbersAndMatches.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+const int nums[10][7] = { { 1,1,1,0,1,1,1 }, {0,0,1,0,0,1,1}, {1,0,1,1,1,0,1}, {1,0,1,1,0,1,1}, {0,1,1,1,0,1,0}, {1,1,0,1,0,1,1}, {1,1,0,1,1,1,1}, {1,0,1,0,0,1,0}, {1,1,1,1,1,1,1}, {1,1,1,1,0,1,1} };
+int nmove[10][10];
+int ndiff[10][10];
+
+class NumbersAndMatches 
+{
+public:
+    long long differentNumbers(long long N, int K) 
+    {		
+        for(int i=0;i<10;i++) {
+            for(int j=0;j<10;j++) {
+                int nadd = 0;
+                int nrem = 0;
+                for(int k=0;k<7;k++) {
+                    nadd += nums[j][k] && !nums[i][k];
+                    nrem += !nums[j][k] && nums[i][k];
+                }
+                nmove[i][j] = nadd + nrem;
+                ndiff[i][j] = nadd - nrem;
+            }
+        }
+
+        for(int i=0;i<10;i++) {
+            for(int j=0;j<10;j++) cout << nmove[i][j] << " ";
+            cout << endl;
+        }
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { long long Arg0 = 10LL; int Arg1 = 1; long long Arg2 = 4LL; verify_case(0, Arg2, differentNumbers(Arg0, Arg1)); }
+	void test_case_1() { long long Arg0 = 23LL; int Arg1 = 1; long long Arg2 = 4LL; verify_case(1, Arg2, differentNumbers(Arg0, Arg1)); }
+	void test_case_2() { long long Arg0 = 66LL; int Arg1 = 2; long long Arg2 = 15LL; verify_case(2, Arg2, differentNumbers(Arg0, Arg1)); }
+	void test_case_3() { long long Arg0 = 888888888LL; int Arg1 = 100; long long Arg2 = 1LL; verify_case(3, Arg2, differentNumbers(Arg0, Arg1)); }
+	void test_case_4() { long long Arg0 = 444444444444444444LL; int Arg1 = 2; long long Arg2 = 1LL; verify_case(4, Arg2, differentNumbers(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    NumbersAndMatches ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
