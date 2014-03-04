f987c1f24f5dd324da0e74ee4394ff3fbdcbb48f
Jeong, Heon <blmarket@gmail.com>
2014-03-04 21:38:00 +0900

    --Empty Message--

--- blmarket/LCMSet.cpp
+++ blmarket/LCMSet.cpp
@@ -22,12 +22,31 @@ template<typename T> int size(const T &a) { return a.size(); }
 
 vector<int> primes;
 
-bool build(vector<int> &a, int t) {
-    int tmp = t;
-    for(int i=0;i<size(a);i++) if(t % a[i] == 0) {
-        tmp = tmp / __gcd(tmp, a[i]);
+bool chk(vector<int> &a, int d, int t) {
+    for(int i=0;i<size(a);i++) {
+        int tmp = a[i];
+        int tt = 0;
+        while(tmp % d == 0) {
+            tmp /= d;
+            tt++;
+        }
+        if(tt == t) return true;
     }
-    return tmp == 1;
+    return false;
+}
+
+bool build(vector<int> &a, int t) {
+    bool ret = true;
+    for(int i=0;i<size(primes);i++) {
+        int tt = 0;
+        while(t % primes[i] == 0) {
+            t /= primes[i];
+            tt++;
+        }
+        if(tt) ret &= chk(a, primes[i], tt);
+    }
+    if(t > 1) ret &= chk(a, t, 1);
+    return ret;
 }
 
 class LCMSet 
@@ -62,7 +81,7 @@ public:
 	private:
 	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
 	void verify_case(int Case, const string &Expected, const string &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
-	void test_case_0() { int Arr0[] = {2,3,4,12}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2,3,4,6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "Equal"; verify_case(0, Arg2, equal(Arg0, Arg1)); }
+	void test_case_0() { int Arr0[] = {2,4,3,9}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {12,18}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "Not equal"; verify_case(0, Arg2, equal(Arg0, Arg1)); }
 	void test_case_1() { int Arr0[] = {4,9}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {6,36}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "Not equal"; verify_case(1, Arg2, equal(Arg0, Arg1)); }
 	void test_case_2() { int Arr0[] = {2,3,5,7,14,105}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2,3,5,6,7,30,35}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "Equal"; verify_case(2, Arg2, equal(Arg0, Arg1)); }
 	void test_case_3() { int Arr0[] = {2,3,5,7,14,105}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2,3,5,6,7,30,36}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); string Arg2 = "Not equal"; verify_case(3, Arg2, equal(Arg0, Arg1)); }
@@ -78,6 +97,6 @@ public:
 int main()
 {
     LCMSet ___test; 
-    ___test.run_test(-1); 
+    ___test.run_test(0); 
 } 
 // END CUT HERE
