a4b44c0c0031cf74f28cbd3ebf946dad88cd16e2
Jeong, Heon <blmarket@gmail.com>
2014-03-04 20:48:01 +0900

    --Empty Message--

---
+++ blmarket/TheAlmostLuckyNumbersDivOne.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+long long aa, bb;
+long long ret = 0;
+
+void gen(int a, int b, long long cur) {
+    if(a == 0) {
+        if(cur >= aa && cur <= bb) ret++;
+        return;
+    }
+    if(b == 1) {
+        gen(a-1, 1, cur * 10 + 4);
+        gen(a-1, 1, cur * 10 + 7);
+    } else {
+        for(int i=0;i<10;i++) {
+            gen(a-1, (i!=4) && (i!=7), cur * 10 + i);
+        }
+    }
+}
+
+class TheAlmostLuckyNumbersDivOne 
+{
+public:
+    long long find(long long a, long long b) 
+    {
+        aa=a;bb=b;
+        for(int i=1;i<=16;i++) {
+            for(int j=1;j<10;j++) {
+                gen(i-1, (j!=4) && (j!=7), j);
+            }
+        }
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { long long Arg0 = 4LL; long long Arg1 = 7LL; long long Arg2 = 4LL; verify_case(0, Arg2, find(Arg0, Arg1)); }
+	void test_case_1() { long long Arg0 = 8LL; long long Arg1 = 19LL; long long Arg2 = 4LL; verify_case(1, Arg2, find(Arg0, Arg1)); }
+	void test_case_2() { long long Arg0 = 28LL; long long Arg1 = 33LL; long long Arg2 = 0LL; verify_case(2, Arg2, find(Arg0, Arg1)); }
+	void test_case_3() { long long Arg0 = 12345678900LL; long long Arg1 = 98765432100LL; long long Arg2 = 91136LL; verify_case(3, Arg2, find(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    TheAlmostLuckyNumbersDivOne ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
