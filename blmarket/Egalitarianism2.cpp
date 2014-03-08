bf58aaf93336da7704156245b67ba7f43f51675c
Jeong, Heon <blmarket@gmail.com>
2014-03-08 20:47:41 +0900

    --Empty Message--

--- blmarket/Egalitarianism2.cpp
+++ blmarket/Egalitarianism2.cpp
@@ -74,6 +74,8 @@ public:
                 sum += d;
             }
 
+            cout << m << " " << sqsum << " " << sum << endl;
+
             double ret = sqsum / (n-1) - sqr(sum / (n-1));
             return ret;
         };
