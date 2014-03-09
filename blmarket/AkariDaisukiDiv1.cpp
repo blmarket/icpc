1d4d95813fd04cf13d9fd7e0b7d8b5b4b21a98f9
Jeong, Heon <blmarket@gmail.com>
2014-03-09 22:24:56 +0900

    --Empty Message--

--- blmarket/AkariDaisukiDiv1.cpp
+++ blmarket/AkariDaisukiDiv1.cpp
@@ -88,6 +88,7 @@ public:
         }
 
         int p2 = pow2(k);
+        cout << pow2(1) << " " << pow2(2) << " " << pow2(3) << endl;
         cout << p2 << endl;
         cc += tmp;
         cc = (cc * p2) % mod;
