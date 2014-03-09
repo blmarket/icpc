c25edb9aae4eb3cc0119d8ea50aefb4aacffa5c9
Jeong, Heon <blmarket@gmail.com>
2014-03-09 18:12:00 +0900

    --Empty Message--

---
+++ blmarket/NoRepeatPlaylist.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+const int mod = 1000000007;
+
+int go(int a, int b, int c) {
+    long long ret = 1;
+    for(int i=0;i<c;i++) {
+        int nc = max(a-i, a-b);
+        ret = (ret * nc) % mod;
+    }
+    return ret;
+}
+
+class NoRepeatPlaylist 
+{
+public:
+    int numPlaylists(int N, int M, int P) 
+    {
+        int ret = 0;
+        bool flag = false;
+        for(int i=N;i>=1;i--) {
+            int tmp = go(i, M, P);
+            if(flag) tmp = -tmp;
+            ret = (ret + tmp) % mod;
+        }
+        if(ret < 0) ret += mod;
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 1; int Arg1 = 0; int Arg2 = 3; int Arg3 = 1; verify_case(0, Arg3, numPlaylists(Arg0, Arg1, Arg2)); }
+	void test_case_1() { int Arg0 = 1; int Arg1 = 1; int Arg2 = 3; int Arg3 = 0; verify_case(1, Arg3, numPlaylists(Arg0, Arg1, Arg2)); }
+	void test_case_2() { int Arg0 = 2; int Arg1 = 0; int Arg2 = 3; int Arg3 = 6; verify_case(2, Arg3, numPlaylists(Arg0, Arg1, Arg2)); }
+	void test_case_3() { int Arg0 = 4; int Arg1 = 0; int Arg2 = 4; int Arg3 = 24; verify_case(3, Arg3, numPlaylists(Arg0, Arg1, Arg2)); }
+	void test_case_4() { int Arg0 = 2; int Arg1 = 1; int Arg2 = 4; int Arg3 = 2; verify_case(4, Arg3, numPlaylists(Arg0, Arg1, Arg2)); }
+	void test_case_5() { int Arg0 = 50; int Arg1 = 5; int Arg2 = 100; int Arg3 = 222288991; verify_case(5, Arg3, numPlaylists(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    NoRepeatPlaylist ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
