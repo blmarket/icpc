de9adf688cf938dd3ca90e13aaf91fdeac285e5e
Jeong, Heon <blmarket@gmail.com>
2014-04-15 22:23:01 +0900

    --Empty Message--

--- blmarket/ColorfulCoins.cpp
+++ blmarket/ColorfulCoins.cpp
@@ -20,11 +20,40 @@ typedef pair<int,int> PII;
 
 template<typename T> int size(const T &a) { return a.size(); }
 
+/**
+ * Problem: 500
+ * Test Case: 6
+ * Succeeded: No
+ * Execution Time: 23 ms
+ * Args:
+ * {{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488}}
+ *
+ * Expected:
+ * 6
+ *
+ * Received:
+ * 7
+ *
+ * Answer checking result:
+ * Returned value must exactly match the expected one.
+ */
+
 map<long long, int> M;
 
-int go(long long dim, int kk) {
-    if(kk == 1) return 0;
-    return go(dim, (dim+kk-1)/dim) + 1;
+int know(int num, long long ng) {
+    if(num == 1) return 0;
+    int gs = (num + ng - 1) / ng;
+    return know(gs, ng) + 1;
+}
+
+int unkn(int num, long long ng) {
+    if(num == 1) return 1;
+    int gs = (num + ng - 1) / ng;
+    if(num % ng == 0) {
+        return unkn(gs, ng) + 1;
+    } else {
+        return max(know(gs, ng), unkn(gs-1, ng)) + 1;
+    }
 }
 
 class ColorfulCoins 
@@ -41,7 +70,8 @@ public:
 
         int ret = 1;
         for(auto &a : M) {
-            ret = max(ret, 1+go(a.first, a.second));
+            // cout << a.first << " " << a.second << " = " << go(a.first, a.second) << endl;
+            ret = max(ret, unkn(a.first, a.second));
         }
         return ret;
     }
