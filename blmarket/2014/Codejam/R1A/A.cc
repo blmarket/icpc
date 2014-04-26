cb2fd5e3eb411613d312af727e158abd2e0a08b8
Jeong, Heon <blmarket@gmail.com>
2014-04-26 10:23:16 +0900

    --Empty Message--

--- blmarket/2014/Codejam/R1A/A.cc
+++ blmarket/2014/Codejam/R1A/A.cc
@@ -79,6 +79,7 @@ void solve(int dataId)
     }
 
     vector<long long> V1(arr1, arr1+N), V2(arr2, arr2+N);
+    cout << "HERE" << endl;
     long long ret = go(V1, V2);
     if(ret == -1) {
         cout << "NOT POSSIBLE" << endl;
