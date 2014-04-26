4cd98d46fa38493ac9b1ec4ed84a257937b3e82c
Jeong, Heon <blmarket@gmail.com>
2014-04-26 10:49:55 +0900

    --Empty Message--

--- blmarket/2014/Codejam/R1A/D.cc
+++ blmarket/2014/Codejam/R1A/D.cc
@@ -16,7 +16,7 @@ int main(void) {
         printf("Case #%d: ", i+1);
         for(int j=0;j<10;j++) {
             long long sum = 0;
-            for(int k=(j-1)*100;k<j*100;k++)
+            for(int k=j*100;k<(j+1)*100;k++)
                 sum += arr[k];
             cout << sum << " ";
         }
