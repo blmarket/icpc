5ea5434711df78cd5aa7e69b5a19dce1f756be6f
Jeong, Heon <blmarket@gmail.com>
2014-04-26 10:44:01 +0900

    --Empty Message--

---
+++ blmarket/2014/Codejam/R1A/D.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1024];
+
+int main(void) {
+    int T;
+    cin >> T;
+    for(int i=0;i<T;i++) {
+        int N;
+        cin >> N;
+        int same = 0;
+        for(int j=0;j<N;j++) {
+            cin >> arr[j];
+            if(j == arr[j]) same++;
+        }
+        printf("Case #%d: ", i+1);
+        if(same > 100) {
+            printf("BAD\n");
+        } else {
+            printf("GOOD\n");
+        }
+    }
+}
