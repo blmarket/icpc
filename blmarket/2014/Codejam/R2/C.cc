60531d5381cc81d9c350ed5fcb1570283d562a63
Jeong, Heon <blmarket@gmail.com>
2014-05-31 23:34:18 +0900

    --Empty Message--

--- blmarket/2014/Codejam/R2/C.cc
+++ blmarket/2014/Codejam/R2/C.cc
@@ -37,6 +37,7 @@ int dx[4] = {-1,0,0,1};
 int dy[4] = {0,-1,1,0};
 
 bool try_flow(int x, int y) {
+    cout << x << "," << y << endl;
     if(x == -1 || y == -1) return false;
     if(y == H-1 && used[x][y] == false) {
         used[x][y] = true;
