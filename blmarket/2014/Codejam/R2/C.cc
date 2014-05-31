b49bf702b24ee417bf6687bb1b4618c1f35e54f1
Jeong, Heon <blmarket@gmail.com>
2014-06-01 01:02:59 +0900

    --Empty Message--

--- blmarket/2014/Codejam/R2/C.cc
+++ blmarket/2014/Codejam/R2/C.cc
@@ -80,8 +80,7 @@ void solve(int dataId)
     for(int i=0;i<W;i++) {
         memset(visit, 0, sizeof(visit));
         if(try_flow(i, 0)) ret++;
-        cout << back[4][2].first << " " << back[4][2].second << endl;
-        cout << back[5][1].first << " " << back[5][1].second << endl;
+        if(ret == 3) break;
     }
 
     cout << endl;
