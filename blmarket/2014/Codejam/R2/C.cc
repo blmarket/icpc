d17388c3c56b2a24266db774a404c1d1e1b3f556
Jeong, Heon <blmarket@gmail.com>
2014-06-01 00:43:38 +0900

    --Empty Message--

--- blmarket/2014/Codejam/R2/C.cc
+++ blmarket/2014/Codejam/R2/C.cc
@@ -38,6 +38,7 @@ int dy[4] = {0,-1,1,0};
 const char *dir = "<V^>";
 
 bool try_flow(int x, int y) {
+    cout << "HERE" << endl;
     if(y == H-1) return true;
     if(visit[x][y]) return false;
     visit[x][y] = true;
@@ -65,11 +66,12 @@ void solve(int dataId)
 {
     printf("Case #%d: ", dataId);
     int ret = 0;
+
     for(int i=0;i<W;i++) {
         memset(visit, 0, sizeof(visit));
         if(try_flow(i, 0)) ret++;
     }
-    cout << ret << endl;
+
     for(int i=H-1;i>=0;i--) {
         for(int j=0;j<W;j++) {
             if(!used[j][i]) {
