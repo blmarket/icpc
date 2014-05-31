2b06788d1560870586df02857ec828d5fd8da9cc
Jeong, Heon <blmarket@gmail.com>
2014-06-01 00:06:43 +0900

    --Empty Message--

--- blmarket/2014/Codejam/R2/C.cc
+++ blmarket/2014/Codejam/R2/C.cc
@@ -91,7 +91,7 @@ void solve(int dataId)
         if(try_flow(x, y)) {
             ret++;
             cout << endl;
-            if(ret == 2) break;
+            if(ret == 3) break;
         }
     }
     for(int i=H-1;i>=0;i--) {
