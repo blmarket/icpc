d6c3540835481b149ef1ea2bbdf70d9d5b8251c2
Jeong, Heon <blmarket@gmail.com>
2014-05-31 19:05:06 +0900

    --Empty Message--

---
+++ blmarket/BuildingRoutes.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int dd[55][55];
+int ff[55][55];
+
+class BuildingRoutes 
+{
+public:
+    int build(vector <string> dist, int T) 
+    {		
+        int n = size(dist);
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) dd[i][j] = (dist[i][j] - '0');
+        }
+
+        memcpy(ff, dd, sizeof(dd));
+
+        for(int k=0;k<n;k++) {
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
+                    if(dd[i][j] > dd[i][k] + dd[k][j]) {
+                        dd[i][j] = dd[i][k] + dd[k][j];
+                    }
+                }
+            }
+        }
+
+        int ret = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) if(i != j) {
+                int cnt = 0;
+                for(int a=0;a<n;a++) {
+                    for(int b=0;b<n;b++) if(a != b) {
+                        if(dd[a][b] == dd[a][i] + ff[i][j] + dd[j][b]) {
+                            cnt++;
+                        }
+                    }
+                }
+                if(cnt >= T) {
+                    ret += ff[i][j];
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"011",
+ "101",
+ "110"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 6; verify_case(0, Arg2, build(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {"033",
+ "309",
+ "390"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 12; verify_case(1, Arg2, build(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"0123",
+ "1023",
+ "1203",
+ "1230"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; int Arg2 = 5; verify_case(2, Arg2, build(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {"05789654",
+ "10347583",
+ "65085479",
+ "55602398",
+ "76590934",
+ "57939045",
+ "12345608",
+ "68647640"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 40; verify_case(3, Arg2, build(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    BuildingRoutes ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
