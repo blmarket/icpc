15d42d3804e25ddb9fb27e291e8c585ae0f3033d
Jeong, Heon <heon.jeong@sk.com>
2014-06-02 15:39:55 +0900

    --Empty Message--

--- blmarket/2014/practice/june/B.cc
+++ blmarket/2014/practice/june/B.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <sys/wait.h>
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
@@ -24,31 +23,6 @@ typedef vector<string> VS;
 typedef pair<int,int> PII;
 
 template<typename T> int size(const T &a) { return a.size(); } 
-
-namespace aux{
-    template<std::size_t...> struct seq{};
-
-    template<std::size_t N, std::size_t... Is>
-        struct gen_seq : gen_seq<N-1, N-1, Is...>{};
-
-    template<std::size_t... Is>
-        struct gen_seq<0, Is...> : seq<Is...>{};
-
-    template<class Ch, class Tr, class Tuple, std::size_t... Is>
-        void print_tuple(std::basic_ostream<Ch,Tr>& os, Tuple const& t, seq<Is...>){
-            using swallow = int[];
-            (void)swallow{0, (void(os << (Is == 0? "" : ", ") << std::get<Is>(t)), 0)...};
-        }
-} // aux::
-
-    template<class Ch, class Tr, class... Args>
-auto operator<<(std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& t)
-    -> std::basic_ostream<Ch, Tr>&
-{
-    os << "(";
-    aux::print_tuple(os, t, aux::gen_seq<sizeof...(Args)>());
-    return os << ")";
-}
 
 int N, M;
 int st[105], et[105], sn[105];
