8fd9a10d6d4df1fa7852f3232073262c5aebbc49
Jeong, Heon <blmarket@gmail.com>
2014-06-08 01:09:13 +0900

    --Empty Message--

---
+++ blmarket/SwitchingGame.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int M;
+int ret;
+
+string go(string A, string B) {
+    bool needOn = false;
+    bool needOff = false;
+    for(int i=0;i<M;i++) {
+        if(A[i] == '-' && B[i] == '+') {
+            needOn = true;
+        }
+        if(A[i] == '+' && B[i] == '-') needOff = true;
+    }
+
+    ret += needOn + needOff;
+
+    for(int i=0;i<M;i++) if(B[i] == '?') {
+        switch(A[i]) {
+            case 'A':
+                B[i] = 'A';
+                break;
+            case '+':
+                if(needOff) B[i] = 'A'; else B[i] = '+';
+                break;
+            case '-':
+                if(needOn) B[i] = 'A'; else B[i] = '-';
+                break;
+        }
+    }
+    return B;
+}
+
+class SwitchingGame 
+{
+public:
+    int timeToWin(vector <string> states) 
+    {
+        M = size(states[0]);
+        ret = 0;
+        string cur(M, '-');
+        for(int i=0;i<size(states);i++) {
+            cur = go(cur, states[i]);
+        }
+        return ret;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"++--",
+ "--++"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 5; verify_case(0, Arg1, timeToWin(Arg0)); }
+	void test_case_1() { string Arr0[] = {"+-++",
+ "+-++"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; verify_case(1, Arg1, timeToWin(Arg0)); }
+	void test_case_2() { string Arr0[] = {"++",
+ "+?",
+ "?+",
+ "++"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 5; verify_case(2, Arg1, timeToWin(Arg0)); }
+	void test_case_3() { string Arr0[] = {"+", 
+ "?",
+ "?",
+ "?",
+ "-"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 7; verify_case(3, Arg1, timeToWin(Arg0)); }
+	void test_case_4() { string Arr0[] = {"+??+++",
+ "++??+-",
+ "?++??+",
+ "?-+-??",
+ "??+?++",
+ "++-?+?",
+ "?++?-+",
+ "?--+++",
+ "-??-?+"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 20; verify_case(4, Arg1, timeToWin(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    SwitchingGame ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
