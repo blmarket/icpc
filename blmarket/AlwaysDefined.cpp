e6d046146bae19830cd0e96927a2f3e677ff69ae
Jeong, Heon <blmarket@gmail.com>
2014-06-08 02:10:21 +0900

    --Empty Message--

--- blmarket/AlwaysDefined.cpp
+++ blmarket/AlwaysDefined.cpp
@@ -34,7 +34,7 @@ long long go2(long long A, int W, int R) {
     long long k = (A - R + W) / W;
     int t = __gcd(W, R);
     long long ret = k / t;
-    for(int i=1;i<t;i++) {
+    for(int i=1;i<R;i++) {
         if((R + (W*i)) % R) continue;
         int RR = (R + W * i) / R;
         ret += go2(A/R, W, RR);
