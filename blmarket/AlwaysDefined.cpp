edf1b30dc03abd2e61c890a7b52ae2b6f6eb0d7f
Jeong, Heon <blmarket@gmail.com>
2014-06-08 09:07:54 +0900

    --Empty Message--

--- blmarket/AlwaysDefined.cpp
+++ blmarket/AlwaysDefined.cpp
@@ -20,10 +20,34 @@ typedef pair<int,int> PII;
 
 template<typename T> int size(const T &a) { return a.size(); }
 
+/*
+ * Problem: 900
+ * Test Case: 74
+ * Succeeded: No
+ * Execution Time: 0 ms
+ * Peak memory used: 0.000MB
+ * Args:
+ * {52654107011850814, 744472477227325439, 2028}
+ *
+ * Expected:
+ * 2819463395911898
+ *
+ * Received:
+ * The code execution time exceeded the 2.000 second time limit.
+ *
+ * Answer checking result:
+ * null
+ */
+
+map<pair<long long, int>, long long> memo;
+
 int W;
 
 long long go2(long long maxk, int r) {
     if(r == 1) return maxk+1;
+    auto key = mp(maxk, r);
+    auto it = memo.find(key);
+    if(it != memo.end()) return it->second;
     long long ret = 0;
     // k W + r
     for(int i=0;i<r;i++) {
@@ -35,7 +59,7 @@ long long go2(long long maxk, int r) {
         ret += go2(maxw, nr);
     }
     // cout << maxk << " " << r << " = " << ret << endl;
-    return ret;
+    return memo[key] = ret;
 }
 
 long long go(long long A) {
@@ -53,6 +77,7 @@ class AlwaysDefined
 public:
     long long countIntegers(long long L, long long R, int W_) 
     {		
+        memo.clear();
         W = W_;
         return go(R) - go(L-1);
     }
