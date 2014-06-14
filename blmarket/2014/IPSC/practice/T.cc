f3765d3588c6f3740ce860e64021a0e9f4d58eb2
Jeong, Heon <blmarket@gmail.com>
2014-06-14 21:45:02 +0900

    --Empty Message--

--- blmarket/2014/IPSC/practice/T.cc
+++ blmarket/2014/IPSC/practice/T.cc
@@ -55,39 +55,31 @@ void process(int dataId)
 
     memset(marker, 0, sizeof(marker));
 
-    int last = -1;
+    long long cnt = 0;
+    int cntlast = -1;
+    int marklast = -1;
     for(int i=0;i<size(V);i++) {
         long long s = V[i].first;
         long long e = V[i].second - K;
-        cout << s << " " << e << endl;
         int p1 = lower_bound(ps.begin(), ps.end(), s) - ps.begin();
         int p2 = lower_bound(ps.begin(), ps.end(), e) - ps.begin();
-        p1 = max(p1, last);
+        p1 = max(p1, cntlast);
         for(int i=p1;i<p2;i++) {
-            marker[i] |= 1;
+            if(marker[i]) {
+                cnt += (ps[i+1] - ps[i]);
+            }
         }
-        last = p2 - 1;
-    }
+        cntlast = max(cntlast, p2);
 
-    last = -1;
-    for(int i=0;i<size(V);i++) {
-        long long s = V[i].first + K;
-        long long e = V[i].second;
-        int p1 = lower_bound(ps.begin(), ps.end(), s) - ps.begin();
-        int p2 = lower_bound(ps.begin(), ps.end(), e) - ps.begin();
-        p1 = max(p1, last);
+        s = V[i].first + K;
+        e = V[i].second;
+        p1 = lower_bound(ps.begin(), ps.end(), s) - ps.begin();
+        p2 = lower_bound(ps.begin(), ps.end(), e) - ps.begin();
+        p1 = max(p1, marklast);
         for(int i=p1;i<p2;i++) {
-            marker[i] |= 2;
+            marker[i] = 1;
         }
-        last = p2 - 1;
-    }
-
-    long long cnt = 0;
-    for(int i=0;i+1<size(ps);i++) {
-        cout << ps[i] << "-" << ps[i+1] << " = " << (int)marker[i] << endl;
-        if(marker[i] == 3) {
-            cnt += (ps[i+1] - ps[i]);
-        }
+        marklast = max(marklast, p2);
     }
 
     cout << L - cnt << endl;
