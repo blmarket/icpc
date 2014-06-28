7a2c3f47292e7b722b7227c3eaef016e40b166bc
Jeong, Heon <heon.jeong@sk.com>
2014-06-28 13:21:56 +0900

    --Empty Message--

--- blmarket/2014/ICPC/EE.cc
+++ blmarket/2014/ICPC/EE.cc
@@ -44,7 +44,6 @@ vector<int> get_sig(int s, int e) {
 
 void set_label(int s, int e, int l) {
     int sp = find(rr[s].begin(), rr[s].end(), e) - rr[s].begin();
-    cout << sp << endl;
     label[s][sp] = l;
 }
 
@@ -82,6 +81,7 @@ int main(void) {
                     mm++;
                 }
             }
+            cout << "HERE" << endl;
             ls[i].clear();
         }
     }
