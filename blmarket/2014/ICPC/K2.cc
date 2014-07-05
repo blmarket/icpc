8422986bd3c4557a9d56dd3c25bdba2aba5508bb
Jeong, Heon <heon.jeong@sk.com>
2014-07-06 08:20:54 +0900

    --Empty Message--

---
+++ blmarket/2014/ICPC/K2.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int N, M;
+vector<PII> V;
+
+int main(void) {
+    scanf("%d %d", &N, &M);
+    V.clear();
+    for(int i=0;i<M;i++) {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        if(a < b) a += N;
+        V.pb(mp(a,b));
+    }
+    sort(V.begin(), V.end());
+    for(int i=0;i<size(V);i++) {
+        cout << V[i].first << "," << V[i].second << endl;
+        while(i+1 < size(V)) {
+            if(V[i+1].second > V[i].second) break;
+            V.erase(V.begin() + i+1);
+        }
+    }
+
+    return 0;
+}
