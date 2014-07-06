ac2d29d04619561af0cde5be87ad56d62e324a87
Jeong, Heon <heon.jeong@sk.com>
2014-07-06 13:42:12 +0900

    --Empty Message--

--- blmarket/2014/ICPC/K3.cc
+++ blmarket/2014/ICPC/K3.cc
@@ -50,6 +50,7 @@ int main(void) {
         V.pb(mp(a,b));
     }
     sort(V.begin(), V.end());
+    cout << size(V) << endl;
     // cout << V.back().first << " " << V.back().second << endl;
     
     int reach = V[0].second + 1;
