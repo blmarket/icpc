c0284fffcaff9af42725e75beba14e2484b8d3b8
Jeong, Heon <heon.jeong@sk.com>
2014-07-12 15:01:25 +0900

    --Empty Message--

--- blmarket/BlockTheBlockPuzzle.cpp
+++ blmarket/BlockTheBlockPuzzle.cpp
@@ -22,6 +22,8 @@ template<typename T> int size(const T &a) { return a.size(); }
 
 vector<string> board;
 int ls = 0;
+vector<PII> links[5500];
+bool sink[5500];
 
 template<typename T> int label(const T &a) {
     static map<T, int> M;
@@ -29,30 +31,65 @@ template<typename T> int label(const T &a) {
     return M[a] = ls++;
 }
 
+const int dx[4] = {0,-1,1,0};
+const int dy[4] = {-1,0,0,1};
+
 class BlockTheBlockPuzzle 
 {
 public:
     int minimumHoles(vector <string> board_) 
     {		
-        int dx, dy;
+        int px, py;
         board = board_;
+        int n = size(board);
+        int sp;
         for(int i=0;i<size(board);i++) {
             for(int j=0;j<size(board[i]);j++) if(board[i][j] == '$') {
-                dx = (i % 3);
-                dy = (j % 3);
+                sp = label(make_tuple(i,j,1));
+                px = (i % 3);
+                py = (j % 3);
                 break;
             }
         }
 
         for(int i=0;i<size(board);i++) for(int j=0;j<size(board);j++) {
-            if((i-dx) % 3 == 0 && (j - dy) % 3 == 0) {
+            if((i-px) % 3 == 0 && (j - py) % 3 == 0) if(board[i][j] != 'H') {
                 int n1 = label(make_tuple(i, j, 0));
                 int n2 = label(make_tuple(i, j, 1));
-                cout << n1 << " " << n2 << endl;
+                if(board[i][j] == 'b') sink[n1] = true;
+                links[n1].pb(mp(n2, 1));
+
+                for(int k=0;k<4;k++) {
+                    int nx = i + dx[k] * 3;
+                    int ny = j + dy[k] * 3;
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+
+                    char c1 = board[i+dx[k]][j+dy[k]];
+                    char c2 = board[i+dx[k]*2][j+dy[k]*2];
+                    int flow = 0;
+                    if(c1 == 'b' || c2 == 'b') flow = 50000;
+                    flow += (c1 == '.') + (c2 == '.');
+
+                    int n3 = label(make_tuple(nx, ny, 0));
+                    links[n2].pb(mp(n3, flow));
+                }
             }
         }
 
-        return 0;
+        int ret = 0;
+        bool visit[5500];
+
+        function<int(int)> try_flow = [&](int sp) -> int {
+            return 0;
+        };
+        while(true) {
+            memset(visit, 0, sizeof(visit));
+            int tmp = try_flow(sp);
+            if(tmp == 0) break;
+            if(tmp > 100) return -1;
+            ret += tmp;
+        }
+        return ret;
     }
 
     
