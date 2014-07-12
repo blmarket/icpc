cd4f69211369aa4f74f657700856024e2d11949e
Jeong, Heon <heon.jeong@sk.com>
2014-07-12 14:35:11 +0900

    --Empty Message--

---
+++ blmarket/BlockTheBlockPuzzle.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define pb push_back
+#define sqr(x) ((x)*(x))
+#define foreach(it,c) for(typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+vector<string> board;
+int ls = 0;
+
+map<PII, int> m1;
+map<tuple<int, int, int, int>, int> m2;
+
+int label1(int a, int b) {
+    PII key = { a, b };
+}
+
+int label2(int a, int b, int c, int d) {
+}
+
+int start(int a, int b) {
+}
+
+class BlockTheBlockPuzzle 
+{
+public:
+    int minimumHoles(vector <string> board_) 
+    {		
+        board = board_;
+        for(int i=0;i<size(board);i++) {
+            for(int j=0;j<size(board[i]);j++) if(board[i][j] == '$') {
+                return start(i, j);
+            }
+        }
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"b..$",
+ "....",
+ "HHHH",
+ "HHHH"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; verify_case(0, Arg1, minimumHoles(Arg0)); }
+	void test_case_1() { string Arr0[] = {"............H..",
+ "...............",
+ "...............",
+ "HHH$HHH.....H..",
+ "HHHHHHH........",
+ "HHHHHHHH.......",
+ "......b..H.....",
+ "...............",
+ "...............",
+ "...H..H..H.....",
+ "...............",
+ "...............",
+ "...............",
+ "...............",
+ "..............."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(1, Arg1, minimumHoles(Arg0)); }
+	void test_case_2() { string Arr0[] = {"............H..",
+ "...............",
+ "...............",
+ "HHH$HHH........",
+ "HHHHHHH........",
+ "HHHHHHHH.......",
+ "......b..H.....",
+ "...............",
+ "...............",
+ "...H..H..H.....",
+ "...............",
+ "...............",
+ "...............",
+ "...............",
+ "..............."}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(2, Arg1, minimumHoles(Arg0)); }
+	void test_case_3() { string Arr0[] = {"b..$...",
+ "...H...",
+ ".......",
+ "b..b..b",
+ "...H...",
+ ".......",
+ "b..b..b"}
+
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(3, Arg1, minimumHoles(Arg0)); }
+	void test_case_4() { string Arr0[] = {"b..b..b",
+ "..b..b.",
+ ".......",
+ "b..$bbb",
+ ".b.....",
+ "....b..",
+ "b..b..b"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = -1; verify_case(4, Arg1, minimumHoles(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main()
+{
+    BlockTheBlockPuzzle ___test; 
+    ___test.run_test(-1); 
+} 
+// END CUT HERE
