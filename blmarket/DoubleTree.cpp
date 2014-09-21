e2d1d00caafe544fd04c0a895e427bfb343bbd35
Jeong, Heon <blmarket@gmail.com>
2014-09-21 13:32:52 +0900

    --Empty Message--

--- blmarket/DoubleTree.cpp
+++ blmarket/DoubleTree.cpp
@@ -20,32 +20,6 @@ typedef pair<int,int> PII;
 
 template<typename T> int size(const T &a) { return a.size(); }
 
-/**
- * Problem: 600
- * Test Case: 8
- * Succeeded: No
- * Execution Time: 2 ms
- * Peak memory used: 11.617MB
- * Args:
- * {
- * {3, 21, 16, 12, 25,  9, 26, 15, 19, 2,  18, 0, 7, 4, 11, 14, 17, 20, 22, 8, 5, 10, 23, 24, 6, 27, 13}, 
- * {1, 1,   3,  1,  1, 25, 12, 21, 26, 12, 26, 9, 0, 15, 19, 2, 18, 7, 4, 20, 20, 14, 20, 20, 5, 10, 6}, 
- * {13, 18, 23, 9, 27, 14, 4, 0, 10, 11, 5, 24, 19, 15, 2, 20, 25, 21, 3, 7, 16, 12, 1, 22, 6, 26, 17}, 
- * {8, 13, 13, 18, 9, 13, 13, 14, 23, 0, 11, 11, 0, 11, 24, 5, 15, 25, 15, 21, 20, 20, 21, 16, 3, 22, 12}, 
- * {439, 677, -245, -564, -289, 687, 501, -359, 19, 691, 335, 211, 196, 21, 10, 335, -711, -233, -49, -371, -316, -432, 228, 776, 503, -17, -608, 28}}
- *
- * Expected:
- * 1696
- *
- * Received:
- * 3174
- *
- * Answer checking result:
- * Returned value must exactly match the expected one.
- *
- */
-
-vector<bool> visit;
 vector<vector<PII> > V;
 
 void init_graph(int sz) {
@@ -66,26 +40,28 @@ int add_edge(int s, int e, int flow) {
 
 int get_edge(int s, int e) { return add_edge(s, e, 0); }
 
-int try_flow(int s, int e, int maxflow) {
-  if(s == e) {
-    return maxflow;
-  }
-  if(visit[s]) return 0;
-  visit[s] = true;
-  for(auto &it : V[s]) {
-    if(it.second > 0) {
-      int tmp = try_flow(it.first, e, min(maxflow, it.second));
-      if(tmp == 0) continue;
+int do_flow(vector<vector<PII> > &V, int s, int e) {
+  vector<bool> visit;
 
-      add_edge(s, it.first, -tmp);
-      add_edge(it.first, s, tmp);
-      return tmp;
+  function<int(int,int,int)> try_flow = [&V, &visit, &try_flow](int s, int e, int maxflow) -> int {
+    if(s == e) {
+      return maxflow;
     }
-  }
-  return 0;
-}
+    if(visit[s]) return 0;
+    visit[s] = true;
+    for(auto &it : V[s]) {
+      if(it.second > 0) {
+        int tmp = try_flow(it.first, e, min(maxflow, it.second));
+        if(tmp == 0) continue;
 
-int do_flow(int s, int e) {
+        add_edge(s, it.first, -tmp);
+        add_edge(it.first, s, tmp);
+        return tmp;
+      }
+    }
+    return 0;
+  };
+
   int tot = 0;
   int tmp;
   while(true) {
@@ -136,7 +112,7 @@ public:
         }
       }
 
-      int tmp = do_flow(n, n+1);
+      int tmp = do_flow(V, n, n+1);
       for(int j=0;j<n;j++) {
           if(score[j] > 0) {
               cout << j << ":" << get_edge(n, j) << " ";
