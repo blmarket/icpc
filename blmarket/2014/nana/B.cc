d6058d12597fa85d289a282d3faa869cae5fbb7c
Jeong, Heon <blmarket@gmail.com>
2014-12-27 10:54:41 +0900

    --Empty Message--

--- blmarket/2014/nana/B.cc
+++ blmarket/2014/nana/B.cc
@@ -40,7 +40,7 @@ long long trial(const string &s) {
             cs = 0;
         }
         tmp = (size(s) - 1 - i) * 2 + (i - cs);
-        cout << s << " " << i << " " << tmp << endl;
+        // cout << s << " " << i << " " << tmp << endl;
         if(ret == -1 || ret > tmp) ret = tmp;
     }
     return ret;
