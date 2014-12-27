cf1ebe676512d806ff1d0e3483c76e562b02e037
Jeong, Heon <blmarket@gmail.com>
2014-12-27 11:35:08 +0900

    --Empty Message--

--- blmarket/2014/nana/B.cc
+++ blmarket/2014/nana/B.cc
@@ -32,6 +32,8 @@ int pp;
 long long trial(const string &s) {
     int cs = 0;
     long long ret = -1;
+    if(size(s) == 1 && s[0] == '0') return 0;
+    if(size(s) == 1) return (pp-1);
     for(int i=1;i<size(s);i++) {
         long long tmp;
         if(s[i] == '0') {
