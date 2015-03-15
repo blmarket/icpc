55405b49a3ee9d81df91af0faa754d96636a3a79
Jeong, Heon <blmarket@gmail.com>
2015-03-15 19:43:36 +0900

    --Empty Message--

--- blmarket/ArmyTeleportation.cpp
+++ blmarket/ArmyTeleportation.cpp
@@ -34,6 +34,7 @@ public:
         sort(v2.begin(), v2.end());
         for(int i=0;i<size(v1);i++) cout << v1[i].first << "," << v1[i].second << " "; cout << endl;
         for(int i=0;i<size(v2);i++) cout << v2[i].first << "," << v2[i].second << " "; cout << endl;
+        return "impossible";
     }
 
     
