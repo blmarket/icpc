cdfccddf575904b38a832a411c7d3712b9996061
Jeong, Heon <blmarket@gmail.com>
2015-03-15 20:45:51 +0900

    --Empty Message--

--- blmarket/ArmyTeleportation.cpp
+++ blmarket/ArmyTeleportation.cpp
@@ -45,8 +45,6 @@ bool match() {
         if(v3[i] != v3[0]) return false;
     }
 
-    cout << v3[0].first << " " << v3[0].second << endl;
-
     v4.resize(3);
     v4[0] = mp(xt[0] - xt[1], yt[0] - yt[1]);
     v4[1] = mp(xt[0] - xt[2], yt[0] - yt[2]);
