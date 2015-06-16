a05d17786efcab07804fc78c34b24b59e7b77a60
Heon Jeong <blmarket@linecorp.com>
2015-06-16 11:42:28 +0900

    --Empty Message--

--- blmarket/2015/ipscprac/test.cc
+++ blmarket/2015/ipscprac/test.cc
@@ -10,6 +10,7 @@ int main(void) {
         S.insert(i);
         if(S.bucket_count() != ps) {
             cout << S.bucket_count() << endl;
+            cout << S.bucket(i) << endl;
         }
         ps = S.bucket_count();
     }
