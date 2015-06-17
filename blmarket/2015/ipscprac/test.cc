43adf7bb00e1a7f29a1bd2712ae88605290285a7
Heon Jeong <blmarket@linecorp.com>
2015-06-17 20:06:34 +0900

    --Empty Message--

--- blmarket/2015/ipscprac/test.cc
+++ blmarket/2015/ipscprac/test.cc
@@ -6,6 +6,7 @@ using namespace std;
 int main(void) {
     int ps = 0;
     unordered_set<long long> S;
+
     for(int i=0;i<100000;i++) {
         S.insert(i);
         if(S.bucket_count() != ps) {
