1980aa6fdaa23aacb6f376dcee516be78ae4d5af
Heon Jeong <blmarket@linecorp.com>
2015-06-20 13:30:11 +0900

    --Empty Message--

--- blmarket/2015/ipscprac/2014B/test.cc
+++ blmarket/2015/ipscprac/2014B/test.cc
@@ -10,6 +10,17 @@ long long mod(long long a) {
     return a % (1LL << 32);
 }
 
+int it = 43;
+long long next() {
+    x[it] = x[(it-22+50)%50] - x[(it-43+50)%50] - c[(it-1+50)%50];
+    c[it] = x[it] < 0 ? 1 : 0;
+    x[it] += c[it] * (1LL << 32);
+
+    long long ret = x[it];
+    it = (it+1) % 50;
+    return ret;
+}
+
 int main(void) {
     for(int i=0;i<=42;i++) {
         x[i] = mod(mod(mod(i*i)*i)*999999999LL);
@@ -18,12 +29,7 @@ int main(void) {
     int it = 43;
 
     for(int i=0;i<10;i++) {
-        x[it] = x[(it-22+50)%50] - x[(it-43+50)%50] - c[(it-1+50)%50];
-        c[it] = x[it] < 0 ? 1 : 0;
-        x[it] += c[it] * (1LL << 32);
-
-        cout << x[it] << " " << c[it] << endl;
-        it = (it+1) % 50;
+        cout << next() << endl;
     }
 
 }
