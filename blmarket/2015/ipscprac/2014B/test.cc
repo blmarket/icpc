e3575f47c1650e0f58565f5cd46a80250be8aed9
Heon Jeong <blmarket@linecorp.com>
2015-06-20 13:29:17 +0900

    --Empty Message--

--- blmarket/2015/ipscprac/2014B/test.cc
+++ blmarket/2015/ipscprac/2014B/test.cc
@@ -20,6 +20,7 @@ int main(void) {
     for(int i=0;i<10;i++) {
         x[it] = x[(it-22+50)%50] - x[(it-43+50)%50] - c[(it-1+50)%50];
         c[it] = x[it] < 0 ? 1 : 0;
+        x[it] += c[it] * (1LL << 32);
 
         cout << x[it] << " " << c[it] << endl;
         it = (it+1) % 50;
