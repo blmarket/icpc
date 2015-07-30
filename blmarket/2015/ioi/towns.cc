93829d6e6736df345c61a5aea0d696c526aad913
Heon Jeong <blmarket@linecorp.com>
2015-07-30 14:35:10 +0900

    --Empty Message--

---
+++ blmarket/2015/ioi/towns.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct Grader {
+    int N;
+    int dist[50][50];
+    int cnt = 0;
+
+    Grader() {
+        scanf("%d", &N);
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++)
+                scanf(" %d", &dist[i][j]);
+        }
+    }
+
+    int getDist(int a, int b) {
+        cnt++;
+        return dist[a][b];
+    }
+};
+
+int main(void) {
+}
