7c1b684682d14dba652363d90bcc82bce4bb94d0
Heon Jeong <blmarket@linecorp.com>
2016-01-09 19:12:38 +0900

    --Empty Message--

--- blmarket/2016/hackercup-qual/d.cc
+++ blmarket/2016/hackercup-qual/d.cc
@@ -34,11 +34,17 @@ struct tri {
 
     tri() {
         memset(cost, -1, sizeof(cost));
+        cost[0] = 0;
     }
 
     void calc() {
         for(int i=0;i<26;i++) {
             if(child[i]) child[i]->calc();
+            for(int j=min(nc, K);j>=1;j--) {
+                for(int k=0;k<j;k++) {
+                    if(cost[k] < 0 || child[i]->cost[j-k] < 0) continue;
+                }
+            }
         }
     }
 };
