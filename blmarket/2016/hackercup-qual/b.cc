47d8949420b1f945a5021815548673d1da4189df
Heon Jeong <blmarket@linecorp.com>
2016-01-09 18:45:15 +0900

    --Empty Message--

--- blmarket/2016/hackercup-qual/b.cc
+++ blmarket/2016/hackercup-qual/b.cc
@@ -48,13 +48,16 @@ void process(void) {
         
         if(r[0][i] == r[1][i]) {
             if(p[0] <= 0 && p[1] <= 0) {
+                p[0] = p[1] = 0;
                 continue;
             }
             if(p[0] <= 1 || p[1] <= 1) {
                 ret++;
+                p[0] = p[1] = 0;
                 continue;
             }
             ret += 2;
+            p[0] = p[1] = 0;
             continue;
         }
 
