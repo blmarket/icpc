e6382454c89bcb473650f649146d2d58872d8978
Jeong, Heon <blmarket@gmail.com>
2016-04-09 17:31:39 +0900

    --Empty Message--

--- blmarket/2016/codejamQual/C.rs
+++ blmarket/2016/codejamQual/C.rs
@@ -19,6 +19,11 @@ fn process(ncase: u32) {
 
 fn main() {
   for i in 2..11 {
-    println!("{}", i);
+    let mut ii = 1;
+    for j in 0..16 {
+      print!("{} ", ii);
+      ii *= i;
+    }
+    println!("");
   }
 }
