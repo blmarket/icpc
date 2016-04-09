0a11192f0a5cf2263d7424cfaadc30601a592850
Jeong, Heon <blmarket@gmail.com>
2016-04-09 20:38:08 +0900

    --Empty Message--

--- blmarket/2016/codejamQual/C.rs
+++ blmarket/2016/codejamQual/C.rs
@@ -53,7 +53,7 @@ fn main() {
             tmp = (tmp + pmod(base, r1[j as usize], *p));
           }
         }
-        println!("{} {}", base, tmp);
+        println!("{} {} {}", base, p, tmp);
         if tmp == 0 {
           found = true;
           break;
