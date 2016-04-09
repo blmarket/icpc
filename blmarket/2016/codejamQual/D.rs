e651369d863cd0d293b24c0b4cef4ca1540fc409
Jeong, Heon <blmarket@gmail.com>
2016-04-09 21:21:11 +0900

    --Empty Message--

--- blmarket/2016/codejamQual/D.rs
+++ blmarket/2016/codejamQual/D.rs
@@ -13,13 +13,35 @@ fn dbg<T: Debug>(d: T) {
   println!("{:?}", d);
 }
 
+fn gen(K: i32, v: &Vec<i32>, x: usize, cur: i64) -> i64 {
+  if (x == v.len()) {
+    return cur;
+  }
+  return gen(K, &v, x+1, cur * (K as i64) + v[x] as i64);
+}
+
 fn process(ncase: u32) {
   print!("Case #{}: ", ncase);
   let ps = read_line::<String>().split(" ")
     .map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
   let (K,C,S) = (ps[0], ps[1], ps[2]);
 
-  println!("{} {} {}", K,C,S);
+  if (C*S < K) {
+    println!("IMPOSSIBLE");
+    return;
+  }
+
+  let mut it = 0;
+  for i in 0..S {
+    let mut v: Vec<i32> = vec![];
+    for j in 0..C {
+      v.push(it);
+      it += 1;
+    }
+
+    print!("{} ", gen(K, &v, 0, 0));
+  }
+  println!("");
 }
 
 fn main() {
