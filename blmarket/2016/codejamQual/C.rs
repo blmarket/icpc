316a890a255483c089d32304a1d07ef79bcdaba3
Jeong, Heon <blmarket@gmail.com>
2016-04-09 17:30:47 +0900

    --Empty Message--

---
+++ blmarket/2016/codejamQual/C.rs
@@ -0,0 +1,24 @@
+use std::io;
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::str::FromStr;
+
+fn read_line<T: FromStr + Clone>() -> T where <T as FromStr>::Err: Debug {
+  let mut ret = String::new();
+  io::stdin().read_line(&mut ret);
+  return ret.trim().parse::<T>().unwrap().to_owned();
+}
+
+fn dbg<T: Debug>(d: T) {
+  println!("{:?}", d);
+}
+
+fn process(ncase: u32) {
+  println!("Case #{}: ", ncase);
+}
+
+fn main() {
+  for i in 2..11 {
+    println!("{}", i);
+  }
+}
