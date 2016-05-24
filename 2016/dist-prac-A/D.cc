89084e3f0223511fc540ed725ab07a93301881bf
Heon Jeong <blmarket@linecorp.com>
2016-05-24 10:49:01 +0900

    --Empty Message--

--- 2016/dist-prac-A/D.cc
+++ 2016/dist-prac-A/D.cc
@@ -28,3 +28,30 @@ typedef long long LL;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 
+#include "load_balance.h"
+
+vector<LL> w;
+
+vector<LL> gen1(int s, int e) {
+    vector<LL> v;
+    v.pb(0);
+    for(int i=s;i<e;i++) {
+        for(int j=v.size()-1;j>=0;j--) {
+            v.pb(v[j] + w[i]);
+        }
+    }
+    return v;
+}
+
+int main(void) {
+    int my = MyNodeId();
+    int nn = NumberOfNodes();
+
+    int n = GetN();
+    for(int i=0;i<n;i++) w.pb(GetWeight(i));
+
+    vector<LL> v1 = move(gen1(0, n/2));
+    vector<LL> v2 = move(gen1(n/2, n));
+
+    return 0;
+}
