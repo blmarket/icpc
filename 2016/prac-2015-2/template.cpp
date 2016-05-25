b33b76d89c4a2b0b0e10a1cd23ce21700dcfda3d
Jeong, Heon <blmarket@gmail.com>
2016-05-25 22:22:09 +0900

    Prac init.

---
+++ 2016/prac-2015-2/template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int main(void) {
+    return 0;
+}
