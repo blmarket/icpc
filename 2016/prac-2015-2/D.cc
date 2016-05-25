51235782d593eef197413497b6a93aaa37589fa1
Jeong, Heon <blmarket@gmail.com>
2016-05-25 22:40:43 +0900

    --Empty Message--

--- 2016/prac-2015-2/D.cc
+++ 2016/prac-2015-2/D.cc
@@ -67,7 +67,7 @@ void go(int a, int b) {
     }
 
     if(tmp == data[a-1][b]) {
-      for(int i=1;i<=3;i++) if(i != tmp) {
+      for(int i=1;i<=3;i++) if(i != data[a-1][b]) {
         data[a][b] = i;
         go(a,b+1);
       }
