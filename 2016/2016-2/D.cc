5e718cf6c1d6e0ebb6a2b5eccac3ef47ce27f180
Jeong, Heon <blmarket@gmail.com>
2016-05-29 00:51:31 +0900

    --Empty Message--

--- 2016/2016-2/D.cc
+++ 2016/2016-2/D.cc
@@ -56,7 +56,7 @@ bool chk() {
         cnt++;
       }
     }
-    cerr << i << " " << total << " " << cnt << endl;
+    // cerr << i << " " << total << " " << cnt << endl;
     if(cnt == total) return false;
   }
   return true;
