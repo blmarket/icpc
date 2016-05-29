438a506306192a81f3eb35acb85f5c36a8a2e4d5
Jeong, Heon <blmarket@gmail.com>
2016-05-30 00:34:30 +0900

    --Empty Message--

--- 2016/dist-1/D.cc
+++ 2016/dist-1/D.cc
@@ -64,8 +64,9 @@ int main(void) {
       for(int i=0;i<nn;i++) {
         LL ll = N * i / nn;
         LL rr = N * (i+1) / nn;
-        cerr << "SIZE = " << rr - ll << " " << max(0LL, min(rr, tail) - ll) << endl;
         LL esti = (total / N) * (rr - ll) + max(0LL, min(rr, tail) - ll);
+
+        cerr << "II = " << left << " " << sums[i] << " " << esti << endl;
 
         PutLL(i, esti);
         PutLL(i, left);
