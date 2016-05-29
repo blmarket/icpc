a021e37166d19222060ce2d200ad85bbd5b953ea
Jeong, Heon <blmarket@gmail.com>
2016-05-30 00:10:22 +0900

    --Empty Message--

---
+++ 2016/dist-1/D.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+#include <message.h>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+#include "crates.h"
+
+int main(void) {
+    int my = MyNodeId();
+    int nn = NumberOfNodes();
+
+    LL N = GetNumStacks();
+    LL ls = N * my / nn;
+    LL rs = N * (my+1) / nn;
+
+    LL sum = 0;
+    for(LL i=ls;i<rs;i++) {
+      sum += GetStackHeight(i);
+    }
+    PutLL(0, sum);
+    Send(0);
+
+    if(my == 0) {
+      LL total = 0;
+      vector<LL> sums;
+      for(int i=0;i<nn;i++) {
+        Receive(i);
+        sums.pb(GetLL(i));
+        total += sums.back();
+      }
+      LL tail = total % N;
+
+      LL left = 0;
+      for(int i=0;i<nn;i++) {
+        LL ll = N * i / nn;
+        LL rr = N * (i+1) / nn;
+        LL esti = total / N * (rr - ll) + max(0, min(rr, tail) - ll);
+
+        PutLL(i, esti);
+        PutLL(i, left);
+        PutLL(i, left + sums[i] - esti);
+        Send(i);
+      }
+    }
+
+    Receive(0);
+    LL esti = GetLL(0);
+    LL left = GetLL(0);
+    LL right = GetLL(0);
+
+    LL oneh = esti / (rs-ls);
+    LL tail = esti % (rs-ls);
+    
+    LL moves = 0;
+    for(LL i=0;i<rs-ls;i++) {
+      LL cur = GetStackHeight(i + ls);
+      LL req = oneh + (i < tail);
+
+      moves += 1 + 2 * abs(left + cur - req);
+      moves %= (1e9 + 7);
+    }
+
+    PutLL(0, moves);
+    Send(0);
+    if(my == 0) {
+      LL total = 0;
+      for(int i=0;i<nn;i++) {
+        Receive(i);
+        total += GetLL(i);
+      }
+      total %= (1e9 + 7);
+      cout << total << endl;
+    }
+
+    return 0;
+}
