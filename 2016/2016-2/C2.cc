5f3b7ad1b8d3d7d330570afc78b9eca9f0706774
Jeong, Heon <blmarket@gmail.com>
2016-05-31 22:03:51 +0900

    --Empty Message--

--- 2016/2016-2/C2.cc
+++ 2016/2016-2/C2.cc
@@ -64,7 +64,7 @@ int trace(int a, int b, bool move) {
       if(!good(px, py)) break;
 
       int pd = track[px][py];
-      cerr << d << " " << pd << " " << px << " " << py << endl;
+      //cerr << d << " " << pd << " " << px << " " << py << endl;
       if((d == 0 && pd == 3) || (d == 1 && pd == 2) || (d == 2 && pd == 1) || (d == 3 && pd == 0))
         ret[px][py] = '/'; 
       else
