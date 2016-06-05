17ff8e2f608ef97638bbca20e685bb92606e6e7b
Jeong, Heon <blmarket@gmail.com>
2016-06-05 12:51:21 +0900

    --Empty Message--

---
+++ 2016/prac-2015-3/D.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+#define STR(p) (p).first << " " << (p).second
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+bool search(map<int, int> &cur) {
+  auto it = cur.rbegin();
+  auto jt = cur.rbegin();
+  ++jt;
+
+  cerr << STR(*it) << endl;
+  cerr << STR(*jt) << endl;
+}
+
+void process() {
+  int p;
+  map<int, int> cur;
+
+  vector<int> d1, d2;
+  scanf(" %d", &p);
+  d1.resize(p); d2.resize(p);
+  for(int i=0;i<p;i++) scanf(" %d", &d1[i]);
+  for(int i=0;i<p;i++) {
+    int tmp;
+    scanf(" %d", &tmp);
+    cur[d1[i]] = tmp;
+  }
+
+  search(cur);
+}
+
+int main(void) {
+  int T;
+  scanf(" %d", &T);
+  for(int i=1;i<=T;i++) {
+    printf("Case #%d: ",i);
+    process();
+  }
+  return 0;
+}
