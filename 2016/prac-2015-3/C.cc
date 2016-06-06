09a7470db5e6cf6f8e5c278baf083d48364433d9
Jeong, Heon <blmarket@gmail.com>
2016-06-06 19:40:14 +0900

    --Empty Message--

--- 2016/prac-2015-3/C.cc
+++ 2016/prac-2015-3/C.cc
@@ -68,8 +68,8 @@ void process(void) {
     for(int j=0;j<=R;j++) {
       for(int k=0;k<=N;k++) if(dyna[i][j][k] < 1e17) {
         double t = dyna[i][j][k];
-        cerr << i << " " << j << " " << k << " = " << t << endl;
         double pos = t * data[i].first + data[i].second;
+        cerr << i << " " << j << " " << k << " = " << t << " " << pos << endl;
 
         if(i < L) {
           int ni = ls[i];
