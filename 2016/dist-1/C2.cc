eeca537d41edf2619b65e32f1bb26cef678786b4
Heon Jeong <blmarket@linecorp.com>
2016-06-10 16:00:50 +0900

    --Empty Message--

--- 2016/dist-1/C2.cc
+++ 2016/dist-1/C2.cc
@@ -76,7 +76,7 @@ int main(void) {
         }
     }
 
-    cout << stack[0].first << " " << stack[0].second << endl;
+    cerr << stack[0].first << " " << stack[0].second << endl;
 
     PutLL(0, stack[0].first);
     Send(0);
@@ -90,6 +90,8 @@ int main(void) {
             while(ss > 1 && stack[ss-2].second == stack[ss-2].second) {
                 auto t1 = stack.back(); stack.pop_back();
                 auto t2 = stack.back(); stack.pop_back();
+
+                cerr << t1.first << " " << t2.first << endl;
 
                 if(comp(t1.first, t2.first)) {
                     stack.pb(mp(t1.first, t1.second + 1));
