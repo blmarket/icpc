c566eccdcd3fd1a6c8e1f01e2c5d0894738c1f4e
Heon Jeong <blmarket@linecorp.com>
2016-06-10 18:03:51 +0700

    --Empty Message--

--- 2016/dist-1/F.cc
+++ 2016/dist-1/F.cc
@@ -49,8 +49,6 @@ int main(void) {
         LL left = N * my / nn;
         LL right = N * (my + 1) / nn;
 
-        cerr << left << " " << right << endl;
-
         LL msp, mep;
         LL cnt = 0;
 
