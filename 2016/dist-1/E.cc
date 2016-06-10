8b0344011b14f10e5c44300d9c9d5af19483eda6
Heon Jeong <blmarket@linecorp.com>
2016-06-10 16:13:15 +0900

    --Empty Message--

--- 2016/dist-1/E.cc
+++ 2016/dist-1/E.cc
@@ -30,6 +30,44 @@ template<typename T> int size(const T &a) { return a.size(); }
 
 #include "winning_move.h"
 
+#define ROT32(x, y) ((x << y) | (x >> (32 - y))) // avoid effort
+uint32_t murmur(const char *key, uint32_t len, uint32_t seed = 1723621) {
+    static const uint32_t c1 = 0xcc9e2d51;
+    static const uint32_t c2 = 0x1b873593;
+    static const uint32_t r1 = 15;
+    static const uint32_t r2 = 13;
+    static const uint32_t m = 5;
+    static const uint32_t n = 0xe6546b64;
+
+    uint32_t hash = seed;
+
+    const int nblocks = len / 4;
+    const uint32_t *blocks = (const uint32_t *) key;
+    int i;
+    uint32_t k;
+    for (i = 0; i < nblocks; i++) {
+	k = blocks[i];
+	k *= c1;
+	k = ROT32(k, r1);
+	k *= c2;
+
+	hash ^= k;
+	hash = ROT32(hash, r2) * m + n;
+    }
+
+    const uint8_t *tail = (const uint8_t *) (key + nblocks * 4);
+    uint32_t k1 = 0;
+
+    hash ^= len;
+    hash ^= (hash >> 16);
+    hash *= 0x85ebca6b;
+    hash ^= (hash >> 13);
+    hash *= 0xc2b2ae35;
+    hash ^= (hash >> 16);
+
+    return hash;
+}
+
 int main(void) {
     int my = MyNodeId();
     int nn = NumberOfNodes();
@@ -38,7 +76,8 @@ int main(void) {
     vector<LL> v;
     for(int i=0;i<N;i++) {
       LL tmp = GetSubmission(i);
-      if((tmp % nn) == my) {
+      int hash = murmur(&tmp, 8);
+      if((hash % nn) == my) {
         v.pb(tmp);
       }
     }
