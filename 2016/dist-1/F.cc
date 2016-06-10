bc505bca85a4c79d03d4f024ea8d0209af129e4d
Heon Jeong <blmarket@linecorp.com>
2016-06-10 21:19:49 +0700

    --Empty Message--

--- 2016/dist-1/F.cc
+++ 2016/dist-1/F.cc
@@ -54,7 +54,6 @@ int main(void) {
 
     while(true) {
         if(my == 0) {
-            cerr << N << endl;
             for(int i=0;i<nn;i++) {
                 PutLL(i, N);
                 Send(i);
