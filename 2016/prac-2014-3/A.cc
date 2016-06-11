1fe540eba6fb44c1dc7610d3f61c5ec5cc344848
Heon Jeong <blmarket@linecorp.com>
2016-06-11 10:30:58 +0700

    --Empty Message--

--- 2016/prac-2014-3/A.cc
+++ 2016/prac-2014-3/A.cc
@@ -39,7 +39,30 @@ void process() {
         tmp += s;
         v.pb(tmp);
     }
-    each(it, v) cerr << it << " "; cerr << endl;
+    vector<LL> ss;
+    ss.pb(v[0]);
+    for(int i=1;i<N;i++) ss.pb(ss.back() + v[i]);
+
+    int jt = 0;
+
+    double ret = 1.0;
+    for(int i=0;i<N;i++) {
+        LL s3 = ss.back() - ss[i];
+        LL s1 = ss[jt];
+        LL s2 = ss[i] - s1;
+        while(jt<i && s1 < s2) {
+            double prob = (double)max(max(s1, s2), s3) / ss.back();
+            if(prob < ret) ret = prob;
+            jt++;
+            s1 = ss[jt];
+            s2 = ss[i] - s1;
+        }
+        double prob = (double)max(max(s1, s2), s3) / ss.back();
+        if(prob < ret) ret = prob;
+    }
+
+    ret = 1.0 - ret;
+    printf("%.12lf\n", ret);
 }
 
 int main(void) {
