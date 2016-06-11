fb5794b52c7b6bc08b01360b59d626e045377d4f
Heon Jeong <blmarket@linecorp.com>
2016-06-11 10:38:21 +0700

    --Empty Message--

--- 2016/prac-2014-3/A.cc
+++ 2016/prac-2014-3/A.cc
@@ -54,7 +54,7 @@ void process() {
         while(jt<i && s1 < s2) {
             double prob = (double)max(max(s1, s2), s3) / ss.back();
             if(prob < ret) {
-                cerr << i << " " << s1 << " " << s2 << " = " << prob << endl;
+                cerr << i << " " << s1 << " " << s2 << " " << s3 << " = " << prob << endl;
                 ret = prob;
             }
             jt++;
@@ -63,7 +63,7 @@ void process() {
         }
         double prob = (double)max(max(s1, s2), s3) / ss.back();
         if(prob < ret) {
-            cerr << i << " " << s1 << " " << s2 << " = " << prob << endl;
+            cerr << i << " " << s1 << " " << s2 << " " << s3 << " = " << prob << endl;
             ret = prob;
         }
     }
