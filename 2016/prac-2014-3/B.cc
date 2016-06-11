a5a322581b8bc42c708da15ee3ee2eb688d8d88f
Heon Jeong <blmarket@linecorp.com>
2016-06-11 15:41:58 +0700

    --Empty Message--

--- 2016/prac-2014-3/B.cc
+++ 2016/prac-2014-3/B.cc
@@ -27,7 +27,7 @@ typedef long long LL;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 
-int dyna[2][10005];
+int dyna[2][2][10005];
 int P, Q, N;
 
 void setmax(int &a, int b) {
@@ -37,7 +37,7 @@ void setmax(int &a, int b) {
 void process() {
     memset(dyna, -1, sizeof(dyna));
     scanf(" %d %d %d", &P, &Q, &N);
-    dyna[0][0] = 0;
+    dyna[0][0][0] = 0;
 
     int ret = 0;
     for(int i=0;i<N;i++) {
@@ -46,18 +46,22 @@ void process() {
         int cur = i % 2;
         int nex = !cur;
         memset(dyna[nex], -1, sizeof(dyna[0]));
-        for(int j=0;j<10005;j++) if(dyna[cur][j] >= 0) {
-            int jj = j + (H+Q-1) / Q;
-            cerr << i << " " << j << " = " << dyna[cur][j] << " " << jj << endl;
-            setmax(dyna[nex][jj], dyna[cur][j]);
-            if(P * jj >= ((H-1) % Q)+1) {
-                int jjj = jj - (((H-1)%Q)+1) / P;
-                setmax(dyna[nex][jjj], dyna[cur][j] + G);
+        for(int t=0;t<2;t++) {
+            for(int j=0;j<10005;j++) if(dyna[cur][t][j] >= 0) {
+                int jj = j + (H+Q-1) / Q;
+                cerr << i << " " << j << " = " << dyna[cur][t][j] << " " << jj << endl;
+                setmax(dyna[nex][0][jj], dyna[cur][t][j]);
+                if(H > 0 && P * jj >= ((H-1) % Q)+1) {
+                    int jjj = jj - (((H-1)%Q)+1) / P;
+                    setmax(dyna[nex][1][jjj], dyna[cur][t][j] + G);
+                }
             }
+            H -= Q;
         }
     }
     for(int i=0;i<10005;i++) {
-        setmax(ret, dyna[N%2][i]);
+        setmax(ret, dyna[N%2][0][i]);
+        setmax(ret, dyna[N%2][1][i]);
     }
     cout << ret << endl;
 }
