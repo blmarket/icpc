40b0e1c24eceb60ca2e1429f267bb8c657974d74
Heon Jeong <blmarket@linecorp.com>
2016-06-11 22:10:24 +0700

    --Empty Message--

---
+++ 2016/2016-3/D.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int N, L;
+vector<string> G;
+string B;
+
+void solve() {
+    int maxc = 0;
+    each(it, G) {
+        if(it == B) {
+            cout << "IMPOSSIBLE" << endl;
+            return;
+        }
+        char now = '0';
+        int cnt = 0;
+        for(int j=0;j<L;j++) {
+            if(now == '0' && it[j] != now) {
+                cnt++;
+            }
+            now = it[j];
+        }
+        maxc = max(maxc, cnt);
+    }
+
+    for(int i=0;i<maxc;i++) {
+        cout << "10?";
+    }
+    cout << " " << string(L-maxc, '?') << endl;
+}
+
+void process() {
+    scanf(" %d %d", &N, &L);
+    char buf[1024];
+    for(int i=0;i<N;i++) {
+        scanf(" %s", buf);
+        G.pb(buf);
+    }
+    scanf(" %s", buf);
+    B = buf;
+    solve();
+}
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+    return 0;
+}
