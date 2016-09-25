374ddd0dc4f570530a16ffb7a1bfdff336505809
Jeong, Heon <blmarket@gmail.com>
2016-09-25 23:13:31 +0900

    --Empty Message--

--- blmarket/2016/codejamAPAC2017/RoundA/D.cc
+++ blmarket/2016/codejamAPAC2017/RoundA/D.cc
@@ -82,6 +82,7 @@ void solve(int dataId)
     LL c1 = it.first;
     LL cr = M - c1;
     auto jt = upper_bound(ret2.begin(), ret2.end(), mp(cr+1LL, -1LL));
+    --jt;
     cerr << c1 << " " << cr << " " << jt->first << endl;
   }
 }
