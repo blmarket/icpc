3714f8995269daaa3a18dce29278f4714be531f3
Heon Jeong <blmarket@linecorp.com>
2016-11-20 12:42:47 +0900

    --Empty Message--

--- blmarket/asdf2.cc
+++ blmarket/asdf2.cc
@@ -12,26 +12,20 @@ int main(void) {
     scanf(" %d %d %d", &n, &m, &k);
     scanf(" %d %d", &prep, &mana);
     spell1.resize(m);
-    spell2.resize(k);
+    spell2.resize(k+1);
     for(int i=0;i<m;i++) scanf(" %d", &spell1[i].second);
     for(int i=0;i<m;i++) scanf(" %d", &spell1[i].first);
-    for(int i=0;i<k;i++) scanf(" %d", &spell2[i].second);
-    for(int i=0;i<k;i++) scanf(" %d", &spell2[i].first);
+    spell2[0] = make_pair(0, 0);
+    for(int i=1;i<=k;i++) scanf(" %d", &spell2[i].second);
+    for(int i=1;i<=k;i++) scanf(" %d", &spell2[i].first);
 
     spell1.emplace_back(make_pair(0, prep));
-    spell2.emplace_back(make_pair(0, 0));
-    sort(spell2.begin(), spell2.end());
 
-    for(int i=1;i<spell2.size();i++) {
-        while(i < spell2.size() && spell2[i].second <= spell2[i-1].second) {
-            spell2.erase(spell2.begin() + i);
-        }
-    }
-    long long mintime = 4e18;
+    long long mintime = 41e17;
     for(auto &it : spell1) {
         int cur = mana - it.first;
         if (cur < 0) continue;
-        vector<pair<int, int> >::iterator jt = upper_bound(spell2.begin(), spell2.end(), make_pair(cur, (int)1e9));
+        vector<pair<int, int> >::iterator jt = upper_bound(spell2.begin(), spell2.end(), make_pair(cur, (int)21e8));
         --jt;
         int remain = n - jt->second;
         if(remain < 0) remain = 0;
