ebae66251a825376869a21a95dae950ce1112011
Jeong, Heon <blmarket@gmail.com>
2016-11-26 20:33:15 +0900

    --Empty Message--

---
+++ 2016/381div2/C.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int n,m;
+
+int main(void) {
+  scanf(" %d %d", &n, &m);
+  int mind = n;
+  for(int i=0;i<m;i++) {
+    int a,b;
+    scanf(" %d %d", &a, &b);
+    int tmp = b-a + 1;
+    if(tmp < mind) mind = tmp;
+  }
+
+  cout << mind << endl;
+  for(int i=0;i<n;i++) {
+    cout << (i % mind) << " ";
+  }
+  cout << endl;
+  return 0;
+}
