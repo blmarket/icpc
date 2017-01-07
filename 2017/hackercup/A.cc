112c87d37540185f46ba32c38be367c36f140f1c
Jeong, Heon <blmarket@gmail.com>
2017-01-08 01:31:36 +0900

    --Empty Message--

---
+++ 2017/hackercup/A.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int p,x,y;
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        scanf(" %d %d %d", &p, &x, &y);
+
+        if(x == 50 && y == 50) {
+          if(p == 100) {
+            printf("black\n");
+            continue;
+          } else {
+            printf("white\n");
+            continue;
+          }
+        }
+
+        x -= 50;
+        y -= 50;
+
+        cout << atan2(y, x) << endl; 
+    }
+    return 0;
+}
