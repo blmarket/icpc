43f625f6a51f3352fa9e814520dbcbc1c8e3d6fc
Jeong, Heon <blmarket@gmail.com>
2017-01-08 01:49:50 +0900

    --Empty Message--

---
+++ 2017/hackercup/C.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int hp, n;
+
+void process() {
+  scanf(" %d %d", &hp, &n);
+  char buf[1024];
+  int nd, y;
+  for(int i=0;i<n;i++) {
+    scanf(" %dd%d%s", &nd, &y, buf);
+    cout << nd << " " << y << " " << buf << endl;
+  }
+}
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+    return 0;
+}
