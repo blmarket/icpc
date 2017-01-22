4eaacd3dc1e2f6506b2a52e490ade06d70910533
Jeong, Heon <blmarket@gmail.com>
2017-01-22 20:56:43 +0900

    --Empty Message--

--- 2017/codeforces/392div2/C.cc
+++ 2017/codeforces/392div2/C.cc
@@ -32,25 +32,43 @@ LL K;
 
 int main(void) {
   cin >> N >> M >> K >> x >> y;
-  LL pn = (N-1) * M;
+  LL pn = (N*2-1) * M;
   LL np = K / pn;
   LL nr = K % pn;
 
   auto fn = [&](int x, int y) -> LL {
     LL ret = 0;
-    if(x == 0) {
+    if(x == 0 || x == N - 1) {
+      ret = np;
+    } else {
+      ret = np * 2;
+    }
+
+    LL rr = nr;
+    for(int i=0;i<N;i++) {
+      for(int j=0;j<M;j++) {
+        if(rr == 0) return ret;
+        rr--;
+        if(x == i && y == j) {
+          ret++;
+        }
+      }
+    }
+    for(int i=N-2;i>=0;i--) {
+      for(int j=0;j<M;j++) {
+        if(rr == 0) return ret;
+        rr--;
+        if(x == i && y == j) {
+          ret++;
+        }
+      }
     }
   };
 
   LL r1,r2,r3;
-  if (K <= N * M) {
-    r1 = 1;
-  } else {
-    r1 = np + 1;
-  }
 
-  if (K < N * M) {
-    r2 = 0;
-  } else {
-  }
+  r1 = max(fn(1,0), fn(N-2,0));
+  r2 = min(fn(0,M-1), fn(N-1,M-1));
+  r3 = fn(x,y);
+  cout << r1 << " " << r2 << " " << r3 << endl;
 }
