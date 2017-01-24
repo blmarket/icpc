44b45d1212c324c9462ee701211fe971e5f73900
Heon Jeong <blmarket@linecorp.com>
2017-01-24 14:45:40 +0900

    --Empty Message--

--- 2017/codeforces/goodbye2016/E.cc
+++ 2017/codeforces/goodbye2016/E.cc
@@ -30,17 +30,38 @@ template<typename T> int size(const T &a) { return a.size(); }
 int n, q;
 char seq[200005];
 int matrix[524288][5][5];
+const int POS = 262144;
 
-int query(int a, int b) {
+void merge(auto &s1, auto &s2) {
 }
 
 int main(void) {
-  memset(matrix, 0, sizeof(matrix));
   scanf(" %d %d", &n, &q);
   scanf(" %s", seq);
 
   for(int i=0;i<n;i++) {
+    for(int j=0;j<5;j++) {
+      matrix[POS+i][j][j] = 0;
+      for(int k=j+1;k<5;k++) matrix[POS+i][j][k] = 1000000;
+    }
     if(seq[i] == '2') {
+      matrix[POS+i][0][0] = 1;
+      matrix[POS+i][0][1] = 0;
+    }
+    if(seq[i] == '0') {
+      matrix[POS+i][1][1] = 1;
+      matrix[POS+i][1][2] = 0;
+    }
+    if(seq[i] == '1') {
+      matrix[POS+i][2][2] = 1;
+      matrix[POS+i][2][3] = 0;
+    }
+    if(seq[i] == '6') {
+      matrix[POS+i][3][3] = 1;
+      matrix[POS+i][4][4] = 1;
+    }
+    if(seq[i] == '7') {
+      matrix[POS+i][3][4] = 0;
     }
   }
 
