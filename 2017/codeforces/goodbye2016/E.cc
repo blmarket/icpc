00b92cd082ca7a2fe1cd19fae92b72e8bd5699ae
Heon Jeong <blmarket@linecorp.com>
2017-01-24 14:58:06 +0900

    --Empty Message--

--- 2017/codeforces/goodbye2016/E.cc
+++ 2017/codeforces/goodbye2016/E.cc
@@ -48,6 +48,7 @@ void merge(auto &s1, auto &s2, auto &r) {
 
 void go(int pos, int l, int r, int s, int e) {
   cout << pos << " " << l << " " << r << endl;
+  if(r <= s || l >= e) return;
   if(s <= l && e >= r) {
     merge(t1, matrix[pos], t2);
     memcpy(t1, t2, sizeof(t1));
