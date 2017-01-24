50d9f18caabc0d7d487b69f6e55fbeb5266f9eab
Heon Jeong <blmarket@linecorp.com>
2017-01-24 14:36:30 +0900

    --Empty Message--

---
+++ 2017/codeforces/goodbye2016/E.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int n, q;
+char seq[200005];
+int matrix[524288][5][5];
+
+int query(int a, int b) {
+}
+
+int main(void) {
+  scanf(" %d %d", &n, &q);
+  scanf(" %s", seq);
+  for(int i=0;i<q;i++) {
+  }
+  return 0;
+}
