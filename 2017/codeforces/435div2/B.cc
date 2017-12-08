70e495df694fc5a2a0233194b3c00fa8ae908a3c
Jeong, Heon <heojeong@amazon.com>
2017-12-07 16:38:19 -0800

    --Empty Message--

---
+++ 2017/codeforces/435div2/B.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+bool flag[100005];
+PII edges[100005];
+
+int main(void) {
+  int n;
+  scanf(" %d", &n);
+  for(int i=0;i<n-1;i++) {
+    int a,b;
+    scanf(" %d %d", &a, &b);
+    if(a>b) swap(a,b);
+    edges[i] = mp(a,b);
+  }
+  sort(edges, edges + n - 1);
+  for(int i=0;i<n-1;i++) {
+  }
+  return 0;
+}
