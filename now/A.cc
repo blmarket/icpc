078af31df2e928f487570a7b9a561e35e778c8f5
Jeong, Heon <heojeong@amazon.com>
2018-04-29 09:18:54 -0700

    --Empty Message--

--- now/A.cc
+++ now/A.cc
@@ -24,6 +24,44 @@ typedef long long LL;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 
+int N, L;
+int V[100005];
+vector<PII> rem;
+
+void process() {
+  scanf(" %d %d", &N, &L);
+  rem.clear();
+  int sum = 0;
+  for(int i=0;i<L;i++) {
+    scanf(" %d", &V[i]);
+    sum += V[i];
+    int tmp = V[i] * 2 % (2 * N);
+    if(tmp >= N) {
+      rem.pb(mp(N, V[i]));
+      continue;
+    }
+    rem.pb(mp((N - tmp + 1) / 2, V[i]));
+  }
+  int ret = 0;
+  int remain = N - sum;
+  sort(rem.begin(), rem.end());
+  for(int i=0;i<size(rem);i++) {
+    if(remain >= rem[i].first) {
+      remain -= rem[i].first;
+      rem[i].second += rem[i].first;
+    }
+    ret += ((rem[i].second * 200) / N + 1) / 2;
+  }
+  cout << ret << endl;
+}
+
 int main(void) {
-    return 0;
+  int T;
+
+  scanf(" %d", &T);
+  for(int i=1;i<=T;i++) {
+    printf("Case #%d: ", i);
+    process();
+  }
+  return 0;
 }
