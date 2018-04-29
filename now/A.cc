1907267e47d1ab9be012bf50bb358b85482f1d2d
Jeong, Heon <heojeong@amazon.com>
2018-04-29 09:21:15 -0700

    --Empty Message--

--- now/A.cc
+++ now/A.cc
@@ -44,6 +44,10 @@ void process() {
   }
   int ret = 0;
   int remain = N - sum;
+  for(int i=0;i<remain;i++) {
+    rem.pb(mp((N+1)/2, 0));
+  }
+
   sort(rem.begin(), rem.end());
   for(int i=0;i<size(rem);i++) {
     if(remain >= rem[i].first) {
