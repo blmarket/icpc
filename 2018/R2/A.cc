f96bf6944c4637422bc23b90460539a20695ea3f
Jeong, Heon <blmarket@gmail.com>
2018-05-19 07:09:26 -0700

    --Empty Message--

---
+++ 2018/R2/A.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); }
+
+int N;
+int V[105];
+
+void process() {
+  scanf(" %d", &N);
+  for(int i=0;i<N;i++) {
+    scanf(" %d", &V[i]);
+  }
+
+  if(V[0] == 0 || V[N-1] == 0) {
+    printf("IMPOSSIBLE\n");
+    return;
+  }
+
+  int s = 0;
+  for(int i=0;i<N;i++) {
+    if(!V[i]) continue;
+    cerr << s << " " << s+V[i] << endl;
+    s += V[i];
+  }
+}
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+    return 0;
+}
