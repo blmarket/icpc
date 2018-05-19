c3adf894d2dec87c2437a24b5a605a1a95159050
Jeong, Heon <blmarket@gmail.com>
2018-05-19 07:45:14 -0700

    --Empty Message--

--- 2018/R2/C.cc
+++ 2018/R2/C.cc
@@ -27,29 +27,39 @@ typedef long long LL;
 
 template<typename T> int size(const T &a) { return a.size(); }
 
+struct conflict {
+  int idx;
+  vector<int> occ;
+  int rem;
+};
+
 int N;
 int V[105][105];
+vector<conflict> row[105], col[105];
+conflict *back[105][105];
 
 void process() {
+  for(int i=0;i<105;i++) { row[i].clear(); col[i].clear(); }
   scanf(" %d", &N);
   for(int i=0;i<N;i++) {
     for(int j=0;j<N;j++) scanf(" %d", &V[i][j]);
   }
 
   for(int i=0;i<N;i++) {
-    unordered_map<int, int> m1, m2;
+    unordered_map<int, vector<int>> m1, m2;
     m1.clear(); m2.clear();
-    for(int j=0;j<N;j++) m1[V[i][j]]++;
-    for(int j=0;j<N;j++) m2[V[j][i]]++;
+    for(int j=0;j<N;j++) m1[V[i][j]].pb(j);
+    for(int j=0;j<N;j++) m2[V[j][i]].pb(j);
 
     for(auto &it : m1) {
-      if(it.second == 1) continue;
-      fprintf(stderr, "r=%d c=%d cnt=%d\n", i, it.first, it.second-1);
+      if(it.second.size() == 1) continue;
+      fprintf(stderr, "r=%d c=%d cnt=%d\n", i, it.first, it.second.size());
+      row[i].pb(conflict { i, it.second, (int)it.second.size() - 1 });
     }
 
     for(auto &it : m2) {
-      if(it.second == 1) continue;
-      fprintf(stderr, "c=%d c=%d cnt=%d\n", i, it.first, it.second-1);
+      if(it.second.size() == 1) continue;
+      fprintf(stderr, "c=%d c=%d cnt=%d\n", i, it.first, it.second.size());
     }
   }
 }
