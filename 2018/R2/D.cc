c2f477580bcbc302d757378a03057c9c5a9e98e5
Jeong, Heon <blmarket@gmail.com>
2018-05-19 08:35:43 -0700

    --Empty Message--

--- 2018/R2/D.cc
+++ 2018/R2/D.cc
@@ -29,13 +29,49 @@ template<typename T> int size(const T &a) { return a.size(); }
 
 int N,M;
 char board[25][25];
+set<string> known;
+int ret;
+
+void check(string &pat) {
+  if(known.count(pat)) return;
+  known.insert(pat);
+  fprintf(stderr, "%s\n", pat.c_str());
+}
 
 void process() {
+  known.clear();
+  ret = 0;
+
   scanf(" %d %d", &N, &M);
   for(int i=0;i<N;i++) {
     scanf(" %s", board[i]);
     printf("%s\n", board[i]);
   }
+
+  for(int i=0;i+1<N;i++) {
+    for(int j=0;j+1<M;j++) {
+      string pat = string() + board[i][j] + board[i][j+1] + board[i+1][j] + board[i+1][j+1];
+      check(pat);
+    }
+  }
+  if(N == 1 && M == 1) {
+    string pat = string(board[0][0], 4);
+    check(pat);
+  }
+  if(M == 1) {
+    for(int i=0;i+1<N;i++) {
+      string pat = string() + board[i][0] + board[i][0] + board[i+1][0] + board[i+1][1];
+      check(pat);
+    }
+  }
+  if(N == 1) {
+    for(int i=0;i+1<M;i++) {
+      string pat = string() + board[0][i] + board[0][i+1] + board[0][i] + board[0][i+1];
+      check(pat);
+    }
+  }
+
+  printf("%d\n", ret);
 }
 
 int main(void) {
