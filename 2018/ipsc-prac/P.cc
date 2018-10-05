f1a1b13da99d77ccb00f9536408ce16fbd711c7c
Jeong, Heon <heojeong@amazon.com>
2018-10-05 15:44:36 -0700

    --Empty Message--

---
+++ 2018/ipsc-prac/P.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int main(void) {
+  for(int i=1;i<=26;i++) {
+    for(int j=1;j<=i;j++) {
+      if((j*j) % i == 0) {
+        cout << i << " " << j << endl;
+        break;
+      }
+    }
+  }
+  return 0;
+}
