4e17ddad5e8ce716bd7a612f05e075ed48875e30
Jeong, Heon <blmarket@gmail.com>
2019-05-03 21:49:15 -0700

    --Empty Message--

--- 2019/prac-2018-r2/D.cc
+++ 2019/prac-2018-r2/D.cc
@@ -40,7 +40,7 @@ int c1(int sx, int sy, int ex, int ey, int flag) {
   int ret = 0;
   for(int i=sx;i<ex;i++) {
     for(int j=sy;j<ey;j++) {
-      if((!!flag) ^ (data[i][j] == 'B'))
+      if((!flag) ^ (data[i][j] == 'B'))
         mask[i][j] = 1;
     }
   }
