0fbec78e261b99fdfe2a34405e6370e099847324
Jeong, Heon <blmarket@gmail.com>
2019-05-03 21:48:50 -0700

    --Empty Message--

--- 2019/prac-2018-r2/D.cc
+++ 2019/prac-2018-r2/D.cc
@@ -62,6 +62,11 @@ void cnt(int a, int b, int c) {
   memset(mask, 0, sizeof(mask));
   c1(0, 0, a, b, c & 1) + c1(a, 0, N, b, c & 2) + c1(0, b, a, M, c & 4) + c1(a, b, N, M, c & 8);
 
+  for(int i=0;i<N;i++) {
+    for(int j=0;j<M;j++) cout << mask[i][j];
+    cout << endl;
+  }
+
   for(int i=0;i<N;i++) for(int j=0;j<M;j++) if(mask[i][j] == 1) {
     int tmp = c2(i, j);
     if(ret < tmp) {
