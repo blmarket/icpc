3f49c44e0e7092985ded467d1b5884d5673565ae
Jeong, Heon <blmarket@gmail.com>
2019-05-08 23:10:11 -0700

    --Empty Message--

--- 2019/r1c/C.cc
+++ 2019/r1c/C.cc
@@ -47,7 +47,8 @@ int go(int sx, int sy, int ex, int ey) {
       if(!fail) {
         bool t1 = !!go(sx, sy, ex, j);
         bool t2 = !go(sx, j+1, ex, ey);
-        cerr << sx << " " << sy << " " << ex << " " << ey << " " << i << " " << j << " NS = " << (t1 ^ t2) << endl;
+        if(sx == 0 && sy == 0 && ex == R && ey == C) 
+          cerr << sx << " " << sy << " " << ex << " " << ey << " " << i << " " << j << " NS = " << (t1 ^ t2) << endl;
         ret += t1 ^ t2;
       }
       fail = false;
@@ -59,7 +60,8 @@ int go(int sx, int sy, int ex, int ey) {
       if(!fail) {
         bool t1 = !!go(sx, sy, i, ey);
         bool t2 = !go(i+1, sy, ex, ey);
-        cerr << sx << " " << sy << " " << ex << " " << ey << " " << i << " " << j << " WE = " << (t1 ^ t2) << endl;
+        if(sx == 0 && sy == 0 && ex == R && ey == C) 
+          cerr << sx << " " << sy << " " << ex << " " << ey << " " << i << " " << j << " WE = " << (t1 ^ t2) << endl;
         ret += t1 ^ t2;
       }
     }
