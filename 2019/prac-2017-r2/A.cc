03b83dd838b4af1c6256e9cc212495c9813e16f8
Jeong, Heon <blmarket@gmail.com>
2019-05-15 23:21:47 -0700

    --Empty Message--

--- 2019/prac-2017-r2/A.cc
+++ 2019/prac-2017-r2/A.cc
@@ -48,7 +48,7 @@ void process() {
     ret += (v[1] / 3) + (v[2] / 3);
     if((v[1]%3) || (v[2]%3)) ret++;
   } else if(P == 4) {
-    cerr << v[1] << " " << v[2] << " " << v[3] << endl;
+    cerr << ret << " " << v[1] << " " << v[2] << " " << v[3];
     int t1 = min(v[1], v[3]);
     ret += t1; v[1] -= t1; v[3] -= t1;
     int r1 = v[1] + v[3];
@@ -58,6 +58,7 @@ void process() {
     }
     ret += r1 / 3; r1 %= 3;
     if(r1 || v[2]) ret++;
+    cerr << " = " << ret << endl;
   }
   cout << ret << endl;
 }
