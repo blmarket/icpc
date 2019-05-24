b68bcabd56a64efd34664d4d92e47790dedf94e4
Jeong, Heon <heojeong@amazon.com>
2019-05-24 15:35:31 -0700

    --Empty Message--

---
+++ 2019/spoj/AIRLINES.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int M, N, K;
+
+int main(void) {
+  while(!feof(stdin)) {
+    scanf(" %d %d %d", &M, &N, &K);
+    cerr << M << " " << N << " " << K << endl;
+  }
+  return 0;
+}
