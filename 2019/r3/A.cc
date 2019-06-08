728a7b00ca5057ac021c365280e209f1467eeca3
Jeong, Heon <blmarket@gmail.com>
2019-06-08 07:09:02 -0700

    --Empty Message--

---
+++ 2019/r3/A.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int nimber(LL sz) {
+  set<int> zz;
+  if(sz < 1e10) return 0;
+  for(int i=0;i+1e10<sz;i+=1e10) {
+    zz.insert(nimber(i) ^ nimber(sz - i - 1e10));
+  }
+  for(int i=1e10-1;i+1e10<sz;i+=1e10) {
+    zz.insert(nimber(i) ^ nimber(sz - i - 1e10));
+  }
+  for(int i=0;;i++) {
+    if(zz.count(i) == 0) return i;
+  }
+}
+
+int main(void) {
+  cout << nimber(1e12) << endl;
+  return 0;
+}
