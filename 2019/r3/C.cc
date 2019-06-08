2bc60eb1fce341715e86632114ae9c022f93f6d3
Jeong, Heon <blmarket@gmail.com>
2019-06-08 09:01:23 -0700

    --Empty Message--

--- 2019/r3/C.cc
+++ 2019/r3/C.cc
@@ -42,7 +42,9 @@ bool chk(char t) {
   for(int i=1;i<=R;i++) for(int j=1;j<=C;j++) if(!visit[i][j] && data[i][j] == t) {
     if(found) return false;
     found = true;
+    cerr << i << " " << j << endl;
     vector<PII> bfs;
+    visit[i][j] = true;
     bfs.pb(i, j);
     auto moves = [&](int a, int b) {
       auto chk = [&](int na, int nb) {
