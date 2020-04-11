26ad3271d6d1c98a93fc39f811ed31da92a7d7a3
Jeong, Heon <blmarket@gmail.com>
2020-04-10 19:22:08 -0700

    --Empty Message--

--- 2020/R1A/B.cc
+++ 2020/R1A/B.cc
@@ -29,6 +29,36 @@ typedef long long LL;
 template<typename T> int size(const T &a) { return a.size(); } 
 
 long long cc[505][505];
+long long n;
+
+bool check(LL n2, int row, bool right) {
+  if(n2 >= (1LL << row)) {
+    if (check(n2 - (1LL << row), row - 1, !right)) {
+      return true;
+    }
+  } else {
+    if(check(n2 - 1, row - 1, right)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void process() {
+  // scanf(" %lld", &n);
+  n += 1;
+  for(int i=1;i<=63;i++) {
+    LL n2 = n;
+    for(int j=1;j<=i;j++) {
+      n2 -= cc[i][j];
+      if(check(n2, i, false)) {
+        cout << "FOUND" << endl;
+        return;
+      }
+    }
+  }
+  cout << "FAIL" << endl;
+}
 
 int main(void) {
   cc[1][1] = 1;
@@ -41,10 +71,12 @@ int main(void) {
     cout << cc[60][i] << " ";
   }
   cout << endl;
+
+  for(int i=0;i<1000000;i++) process();
   // int T;
   // scanf(" %d", &T);
   // for(int i=1;i<=T;i++) {
-  //   printf("Case #%d: ", i);
+  //   printf("Case #%d:\n", i);
   //   process();
   // }
   // return 0;
