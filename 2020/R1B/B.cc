cf2e5a43f6024780798c5dcb856e45b6aa941b28
Jeong, Heon <blmarket@gmail.com>
2020-04-26 13:02:26 -0700

    --Empty Message--

--- 2020/R1B/B.cc
+++ 2020/R1B/B.cc
@@ -18,6 +18,7 @@
 #define mp make_pair
 #define each(it, v) for(auto &it: v)
 #define pb emplace_back
+#define mt make_tuple
 
 using namespace std;
 
@@ -32,7 +33,46 @@ template<typename T> int size(const T &a) { return a.size(); }
 mt19937 rnd;
 int t, a, b;
 
+int safe_rnd() {
+  int tmp = (int)rnd();
+  while(tmp <= -1e9 || tmp >= 1e9) {
+    tmp = (int) rnd();
+  }
+  return tmp;
+}
+
+char chk(int x, int y) {
+  char buf[10];
+  printf("%d %d\n", x, y);
+  fflush(stdout);
+  scanf(" %s", buf);
+  return buf[0];
+}
+
+tuple<int, int> stage1() {
+  while(true) {
+    int t1, t2;
+    t1 = safe_rnd();
+    t2 = safe_rnd();
+    char tmp = chk(t1, t2);
+    if(tmp == 'C') return make_tuple(2e9, 2e9);
+    if(tmp == 'H') return mt(t1, t2);
+  }
+}
+
+int bs(int v, const function<int(int)> &func) {
+}
+
 void process() {
+  int x, y;
+  tie(x, y) = stage1();
+  if(x == 2e9) return;
+  int rx = bs(x, [y](int x) -> int {
+      return 0;
+      });
+  int ry = bs(y, [x](int y) -> int {
+      return 0;
+      });
 }
 
 int main(void) {
