1e44336084296cb8608fd19b9d705043fd9e6085
Jeong, Heon <blmarket@gmail.com>
2020-04-26 21:25:28 -0700

    --Empty Message--

--- 2020/R1B/A.cc
+++ 2020/R1B/A.cc
@@ -28,15 +28,41 @@ typedef long long LL;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 
-int x, y;
+char output[32];
+
+bool chk(int s, int t, int x, int y) {
+  if(abs(x) > abs(y)) {
+    if(x < 0) {
+      output[s] = 'S';
+      return chk(s-1, t>>1, x+t, y);
+    } else {
+      output[s] = 'N';
+      return chk(s-1, t>>1, x-t, y);
+    }
+  } else {
+    if(y < 0) {
+      output[s] = 'W';
+      return chk(s-1, t>>1, x, y+t);
+    } else {
+      output[s] = 'E';
+      return chk(s-1, t>>1, x, y-t);
+    }
+  }
+}
 
 void process() {
+  int x, y;
   scanf(" %d %d", &x, &y);
   int t1 = 1;
   for(int i=0;i<31;i++) {
-    cout << t1 << endl;
+    output[i+1] = 0;
+    if(chk(i, t1, x, y)) {
+      cout << output << endl;
+      return;
+    }
     t1 <<= 1;
   }
+  cout << "IMPOSSIBLE" << endl;
 }
 
 int main(void) {
