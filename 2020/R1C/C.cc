5269f4ae17afd391da6d852dc4aecbf8c0f02803
Jeong, Heon <blmarket@gmail.com>
2020-05-15 16:25:48 -0700

    --Empty Message--

--- 2020/R1C/C.cc
+++ 2020/R1C/C.cc
@@ -24,15 +24,40 @@ typedef vector<int> VI;
 typedef vector<VI> VVI;
 typedef vector<string> VS;
 typedef pair<int,int> PII;
-typedef pair<long long, long long> PLL;
 typedef long long LL;
 
 template<typename T> int size(const T &a) { return a.size(); } 
 
-map<PLL, VI> cand;
+struct rat {
+  LL a;
+  int b;
+
+  bool operator<(const rat &rhs) const {
+    return a * rhs.b < rhs.a * b;
+  }
+};
+
+map<rat, vector<LL>> cand;
+int N, D;
+
+LL gcd(LL a, LL b) {
+  if(a<b) return gcd(b,a);
+  if(b == 0) return a;
+  return gcd(b, a % b);
+}
 
 void process() {
   cand.clear();
+  scanf(" %d %d", N, D);
+  for(int i=0;i<N;i++) {
+    LL tmp;
+    scanf(" %lld", &tmp);
+    for(int j=1;j<=D;j++) {
+      int gg = gcd(tmp, j);
+      rat rr = rat { tmp / gg, j / gg };
+      cand[rr].pb(j);
+    }
+  }
 }
 
 int main(void) {
