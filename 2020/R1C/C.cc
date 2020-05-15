05e7d8be24b352d5d69e13eb1a0e5619cd879b02
Jeong, Heon <blmarket@gmail.com>
2020-05-15 16:18:29 -0700

    --Empty Message--

---
+++ 2020/R1C/C.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef pair<long long, long long> PLL;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+map<PLL, VI> cand;
+
+void process() {
+  cand.clear();
+}
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+    return 0;
+}
