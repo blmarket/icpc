bd330508122c5ef75205bdc20f4fc8b93f2c4014
Jeong, Heon <blmarket@gmail.com>
2020-05-30 10:08:37 -0700

    --Empty Message--

--- 2020/R1C/B.cc
+++ 2020/R1C/B.cc
@@ -49,8 +49,8 @@ void process() {
       // C[i][s[s.size()-1-i]] += 1;
       C[i][s[i]] += 1;
     }
-    for(auto &it: C[i]) cout << it.first << " " << it.second << endl;
-    cout << endl;
+    for(auto &it: C[i]) cerr << it.first << " " << it.second << endl;
+    cerr << endl;
   }
 
   vector<pair<int, char> > cs;
