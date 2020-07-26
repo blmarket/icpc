0a92b9237b5e42e4a5d31d9beba1e163944c35a1
Jeong, Heon <blmarket@gmail.com>
2020-07-26 08:31:04 -0700

    --Empty Message--

--- 2020/Hqual/D1.cc
+++ 2020/Hqual/D1.cc
@@ -34,6 +34,7 @@ int n,m;
 LL c[2100000];
 
 LL findmin(int s, int e, int p=1,int ss=0, int ee=1<<20) {
+  if(s >= e) return 0;
   if(s > ee || e < ss) return BIG;
   cout << s << " " << e << " " << p << " " << ss << " " << ee << endl;
   if(s <= ss && e >= ee) return c[p];
