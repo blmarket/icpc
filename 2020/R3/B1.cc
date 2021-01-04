07abaac10038b21d38b69280730a380421352e76
Jeong, Heon <blmarket@gmail.com>
2021-01-03 17:08:32 -0800

    --Empty Message--

---
+++ 2020/R3/B1.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <sstream>
+#include <numeric>
+#include <iterator>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+
+#define mp make_pair
+#define each(it, v) for(auto &it: v)
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+int K, N;
+int T[20];
+
+void process() {
+  scanf(" %d %d", &K, &N);
+  for(int i=0;i<N;i++) {
+    scanf(" %d", &T[i]);
+  }
+  for(int i=0;i<N;i++) {
+    int unused; scanf(" %d", &unused);
+  }
+
+  int nn = (1 << (K*2));
+  for(int i=0;i<nn;i++) {
+    for(int j=0;j<N;j++) if((1<<j) & i) {
+      cerr << j << " ";
+    }
+    cerr << endl;
+  }
+}
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+    return 0;
+}
