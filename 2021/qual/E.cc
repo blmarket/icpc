5f6c96a6f71192f8279beb64dd04a0d3649ba387
Jeong, Heon <blmarket@gmail.com>
2021-03-27 09:53:43 -0700

    --Empty Message--

--- 2021/qual/E.cc
+++ 2021/qual/E.cc
@@ -26,7 +26,7 @@ void process() {
   for(int i=0;i<10000;i++) {
     int cnt = 0;
     for(int j=0;j<100;j++) if(ans[j][i] == '1') ++cnt;
-    diff[i] = {-cnt, i};
+    diff[i] = {cnt, i};
   }
   sort(diff, diff + 10000);
 
