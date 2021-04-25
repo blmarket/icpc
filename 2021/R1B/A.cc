c9f80db6ad6184f5d9553e04276b4f0386f5c7e6
Jeong, Heon <blmarket@gmail.com>
2021-04-25 10:08:58 -0700

    --Empty Message--

--- 2021/R1B/A.cc
+++ 2021/R1B/A.cc
@@ -67,7 +67,7 @@ bool check(long long h, long long m, long long s) {
       long long s2 = s - i * NANO * 720;
 
       if (j == 6 && i == 1800) {
-        cout << h2 << " " << m2 << " " << s2 << endl;
+        cerr << h2 << " " << m2 << " " << s2 << endl;
       }
 
       long long d = h2 * 12 - m2;
@@ -96,7 +96,6 @@ bool check(long long h, long long m, long long s) {
 void process() {
   scanf(" %lld %lld %lld", &inp[0], &inp[1], &inp[2]);
   if (check(inp[2], inp[1], inp[0])) return;
-  return;
   if (check(inp[0], inp[1], inp[2])) return;
   if (check(inp[0], inp[2], inp[1])) return;
   if (check(inp[1], inp[0], inp[2])) return;
