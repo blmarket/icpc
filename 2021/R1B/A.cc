91808f841a78cfadd30b111c343c67b817a6a7dd
Jeong, Heon <blmarket@gmail.com>
2021-04-25 10:20:04 -0700

    --Empty Message--

--- 2021/R1B/A.cc
+++ 2021/R1B/A.cc
@@ -38,13 +38,12 @@ bool check(long long h, long long m, long long s) {
         }
       }
 
-      cerr << d << endl;
-
       h2 = (((h2-d) % ROUND) + ROUND) % ROUND;
       m2 = (((m2-d) % ROUND) + ROUND) % ROUND;
       s2 = (((s2-d) % ROUND) + ROUND) % ROUND;
 
       if((h2*12) % ROUND == m2 && (h2*720) == s2 % ROUND) {
+        cerr << d << endl;
         cout << j << " " << mm << " " << ss << " " << ((-d % NANO) + NANO) % NANO << endl;
         return true;
       }
