9778cff22cdf6c888c7583fd97344f6337018b34
Jeong, Heon <blmarket@gmail.com>
2021-05-08 21:54:37 -0700

    --Empty Message--

---
+++ 2021/R1C/B.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define pb emplace_back
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef long long LL;
+
+template<typename T> int size(const T &a) { return a.size(); } 
+
+const long long MAX = 2000000000000000000LL;
+
+void process() {
+}
+
+int main(void) {
+    int T;
+    scanf(" %d", &T);
+    for(int i=1;i<=T;i++) {
+        printf("Case #%d: ", i);
+        process();
+    }
+    return 0;
+}
