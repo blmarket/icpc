230509fa6d1e1dad10cb3b1162b99bfb6a4c1c4e
Jeong, Heon <blmarket@gmail.com>
2021-05-09 22:17:15 -0700

    --Empty Message--

--- 2021/R1C/C.cc
+++ 2021/R1C/C.cc
@@ -65,7 +65,7 @@ void process() {
     bool fail = false;
     int tmp = sp;
     for(int j=0;j<v2.size();j++) {
-      // cerr << sp << " " << j << " " << tmp << endl;
+      cerr << sp << " " << j << " " << tmp << endl;
       int p1 = sp+j;
       if(p1+1 < v1.size()) {
         if(v1[p1] != v2[j]) {
